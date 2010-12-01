9ecb0ba4d30c704339c4d8eccd28e2eb7a0fda29
Joel Andersson <j.a.e.andersson@gmail.com>
2010-12-01 14:18:52 +0000

    license clause in all C++ files

--- ipopt_interface/test/hs071_main.cpp
+++ ipopt_interface/test/hs071_main.cpp
@@ -1,3 +1,26 @@
+/*
+ *    This file is part of CasADi.
+ *
+ *    CasADi -- A minimalistic computer algebra system with automatic differentiation 
+ *    and framework for dynamic optimization.
+ *    Copyright (C) 2010 by Joel Andersson, Moritz Diehl et al., K.U.Leuven. All rights reserved.
+ *
+ *    CasADi is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    CasADi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with CasADi; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
 // Copyright (C) 2005, 2007 International Business Machines and others.
 // All Rights Reserved.
 // This code is published under the Common Public License.
---
+++ misc/update_license.py
@@ -0,0 +1,46 @@
+# updates the copyright information for all .cs files
+# usage: call recursive_traversal, with the following parameters
+# parent directory, old copyright text content, new copyright text content
+
+import os
+
+excludedir = ["..\\Lib"]
+
+def update_source(filename, oldcopyright, copyright):
+    utfstr = chr(0xef)+chr(0xbb)+chr(0xbf)
+    fdata = file(filename,"r+").read()
+    isUTF = False
+    if (fdata.startswith(utfstr)):
+        isUTF = True
+        fdata = fdata[3:]
+    if (oldcopyright != None):
+        if (fdata.startswith(oldcopyright)):
+            fdata = fdata[len(oldcopyright):]
+    if not (fdata.startswith(copyright)):
+        print "updating "+filename
+        fdata = copyright + fdata
+        if (isUTF):
+            file(filename,"w").write(utfstr+fdata)
+        else:
+            file(filename,"w").write(fdata)
+
+def recursive_traversal(dir,  oldcopyright, copyright):
+    global excludedir
+    fns = os.listdir(dir)
+    print "listing "+dir
+    for fn in fns:
+        fullfn = os.path.join(dir,fn)
+        if (fullfn in excludedir):
+            continue
+        if (os.path.isdir(fullfn)):
+            recursive_traversal(fullfn, oldcopyright, copyright)
+        else:
+            if (fullfn.endswith(".cpp") or fullfn.endswith(".hpp") or fullfn.endswith(".h")):
+                update_source(fullfn, oldcopyright, copyright)
+
+
+#oldcright = file("license_header.txt","r+").read()
+oldcright = None
+cright = file("license_header.txt","r+").read()
+recursive_traversal("..", oldcright, cright)
+exit()
