4760a1af4349471c05548f601c40a135665566a4
Joel Andersson <j.a.e.andersson@gmail.com>
2010-12-02 18:51:16 +0000

    added super casadi lu framework

---
+++ superlu_interface/superlu.hpp
@@ -0,0 +1,52 @@
+/*
+ *    This file is part of CasADi.
+ *
+ *    CasADi -- A minimalistic computer algebra system with automatic differentiation 
+ *    and framework for dynamic optimization.
+ *    Copyright (C) 2010 by Joel Andersson, Moritz Diehl et al., K.U.Leuven. All rights reserved.
+ *
+ *    CasADi is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    CasADi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with CasADi; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef SUPERLU_HPP
+#define SUPERLU_HPP
+
+#include "casadi/fx/fx.hpp"
+
+namespace CasADi{
+  
+/** \brief  Forward declaration of internal class */
+class SuperLUInternal;
+
+/** \brief  Public class */
+class SuperLU : public FX{
+public:
+
+  /// Default (empty) constructor
+  SuperLU();
+  
+  /// Create a linear solver given a sparsity pattern
+  SuperLU(int nrow, int ncol, const std::vector<int>& rowind, const std::vector<int>& col, int nrhs=1);
+  
+  /** \brief  Access functions of the node */
+  SuperLUInternal* operator->();
+  const SuperLUInternal* operator->() const;
+};
+
+} // namespace CasADi
+
+#endif //SUPERLU_HPP
+
---
+++ superlu_interface/superlu_internal.hpp
@@ -0,0 +1,51 @@
+/*
+ *    This file is part of CasADi.
+ *
+ *    CasADi -- A minimalistic computer algebra system with automatic differentiation 
+ *    and framework for dynamic optimization.
+ *    Copyright (C) 2010 by Joel Andersson, Moritz Diehl et al., K.U.Leuven. All rights reserved.
+ *
+ *    CasADi is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    CasADi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with CasADi; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef SUPERLU_INTERNAL_HPP
+#define SUPERLU_INTERNAL_HPP
+
+#include "superlu.hpp"
+
+namespace CasADi{
+  
+class SuperLUInternal : public FXNode{
+  public:
+    // Create a linear solver given a sparsity pattern and a number of right hand sides
+    SuperLUInternal(int nrow, int ncol, const std::vector<int>& rowind, const std::vector<int>& col, int nrhs);
+
+    // Solve the system of equations
+    virtual void evaluate(int fsens_order, int asens_order);
+
+  protected:
+    
+    // Sparsity in CRS format
+    int nrow_, ncol_;
+    std::vector<int> rowind_, col_;
+    int nrhs_;
+    
+};
+
+} // namespace CasADi
+
+#endif //SUPERLU_INTERNAL_HPP
+
