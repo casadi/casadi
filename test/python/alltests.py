316380b6066d8cdb46a0afd3bb06a75193056a39
Joris Gillis <joris.gillis42@gmail.com>
2011-02-14 22:52:11 +0000

    Added NLP unittests

--- test/python/alltests.py
+++ test/python/alltests.py
@@ -3,6 +3,7 @@ from sx import *
 from typemaps import *
 from integration import *
 from ocp import *
+from nlp import *
 
 if __name__ == '__main__':
     unittest.main()
---
+++ test/python/nlp.py
@@ -0,0 +1,71 @@
+from casadi import *
+import casadi as c
+from numpy import *
+import unittest
+from types import *
+from helpers import *
+
+class NLPtests(casadiTestCase):
+  def testIPOPT(self):
+    self.message("trivial IPOP")
+    x=SX("x")
+    f=SXFunction([x],[(x-1)**2])
+    g=SXFunction([x],[x])
+    
+    solver = IpoptSolver(f,g)
+    solver.setOption("tol",1e-5)
+    solver.setOption("hessian_approximation", "limited-memory")
+    solver.setOption("max_iter",100)
+    solver.setOption("print_level",0)
+    solver.init()
+    solver.input(NLP_LBX).set([-10])
+    solver.input(NLP_UBX).set([10])
+    solver.input(NLP_LBG).set([-10])
+    solver.input(NLP_UBG).set([10])
+    solver.solve()
+    self.assertAlmostEqual(solver.output(NLP_COST)[0],0,10,"IPOPT")
+    self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
+
+  def testIPOPTc(self):
+    self.message("trivial IPOP, overconstrained")
+    x=SX("x")
+    f=SXFunction([x],[(x-1)**2])
+    g=SXFunction([x],[[x,x,x]])
+    
+    solver = IpoptSolver(f,g)
+    solver.setOption("tol",1e-5)
+    solver.setOption("hessian_approximation", "limited-memory")
+    solver.setOption("max_iter",100)
+    solver.setOption("print_level",0)
+    solver.init()
+    solver.input(NLP_LBX).set([-10])
+    solver.input(NLP_UBX).set([10])
+    solver.input(NLP_LBG).set([-10, -10, -10])
+    solver.input(NLP_UBG).set([10, 10, 10])
+    solver.solve()
+    self.assertAlmostEqual(solver.output(NLP_COST)[0],0,10,"IPOPT")
+    self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
+ 
+  def testIPOPTc2(self):
+    self.message("trivial IPOP, overconstrained")
+    x=SX("x")
+    f=SXFunction([x],[(x-1)**2])
+    g=SXFunction([x],[[x,x,x+x]])
+    
+    solver = IpoptSolver(f,g)
+    solver.setOption("tol",1e-10)
+    solver.setOption("hessian_approximation", "limited-memory")
+    solver.setOption("max_iter",100)
+    solver.setOption("print_level",0)
+    solver.init()
+    solver.input(NLP_LBX).set([-10])
+    solver.input(NLP_UBX).set([10])
+    solver.input(NLP_LBG).set([-10, -10, -10])
+    solver.input(NLP_UBG).set([10, 10, 10])
+    solver.solve()
+    self.assertAlmostEqual(solver.output(NLP_COST)[0],0,10,"IPOPT")
+    self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,8,"IPOPT")
+    
+if __name__ == '__main__':
+    unittest.main()
+
