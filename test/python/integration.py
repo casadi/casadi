2e13325460f3a3d5786d18fc994bab5b0e0807ad
Joris Gillis <joris.gillis42@gmail.com>
2011-02-15 19:26:03 +0000

    Unittest for integrator hessian by fwd-over-adjoint

--- test/python/integration.py
+++ test/python/integration.py
@@ -30,6 +30,7 @@ class Integrationtests(casadiTestCase):
     qend=integrator([t0,tend,q0,par,dq0])
     qe=MXFunction([tend,q0,par],[qend])
     qe.init()
+    self.integrator = integrator
     self.qe=qe
     self.num={'tend':2.3,'q0':7.1,'p':2}
     pass
@@ -94,21 +95,25 @@ class Integrationtests(casadiTestCase):
     p=num['p']
     self.assertAlmostEqual(J.output()[0],-(q0*tend**3*exp(tend**3/(3*p)))/(3*p**2),9,"Evaluation output mismatch")
     
-  def test_hess0(self):
-    return
-    self.message('IPOPT integration: jacobian to end time')
+  def test_hess(self):
+    self.message('IPOPT integration: hessian to p: fwd-over-adjoint on integrator')
     num=self.num
-    J=self.qe.hessian(0)
+    J=self.integrator.jacobian(INTEGRATOR_P,INTEGRATOR_XF)
+    J.setOption("number_of_fwd_dir",0)
+    J.setOption("number_of_adj_dir",1)
     J.init()
-    J.input(0).set([num['tend']])
-    J.input(1).set([num['q0']])
-    J.input(2).set([num['p']])
-    J.evaluate()
+    J.input(INTEGRATOR_TF).set([num['tend']])
+    J.input(INTEGRATOR_X0).set([num['q0']])
+    J.input(INTEGRATOR_P).set([num['p']])
+    J.adjSeed(0).set([1])
+    # Evaluate
+    J.evaluate(0,1)
+      
     tend=num['tend']
     q0=num['q0']
     p=num['p']
-    # known bug
-    #self.assertAlmostEqual(J.output()[0],(q0*tend**2*exp(tend**3/(3*p)))/p,9,"Evaluation output mismatch")
+
+    self.assertAlmostEqual(J.adjSens(INTEGRATOR_P)[0],(q0*tend**6*exp(tend**3/(3*p)))/(9*p**4)+(2*q0*tend**3*exp(tend**3/(3*p)))/(3*p**3),9,"Evaluation output mismatch")
     
 if __name__ == '__main__':
     unittest.main()
--- test/python/nlp.py
+++ test/python/nlp.py
@@ -9,7 +9,7 @@ class NLPtests(casadiTestCase):
   def testIPOPT(self):
     self.message("trivial IPOP")
     x=SX("x")
-    f=SXFunction([x],[(x-1)**2])
+    f=SXFunction([x],[[(x-1)**2]])
     g=SXFunction([x],[x])
     
     solver = IpoptSolver(f,g)
@@ -26,6 +26,163 @@ class NLPtests(casadiTestCase):
     self.assertAlmostEqual(solver.output(NLP_COST)[0],0,10,"IPOPT")
     self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
     
+    
+  def testIPOPTrb(self):
+    self.message("IPOPT rosenbrock, limited-memorey hessian approx")
+    x=SX("x")
+    y=SX("y")
+    
+    f=SXFunction([[x,y]],[(1-x)**2+100*(y-x**2)**2])
+    
+    solver = IpoptSolver(f)
+    solver.setOption("tol",1e-8)
+    solver.setOption("hessian_approximation", "limited-memory")
+    solver.setOption("max_iter",100)
+    #solver.setOption("print_level",0)
+    solver.init()
+    solver.input(NLP_LBX).set([-10]*2)
+    solver.input(NLP_UBX).set([10]*2)
+    solver.solve()
+    self.assertAlmostEqual(solver.output(NLP_COST)[0],0,10,"IPOPT")
+    self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
+    self.assertAlmostEqual(solver.output(NLP_X_OPT)[1],1,10,"IPOPT")
+
+    
+  def testIPOPTrb(self):
+    self.message("IPOPT rosenbrock, limited-memorey hessian approx")
+    x=SX("x")
+    y=SX("y")
+    
+    f=SXFunction([[x,y]],[(1-x)**2+100*(y-x**2)**2])
+    g=SXFunction([[x,y]],[x+y])
+    solver = IpoptSolver(f,g)
+    solver.setOption("tol",1e-8)
+    solver.setOption("hessian_approximation", "limited-memory")
+    solver.setOption("max_iter",100)
+    #solver.setOption("print_level",0)
+    solver.init()
+    solver.input(NLP_LBX).set([-10]*2)
+    solver.input(NLP_UBX).set([10]*2)
+    solver.input(NLP_LBG).set([-10])
+    solver.input(NLP_UBG).set([10])
+    solver.solve()
+    self.assertAlmostEqual(solver.output(NLP_COST)[0],0,10,"IPOPT")
+    self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
+    self.assertAlmostEqual(solver.output(NLP_X_OPT)[1],1,10,"IPOPT")
+    
+    
+  def testIPOPTrhb2(self):
+    return
+    self.message("IPOPT rosenbrock, exact hessian")
+    x=SX("x")
+    y=SX("y")
+    
+    f=SXFunction([[x,y]],[(1-x)**2+100*(y-x**2)**2])
+    g=SXFunction([[x,y]],[x+y])
+    solver = IpoptSolver(f,g)
+    
+    h=f.hessian()
+    h.init()
+    h.input().set([1,1])
+    h.evaluate()
+    print array(h.output())
+    
+    
+    solver = IpoptSolver(f,g,h)
+    solver.setOption("tol",1e-8)
+    solver.setOption("max_iter",100)
+    solver.setOption("hessian_approximation", "exact")
+    #solver.setOption("print_level",0)
+    solver.init()
+    solver.input(NLP_LBX).set([-10]*2)
+    solver.input(NLP_UBX).set([10]*2)
+    solver.input(NLP_LBG).set([-10])
+    solver.input(NLP_UBG).set([10])
+    solver.solve()
+    self.assertAlmostEqual(solver.output(NLP_COST)[0],0,10,"IPOPT")
+    self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
+    self.assertAlmostEqual(solver.output(NLP_X_OPT)[1],1,10,"IPOPT")
+     
+  def testIPOPTrhb(self):
+    self.message("IPOPT rosenbrock, exact hessian")
+    x=SX("x")
+    y=SX("y")
+    
+    obj=(1-x)**2+100*(y-x**2)**2
+    f=SXFunction([[x,y]],[obj])
+    
+    sigma=SX("sigma")
+    
+    h=SXFunction([[x,y],[],[sigma]],[sigma*hessian(obj,[x,y])])
+    
+    solver = IpoptSolver(f,FX(),h)
+    solver.setOption("tol",1e-10)
+    solver.setOption("max_iter",100)
+    solver.setOption("hessian_approximation", "exact")
+    #solver.setOption("print_level",0)
+    solver.init()
+    solver.input(NLP_LBX).set([-10]*2)
+    solver.input(NLP_UBX).set([10]*2)
+    solver.solve()
+    self.assertAlmostEqual(solver.output(NLP_COST)[0],0,10,"IPOPT")
+    self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
+    self.assertAlmostEqual(solver.output(NLP_X_OPT)[1],1,10,"IPOPT")
+     
+  def testIPOPTnorm(self):
+    return 
+    self.message("IPOPT min ||x||_2")
+    def norm_2(mx):
+      return sqrt(inner_prod(mx,mx))
+    N=10
+    x=MX("x",N)
+    x0=linspace(0,1,N)
+    X0=MX(x0)
+    f=MXFunction([x],[norm_2(x-X0)])
+    g=MXFunction([x],[2*x])
+    solver = IpoptSolver(f,g)
+    solver.setOption("tol",1e-5)
+    solver.setOption("hessian_approximation", "limited-memory")
+    solver.setOption("max_iter",103)
+    #solver.setOption("print_level",0)
+    solver.init()
+    solver.input(NLP_LBX).set([-10]*N)
+    solver.input(NLP_UBX).set([10]*N)
+    solver.input(NLP_LBG).set([-10]*N)
+    solver.input(NLP_UBG).set([10]*N)
+    solver.solve()
+    print "residuals"
+    print array(solver.output(NLP_X_OPT)).squeeze()-x0
+    #self.assertAlmostEqual(solver.output(NLP_COST)[0],0,10,"IPOPT")
+    #self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
+    
+  def testIPOPTnormSX(self):
+    self.message("IPOPT min ||x||_2")
+    def norm_2(mx):
+      return inner_prod(mx,mx)
+    N=10
+    x=symbolic("x",N)
+    x0=linspace(0,1,N)
+    X0=DMatrix(x0)
+    print norm_2(x-X0)
+    f=SXFunction([x],[norm_2(x-X0)])
+    #g=SXFunction([x],[2*x])
+    solver = IpoptSolver(f)
+    solver.setOption("tol",1e-5)
+    solver.setOption("hessian_approximation", "limited-memory")
+    solver.setOption("max_iter",103)
+    solver.setOption("derivative_test","first-order")
+    #solver.setOption("print_level",0)
+    solver.init()
+    solver.input(NLP_LBX).set([-10]*N)
+    solver.input(NLP_UBX).set([10]*N)
+    #solver.input(NLP_LBG).set([-10]*N)
+    #solver.input(NLP_UBG).set([10]*N)
+    solver.solve()
+    print "residuals"
+    print array(solver.output(NLP_X_OPT)).squeeze()-x0
+    #self.assertAlmostEqual(solver.output(NLP_COST)[0],0,10,"IPOPT")
+    #self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
+  
   def testIPOPTnoc(self):
     self.message("trivial IPOP, no constraints")
     """ There is an assertion error thrown, but still it works"""
