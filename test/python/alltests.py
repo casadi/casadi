da4aa33fa4001ad06a08465f94d541dbe6c33988
Joris Gillis <joris.gillis42@gmail.com>
2011-04-04 15:15:21 +0000

    Added unittest for sparsity

--- test/python/alltests.py
+++ test/python/alltests.py
@@ -5,6 +5,7 @@ from integration import *
 from ocp import *
 from nlp import *
 from ad import *
+from sparsity import *
 
 if __name__ == '__main__':
     unittest.main()
---
+++ test/python/sparsity.py
@@ -0,0 +1,41 @@
+from casadi import *
+import casadi as c
+from numpy import *
+import unittest
+from types import *
+from helpers import *
+
+class Sparsitytests(casadiTestCase):
+  def test_union(self):
+    self.message("Sparsity union")
+    nza = set([  (0,0),
+             (0,1),
+             (2,0),
+             (3,1)])
+    nzb = set([  (0,2),
+             (0,0),
+             (2,2)])
+    
+    a = CRSSparsity(4,5)
+    for i in nza:
+      a.getNZ(i[0],i[1])
+      
+    b = CRSSparsity(4,5)  
+    for i in nzb:
+      b.getNZ(i[0],i[1])
+      
+    w = IVector()
+    c=a.patternUnion(b,w)
+    self.assertEquals(w.size(),len(nza.union(nzb)))
+    for k in range(w.size()):
+      ind = (c.getRow()[k],c.col(k))
+      if (ind in nza and ind in nzb):
+        self.assertEquals(w[k],0)
+      elif (ind in nza):
+        self.assertEquals(w[k],-1)
+      elif (ind in nzb):
+        self.assertEquals(w[k],1)
+      
+if __name__ == '__main__':
+    unittest.main()
+
