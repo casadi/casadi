1ccc51aeaa9280186e6cbaf99c117a95f7d8e548
Joris Gillis <joris.gillis42@gmail.com>
2011-04-22 19:46:40 +0000

    Unittest infinity bounds on ipopt

--- test/python/nlp.py
+++ test/python/nlp.py
@@ -7,7 +7,7 @@ from helpers import *
 
 class NLPtests(casadiTestCase):
   def testIPOPT(self):
-    self.message("trivial IPOP")
+    self.message("trivial IPOPT")
     x=SX("x")
     f=SXFunction([x],[[(x-1)**2]])
     g=SXFunction([x],[x])
@@ -26,7 +26,27 @@ class NLPtests(casadiTestCase):
     solver.solve()
     self.assertAlmostEqual(solver.output(NLP_COST)[0],0,10,"IPOPT")
     self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
+
+  def testIPOPTinf(self):
+    self.message("trivial IPOPT, infinity bounds")
+    x=SX("x")
+    f=SXFunction([x],[[(x-1)**2]])
+    g=SXFunction([x],[x])
     
+    solver = IpoptSolver(f,g)
+    solver.setOption("tol",1e-5)
+    solver.setOption("hessian_approximation", "limited-memory")
+    solver.setOption("max_iter",100)
+    solver.setOption("print_level",0)
+    solver.setOption("derivative_test","first-order")
+    solver.init()
+    solver.input(NLP_LBX).set([-Inf])
+    solver.input(NLP_UBX).set([Inf])
+    solver.input(NLP_LBG).set([-Inf])
+    solver.input(NLP_UBG).set([Inf])
+    solver.solve()
+    self.assertAlmostEqual(solver.output(NLP_COST)[0],0,10,"IPOPT")
+    self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
     
   def testIPOPTrb(self):
     self.message("IPOPT rosenbrock, limited-memorey hessian approx")
@@ -213,7 +233,7 @@ class NLPtests(casadiTestCase):
     #self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
   
   def testIPOPTnoc(self):
-    self.message("trivial IPOP, no constraints")
+    self.message("trivial IPOPT, no constraints")
     """ There is an assertion error thrown, but still it works"""
     x=SX("x")
     f=SXFunction([x],[(x-1)**2])
@@ -228,7 +248,7 @@ class NLPtests(casadiTestCase):
     self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
     
   def testIPOPTmx(self):
-    self.message("trivial IPOP, using MX")
+    self.message("trivial IPOPT, using MX")
     x=MX("x")
     f=MXFunction([x],[(x-1)**2])
     g=MXFunction([x],[2*x])
@@ -249,7 +269,7 @@ class NLPtests(casadiTestCase):
     self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
     
   def testIPOPTc(self):
-    self.message("trivial IPOP, overconstrained")
+    self.message("trivial IPOPT, overconstrained")
     x=SX("x")
     f=SXFunction([x],[(x-1)**2])
     g=SXFunction([x],[[x,x,x]])
@@ -270,7 +290,7 @@ class NLPtests(casadiTestCase):
     self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
     
   def testIPOPTc2(self):
-    self.message("trivial IPOP, overconstrained")
+    self.message("trivial IPOPT, overconstrained")
     x=SX("x")
     f=SXFunction([x],[(x-1)**2])
     g=SXFunction([x],[[x,x,x+x]])
@@ -291,7 +311,7 @@ class NLPtests(casadiTestCase):
     self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,8,"IPOPT")
     
   def testIPOPTcmx(self):
-    self.message("trivial IPOP, overconstrained, using MX")
+    self.message("trivial IPOPT, overconstrained, using MX")
     x=MX("x")
     f=MXFunction([x],[(x-1)**2])
     g=MXFunction([x],[vertcat([2*x,3*x,4*x])])
@@ -312,7 +332,7 @@ class NLPtests(casadiTestCase):
     self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],1,10,"IPOPT")
 
   def testIPOPTdeg(self):
-    self.message("degenerate optimization IPOP")
+    self.message("degenerate optimization IPOPT")
     x=SX("x")
     y=SX("y")
     f=SXFunction([[x,y]],[0])
@@ -333,7 +353,7 @@ class NLPtests(casadiTestCase):
     self.assertAlmostEqual(solver.output(NLP_X_OPT)[0],solver.output(NLP_X_OPT)[1],10,"IPOPT")
 
   def testIPOPTdegc(self):
-    self.message("degenerate optimization IPOP, overconstrained")
+    self.message("degenerate optimization IPOPT, overconstrained")
     x=SX("x")
     y=SX("y")
     f=SXFunction([[x,y]],[0])
