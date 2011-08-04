5486a37840f9682a5b964586dca22a6a885f9711
Joris Gillis <joris.gillis42@gmail.com>
2011-08-04 09:12:56 +0000

    A 'make full' inside documentation/api-doc will now make sure that the examples and tutorials are up to date.

--- documentation/api-doc/publish.sh
+++ documentation/api-doc/publish.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 echo "This script will synchronize the html doc/folder with http://casadi.sourceforge.net/api/html/"
 echo -n "Please type your sourceforge username:"
 read username
 rsync -avP -e ssh html "$username,casadi@web.sourceforge.net:/home/groups/c/ca/casadi/htdocs/api/"
+rsync -avP -e ssh ../tutorials/python/pdf/ "$username,casadi@web.sourceforge.net:/home/groups/c/ca/casadi/htdocs/tutorials/"
 #rsync -avP -e ssh sphinx/_build/html "$username,casadi@web.sourceforge.net:/home/groups/c/ca/casadi/htdocs/api/python/"
