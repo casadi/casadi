3c7b0c9c095269544da61fcf593c233b091c3661
Joel Andersson <j.a.e.andersson@gmail.com>
2011-08-18 14:22:12 +0000

    added header files which includes the most essential classes

---
+++ casadi/casadi.hpp
@@ -0,0 +1,40 @@
+/*
+ *    This file is part of CasADi.
+ *
+ *    CasADi -- A symbolic framework for dynamic optimization.
+ *    Copyright (C) 2010 by Joel Andersson, Moritz Diehl, K.U.Leuven. All rights reserved.
+ *
+ *    CasADi is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    CasADi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with CasADi; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef CASADI_HPP
+#define CASADI_HPP
+
+// Matrices
+#include "matrix/matrix.hpp"
+#include "matrix/matrix_tools.hpp"
+
+// Scalar expressions
+#include "sx/sx.hpp"
+#include "sx/sx_tools.hpp"
+#include "fx/sx_matrix.hpp"
+
+// Matrix expressions
+#include "mx/mx.hpp"
+#include "mx/mx_tools.hpp"
+#include "fx/mx_matrix.hpp"
+
+#endif //CASADI_HPP
