75146c541ad3c054a0ba8bf3dce55543d48c585f
Joel Andersson <j.a.e.andersson@gmail.com>
2011-10-18 16:32:06 +0000

    fixed doxygen documentation for FlatOCP

--- optimal_control/flat_ocp.hpp
+++ optimal_control/flat_ocp.hpp
@@ -32,51 +32,52 @@ namespace OptimalControl{
 class FlatOCPInternal;
 
 /** \brief A flat OCP representation coupled to an XML file
-  Variables:
-  t:     time
-  s:     implicitly defined states (differential or algebraic)
-  x:     differential states
-  z:     algebraic states
-  q:     quadrature states
-  y:     dependent variables
-  p:     independent parameters
-  u:     control signals
-  
-  Equations
-  fully implicit DAE:       0 == dae(t,s,sdot,x,z,u,p)
-  explicit ODE:          xdot == ode(t,s,x,z,u,p)
-  quadratures:           qdot == qua(t,s,x,z,u,p)
-  algebraic equations:      0 == alg(t,s,x,z,u,p)
-  dependent equations:      y == dep(t,s,x,z,u,p)
-  initial equations:        0 == ieq(t,s,sdot,x,z,u,p)
-
-  Note that when parsed, all dynamic states, differential and algebraic, end up in the category "s" 
-  and all dynamic equations end up in the implicit category "dae". At a later state, the DAE can be
-  reformulated, for example in semi-explicit form, possibly in addition to a set of quadrature states.
-
-  The functions for reformulation is are provided as member functions to this class or as independent
-  functions located in the header file "ocp_tools.hpp".
-
-  Usage skeleton:
-  
-  ** 1. Call constructor with an FMI conformant XML file or pass an empty string ("") to build up the OCP from scratch
-  > FlatOCP ocp(xml_file_name)
-  
-  ** 2. Set options
-  > ocp.setOption(...,...)
-
-  ** 3. Initialize and parse XML
-  > ocp.init()
-
-  ** 4. Modify/add variables, equations, optimization
-  > ...
-  
-  When the optimal control problem is in a suitable form, it is possible to either generate functions
-  for numeric/symbolic evaluation or exporting the OCP formulation into a new FMI conformant XML file.
-  The latter functionality is not yet available.
-
-  \date 2011
-  \author Joel Andersson
+*
+*  <H3>Variables:  </H3>
+*    \f$ t \f$:     time <BR>
+*    \f$ s \f$:     implicitly defined states (differential or algebraic) <BR>
+*   \f$ x \f$:     differential states <BR>
+*    \f$ z \f$:     algebraic states <BR>
+*    \f$ q \f$:     quadrature states <BR>
+*    \f$ y \f$:     dependent variables <BR>
+*    \f$ p \f$:     independent parameters <BR>
+*    \f$ u \f$:     control signals <BR>
+*  
+*  <H3>Equations:  </H3>
+*  fully implicit DAE: \f$ 0 = \text{dae}(t,s,\dot{s},x,z,u,p)  \f$  <BR>
+*  explicit ODE:        \f$  \dot{x} = \text{ode}(t,s,x,z,u,p)       \f$ <BR>
+*  quadratures:         \f$  \dot{q} = \text{qua}(t,s,x,z,u,p)       \f$ <BR>
+*  algebraic equations: \f$     0 = \text{alg}(t,s,x,z,u,p)       \f$ <BR>
+*  dependent equations: \f$     y = \text{dep}(t,s,x,z,u,p)       \f$ <BR>
+*  initial equations:   \f$     0 = \text{ieq}(t,s,\dot{s},x,z,u,p)  \f$ <BR>
+*
+*  Note that when parsed, all dynamic states, differential and algebraic, end up in the category "s" 
+*  and all dynamic equations end up in the implicit category "dae". At a later state, the DAE can be
+*  reformulated, for example in semi-explicit form, possibly in addition to a set of quadrature states.
+*
+*  The functions for reformulation is are provided as member functions to this class or as independent
+*  functions located in the header file "ocp_tools.hpp".
+*
+*  <H3>Usage skeleton:</H3>
+*  
+*  1. Call constructor with an FMI conformant XML file or pass an empty string ("") to build up the OCP from scratch <BR>
+*  > FlatOCP ocp(xml_file_name)
+*  
+*  2. Set options <BR>
+*  > ocp.setOption(...,...)
+*
+*  3. Initialize and parse XML <BR>
+*  > ocp.init()
+*
+*  4. Modify/add variables, equations, optimization <BR>
+*  > ...
+*  
+*  When the optimal control problem is in a suitable form, it is possible to either generate functions
+*  for numeric/symbolic evaluation or exporting the OCP formulation into a new FMI conformant XML file.
+*  The latter functionality is not yet available.
+*
+*  \date 2011
+*  \author Joel Andersson
 */
 class FlatOCP : public OptionsFunctionality{
   public:
@@ -107,38 +108,82 @@ class FlatOCP : public OptionsFunctionality{
     /// Check if the node is pointing to the right type of object
     virtual bool checkNode() const;
 
+    /** @name Variables
+    *  Get all variables of a particular type 
+    */
     //@{
-    /// Variables
-    SX t() const;                   /// Time
-    std::vector<Variable>& s();     /// Implicitly defined states
-    std::vector<Variable>& x();     /// Differential states
-    std::vector<Variable>& z();     /// Algebraic states
-    std::vector<Variable>& q();     /// Quadrature states
-    std::vector<Variable>& y();     /// Dependent variables
-    std::vector<Variable>& p();     /// Independent parameters
-    std::vector<Variable>& u();     /// Control signals
+    /// Time
+    SX t() const;
+    
+    /// Implicitly defined states
+    std::vector<Variable>& s();
+    
+    /// Differential states
+    std::vector<Variable>& x();
+    
+    /// Algebraic states
+    std::vector<Variable>& z();
+    
+    /// Quadrature states
+    std::vector<Variable>& q();
+    
+    /// Dependent variables
+    std::vector<Variable>& y();
+    
+    /// Independent parameters
+    std::vector<Variable>& p();
+    
+    /// Control signals
+    std::vector<Variable>& u();
     //@}
     
     #if 0
+    /** @name Equations
+    *  Get all equations of a particular type 
+    */
     //@{
-    /// Model equations
-    std::vector<SX>& dae();         /// Fully implicit DAE (length == s().size())
-    std::vector<SX>& ode();         /// Explicit ODE  (length == x().size())
-    std::vector<SX>& alg();         /// Algebraic equations (length == z().size())
-    std::vector<SX>& qua();         /// Quadrature states (length == q().size())
-    std::vector<SX>& dep();         /// Dependent equations (length == y().size())
-    std::vector<SX>& ieq();         /// Initial equations (remove?)
+    /// Fully implicit DAE (length == s().size())
+    std::vector<SX>& dae();
+    
+    /// Explicit ODE  (length == x().size())
+    std::vector<SX>& ode();
+    
+    /// Algebraic equations (length == z().size())
+    std::vector<SX>& alg();
+    
+    /// Quadrature states (length == q().size())
+    std::vector<SX>& qua();
+    
+    /// Dependent equations (length == y().size())
+    std::vector<SX>& dep();
+    
+    /// Initial equations (remove?)
+    std::vector<SX>& ieq();
     //@}
     #endif
 
+    /** @name Optimization
+    *  Formulate the dynamic optimization problem. Note that the variable bounds are located inside
+       the respective variable.
+    */
     //@{
-    /// Optimization
-    std::vector<SX>& mterm();       /// Mayer terms in the objective
-    std::vector<SX>& lterm();       /// Lagrange terms in the objective
-    double& t0();                   /// Interval start time
-    double& tf();                   /// Interval final time
-    bool& t0_free();                /// Interval start time is free
-    bool& tf_free();                /// Interval final time is free
+    /// Mayer terms in the objective
+    std::vector<SX>& mterm();
+    
+    /// Lagrange terms in the objective
+    std::vector<SX>& lterm();
+    
+    /// Interval start time
+    double& t0();
+    
+    /// Interval final time
+    double& tf();
+    
+    /// Interval start time is free
+    bool& t0_free();
+    
+    /// Interval final time is free
+    bool& tf_free();
     //@}
 };
 
