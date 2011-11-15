f1430cd49ae76b1229fa83292f3d23dbc13b412c
Joris Gillis <joris.gillis42@gmail.com>
2011-11-15 13:41:29 +0000

    Adding casadi-doc.tex to 'build and upload' system. http://casadi.sourceforge.net/tutorials/cpp/casadi-doc/casadi-doc.html

--- documentation/api-doc/publish.sh
+++ documentation/api-doc/publish.sh
@@ -4,5 +4,6 @@ echo -n "Please type your sourceforge username:"
 read username
 rsync -avP -e ssh html "$username,casadi@web.sourceforge.net:/home/groups/c/ca/casadi/htdocs/api/"
 rsync -avP -e ssh ../tutorials/python/pdf/ "$username,casadi@web.sourceforge.net:/home/groups/c/ca/casadi/htdocs/tutorials/"
+rsync -avP -e ssh ../tutorials/cpp/ "$username,casadi@web.sourceforge.net:/home/groups/c/ca/casadi/htdocs/tutorials/cpp"
 rsync -avP -e ssh ../cheatsheet/*.pdf "$username,casadi@web.sourceforge.net:/home/groups/c/ca/casadi/htdocs/cheatsheets/"
 #rsync -avP -e ssh sphinx/_build/html "$username,casadi@web.sourceforge.net:/home/groups/c/ca/casadi/htdocs/api/python/"
