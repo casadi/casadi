f98ae5b35a7a14fce94b1eca217d1c88fe3f1c78
Joris Gillis <joris.gillis42@gmail.com>
2011-11-21 23:00:49 +0000

    Some Parallelizer example

---
+++ documentation/examples/FX/parallelizer.hpp
@@ -0,0 +1,13 @@
+/** 
+* This example looks at the parallelizer method
+*
+* <a href="FX/parallelizer.pdf">View output (PDF)</a>
+*
+* \example FX/parallelizer.py
+*
+* \sa
+* \code
+* CasADi::Parallelizer();
+* \endcode
+*
+*/
---
+++ documentation/examples/FX/parallelizer.py
@@ -0,0 +1,28 @@
+#! Demonstration of Parallelizer
+#! =============================
+from casadi import *
+
+n = 10
+
+#! Construct a functon that is expensive to evaluate
+x = msym("x",100,100)
+y = msym("y",100,1)
+z = x
+
+for i in range(20):
+  z = mul(x,z)
+
+
+f = MXFunction([x,y],[z])
+f.init()
+
+#! Evaluate this function ten times in parallel
+p = Parallelizer([f]*n)
+p.setOption("parallelization","openmp")
+p.init()
+
+#! Note that the parallelizer MX input/output interface is a repitition of our function's I/O interface
+assert(p.getNumInputs() == n*f.getNumInputs())
+
+p.evaluate(1,1)
+
