bc8421a8b27c1db1be8e6c1be15383305ba82df3
Joris Gillis <joris.gillis42@gmail.com>
2011-11-29 22:42:18 +0000

    experiment: DAE via Lagrange

---
+++ experimental/joris/dae_lagrange.py
@@ -0,0 +1,129 @@
+from casadi import *
+from casadi.tools import *
+
+x = ssym("[x y]",2)
+dx = ssym("[dx dy]",2)
+u = ssym("[u v]",2)
+du = ssym("[du dv]",2)
+ddx = ssym("[ddx ddy]",2)
+
+q = vertcat([x,u])
+dq = vertcat([dx,du])
+ddq = ssym("[ddx,ddy,ddu,ddv]",4)
+
+[m,g,L] = ssym("[m,g,L]",3)
+
+T = 0.5*m*mul(dx.T,dx)     # Kinetic    energy
+V = m*g*x[1]               # Potential  energy
+C = 0.5*(mul(x.T,x)-L**2)  # Constraint energy
+
+lambd = ssym("lambda")     # Constraint multiplier
+
+L = T - V - lambd*C        # Lagrange function
+
+
+
+
+Ldx = jacobian(L,dx)       # partial dL/dx
+
+ # A function to take a time-derivative
+dt = lambda L: mul(jacobian(L,x),dx) + mul(jacobian(L,dx),ddx)  
+
+
+print "L = ", L
+
+print "Some intermediary results:"
+print "Ldx = diff(L,dx)   =   ", Ldx
+print "diff(L,x)          =   ", jacobian(L,x)
+print "diff(Ldx,t)        =   diff(Ldx,x)*dx + diff(Ldx,dx)*ddx"
+print "diff(Ldx,x)        =   ", jacobian(Ldx,x)
+print "diff(Ldx,dx)       =   ", jacobian(Ldx,dx)
+
+print ""
+
+
+print "Finally Lagrange provides us with these equations:"
+
+U = dt(Ldx) - jacobian(L,x).T
+
+print U
+
+print 
+print "But we want a first-order ODE, so we will introduce helper variables " ,u
+
+
+U = vertcat([substitute(U,vertcat([dx,ddx]),vertcat([u,du])),u-dx])
+
+g = C
+
+print " DAE in fully implicit form:"
+print "  0 = U(y,y',z,t)"
+print "  0 = g(y,z,t)"
+print ""
+print " U: ", U
+print " g: ", g
+
+print ""
+print ""
+
+print "DAE index is a concept defined for semi-explicit DAE's:"
+print "  y' = f(y,z,t)"
+print "  0  = g(y,z,t)"
+
+print ""
+print "  We can find f as jacobian(U,y')^-1*U(y,0,z,t)"
+print "   U(y,0,z,t) : " , substitute(U,dq,[0,0,0,0])
+print "   jacobian(U,y') : " ,    jacobian(U,dq)
+print "   jacobian(U,y')^-1 : " , inv(jacobian(U,dq))
+print ""
+f =  -mul(inv(jacobian(U,dq)),substitute(U,dq,[0,0,0,0]))
+
+
+print "This DAE has a y' = f(y,z,t) part:"
+print "f : " , f
+print "g : " , C
+print "y : ", q
+print "z : ", lambd
+
+
+print ""
+print ""
+print "Will determine index of DAE"
+print "Start out with g as before"
+
+g = C
+
+
+ # A function to take a time-derivative
+dt = lambda L: mul(jacobian(L,q),dq) + mul(jacobian(L,dq),ddq)  
+
+
+for i in range(1,4):
+  print " Is it index-%d ?" %  i
+  print " ==================="
+  print "  Is J=jacobian(g,z) invertible?"
+  
+  J = jacobian(g,lambd)
+  print "  J =", J
+  print ""
+  if J.size()==1:
+    print "  Yes. You are finished"
+    break
+  else:
+    print "  No. Find a new g:    g ->    diff(g)  & substitute y' = f(y,z,t) "
+
+    print "  This is diff(g): ", dt(g)
+    g =  substitute(dt(g),dq,f)
+    print "  This is after substitution: ", g
+    
+  print ""
+  
+  
+print ""
+sys = vertcat([U,g])
+print "So we end up with this %s residual: " % (str(sys.shape))
+
+print sys
+
+print "But wait, there's too much symbols in there. This is not a closed system!"
+print "We are screwed"
