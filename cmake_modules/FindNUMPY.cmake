c50dfdb20186a942ee4a137f0d85caca5eb732dd
Joel Andersson <j.a.e.andersson@gmail.com>
2011-12-16 10:43:54 +0000

    Fixed FindNUMPY.cmake for windows

--- cmake_modules/FindNUMPY.cmake
+++ cmake_modules/FindNUMPY.cmake
@@ -1,10 +1,12 @@
 find_package(PythonLibs QUIET)
 find_package(PythonInterp QUIET)
 if(PYTHON_EXECUTABLE AND NOT NUMPY_INCLUDE_DIR)
+  message(STATUS "Python executable is ${PYTHON_EXECUTABLE}")
   exec_program ("${PYTHON_EXECUTABLE}" 
-       ARGS "-c 'import numpy; print numpy.get_include()'"
+       ARGS "-c \"import numpy; print numpy.get_include()\""
        OUTPUT_VARIABLE NUMPY_INCLUDE_DIR
        RETURN_VALUE NUMPY_NOT_FOUND)
+  message(STATUS "numpy.get_include() is ${NUMPY_INCLUDE_DIR}")
 endif(PYTHON_EXECUTABLE AND NOT NUMPY_INCLUDE_DIR)
 
 FIND_PATH(NUMPY_PATH
