eac85ba42bcc723481723456a7fa87c98eb97a49
Joris Gillis <joris.gillis42@gmail.com>
2012-01-09 10:53:12 +0000

    unittests: helper to capture stdout/stderr

--- test/python/helpers.py
+++ test/python/helpers.py
@@ -4,6 +4,42 @@ import unittest
 import sys
 from math import isnan, isinf
 
+
+from StringIO import StringIO
+import sys
+
+class TeeString(StringIO):
+  def __init__(self,stream):
+    StringIO.__init__(self)
+    self.stream = stream
+        
+  def write(self, data):
+    StringIO.write(self,data)
+    self.stream.write(data)
+
+class Stdout():
+    def __init__(self):
+        self.stream = TeeString(sys.stdout)
+
+    def __enter__(self):
+        sys.stdout = self.stream
+        return self.stream
+
+    def __exit__(self, type, value, traceback):
+        sys.stdout = self.stream.stream
+        
+class Stderr():
+    def __init__(self):
+        self.stream = TeeString(sys.stderr)
+
+    def __enter__(self):
+        sys.stderr = self.stream
+        return self.stream
+
+    def __exit__(self, type, value, traceback):
+        sys.stderr = self.stream.stream
+        
+        
 class FunctionPool:
   def __init__(self):
     self.numpyoperators=[]
