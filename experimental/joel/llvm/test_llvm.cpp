8e00553cd6db8cc3bce63368d018b1aa2d2921c7
Joel Andersson <j.a.e.andersson@gmail.com>
2012-02-29 09:08:31 +0000

    llvm stuff

--- experimental/joel/llvm/test_llvm.cpp
+++ experimental/joel/llvm/test_llvm.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+#include "llvm/DerivedTypes.h"
+#include "llvm/LLVMContext.h"
+#include "llvm/Module.h"
+#include "llvm/Analysis/Verifier.h"
+#include "llvm/Support/IRBuilder.h"
+
 using namespace std;
+using namespace llvm;
 
 int main(){
+  LLVMContext &Context = getGlobalContext();
+
+  
+  
   cout << "hello work" << endl;
 
   return 0;
