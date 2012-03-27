6aac4f245d030dc21a1e727b8adc3a570bb8d758
Joel Andersson <j.a.e.andersson@gmail.com>
2012-03-27 11:26:25 +0000

    lifted newton: cleanup

--- experimental/joel/single_shooting_rk4.cpp
+++ experimental/joel/single_shooting_rk4.cpp
@@ -11,7 +11,7 @@
  *
  *    CasADi is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSefcn.  See the GNU
  *    Lesser General Public License for more details.
  *
  *    You should have received a copy of the GNU Lesser General Public
@@ -34,7 +34,7 @@ using namespace CasADi;
 using namespace std;
 
 enum LinIn{LIN_X,LIN_LAM_X,LIN_LAM_HG,LIN_NUM_IN};
-enum LinOut{LIN_HL,LIN_GL,LIN_JHG,LIN_F,LIN_HG,LIN_NUM_OUT};
+enum LinOut{LIN_LHESS,LIN_LGRAD,LIN_GJAC,LIN_GLIN,LIN_OBJ,LIN_CON,LIN_NUM_OUT};
 
 double norm22(const DMatrix& v){
   double ret = 0;
@@ -91,8 +91,14 @@ void liftedNewton(SXFunction &ffcn, SXFunction &gfcn, const DMatrix& x_min, cons
   // Lagrange multipliers for constraints
   SXMatrix lam_hg = vertcat(lam_h,lam_g);
   
+  // Lagrangian function
+  SXMatrix lag = f + inner_prod(lam_x,x);
+  if(!g.empty()) lag += inner_prod(lam_g,g);
+  if(!v.empty()) lag += inner_prod(lam_h,v_def);
+  
   // Gradient of the lagrangian
-  SXMatrix lgrad = gradient(f + inner_prod(lam_x,x) + inner_prod(lam_g,g) + inner_prod(lam_h,v_def),x) - vertcat(SXMatrix::zeros(nu),lam_h);
+  SXMatrix lgrad = gradient(lag,x);
+  if(!v.empty()) lgrad -= vertcat(SXMatrix::zeros(nu),lam_h); // Put here to ensure that lgrad is of the form "h_extended -v_extended"
   makeDense(lgrad);
 
   // Condensed gradient of the Lagrangian
@@ -120,45 +126,50 @@ void liftedNewton(SXFunction &ffcn, SXFunction &gfcn, const DMatrix& x_min, cons
   G_in.push_back(lam_v);
   G_out.push_back(h_extended);
   G_out.push_back(lgrad_u);
-  G_out.push_back(g);
+  G_out.push_back(hg);
+  G_out.push_back(f);
   SXFunction G(G_in,G_out);
   G.init();
   
   // Difference vector d
   SXMatrix d = ssym("d",2*nv);
-  SXMatrix dg = ssym("dgggggggggggg",nv);
+  SXMatrix dg = ssym("dg",nv);
   copy(dg.begin(),dg.end(),d.rbegin());
   
   // Substitute out the v from the h
   SXMatrix d_def = (h_extended+v_extended)-d;
-  SXMatrixVector ex(2);
+  SXMatrixVector ex(3);
   ex[0] = lgrad_u;
-  ex[1] = g;
+  ex[1] = f;
+  ex[2] = g;
   substituteInPlace(v_extended, d_def, ex, false, false);
   SXMatrix lgrad_u_z = ex[0];
-  SXMatrix g_z = ex[1];
-
+  SXMatrix f_z = ex[1];
+  SXMatrix g_z = ex[2];
+  
   // Modified function Z
-  SXMatrixVector Z_in,Z_out;
-  Z_in.push_back(u);
-  Z_in.push_back(d);
-  Z_in.push_back(lam_u);
-  Z_in.push_back(lam_g);
-  Z_in.push_back(lam_v);
-  Z_out.push_back(d_def);
-  Z_out.push_back(lgrad_u_z);
-  Z_out.push_back(g_z);
-  SXFunction Z(Z_in,Z_out);
-  Z.init();
+  enum ZIn{Z_U,Z_D,Z_LAM_U,Z_LAM_G,Z_LAM_V,Z_NUM_IN};
+  SXMatrixVector zfcn_in(Z_NUM_IN);
+  zfcn_in[Z_U] = u;
+  zfcn_in[Z_D] = d;
+  zfcn_in[Z_LAM_U] = lam_u;
+  zfcn_in[Z_LAM_G] = lam_g;
+  zfcn_in[Z_LAM_V] = lam_v;
+  enum ZOut{Z_D_DEF,Z_LGRAD,Z_G,Z_NUM_OUT};
+  SXMatrixVector zfcn_out(Z_NUM_OUT);
+  zfcn_out[Z_D_DEF] = d_def;
+  zfcn_out[Z_LGRAD] = lgrad_u_z;
+  zfcn_out[Z_G] = g_z;
+  SXFunction zfcn(zfcn_in,zfcn_out);
+  zfcn.init();
 
   // Matrix A and B in lifted Newton
-  SXMatrix A = Z.jac(0,0);
-  SXMatrix B1 = Z.jac(0,1);
-  SXMatrix B2 = Z.jac(0,2);
+  SXMatrix B1 = zfcn.jac(0,1);
+  SXMatrix B2 = zfcn.jac(0,2);
 
   // Directional derivative of Z
-  vector<vector<SXMatrix> > Z_fwdSeed(2,Z_in);
-  vector<vector<SXMatrix> > Z_fwdSens(2,Z_out);
+  vector<vector<SXMatrix> > Z_fwdSeed(2,zfcn_in);
+  vector<vector<SXMatrix> > Z_fwdSens(2,zfcn_out);
   vector<vector<SXMatrix> > Z_adjSeed;
   vector<vector<SXMatrix> > Z_adjSens;
   Z_fwdSeed[0][0].setZero();
@@ -171,33 +182,36 @@ void liftedNewton(SXFunction &ffcn, SXFunction &gfcn, const DMatrix& x_min, cons
   SXMatrix du = ssym("du",nu);
   SXMatrix dlam_g = ssym("dlam_g",ng);
   
-  Z_fwdSeed[1][0] = du;
-  Z_fwdSeed[1][1] = -d;
-  Z_fwdSeed[1][2].setZero();
-  Z_fwdSeed[1][3] = dlam_g;
-  Z_fwdSeed[1][4].setZero();
-  Z.eval(Z_in,Z_out,Z_fwdSeed,Z_fwdSens,Z_adjSeed,Z_adjSens,true,false);
-  SXMatrix a = Z_fwdSens[0][0];
-  SXMatrix b1 = lgrad_u_z+Z_fwdSens[0][1]; // mux disappears from Z (constant term)
-  SXMatrix b2 = g_z+Z_fwdSens[0][2];
-  SXMatrixVector AB_out;
-  AB_out.push_back(A);
-  AB_out.push_back(B1);
-  AB_out.push_back(B2);
-  AB_out.push_back(a);
-  AB_out.push_back(b1);
-  AB_out.push_back(b2);
-  AB_out.insert(AB_out.end(),Z_out.begin(),Z_out.end());
-  SXFunction AB(Z_in,AB_out);
-  AB.init();
+  SXMatrix b1 = lgrad_u_z;
+  SXMatrix b2 = g_z;
+  if(!v.empty()){
+    Z_fwdSeed[1][0] = du;
+    Z_fwdSeed[1][1] = -d;
+    Z_fwdSeed[1][2].setZero();
+    Z_fwdSeed[1][3] = dlam_g;
+    Z_fwdSeed[1][4].setZero();
+    zfcn.eval(zfcn_in,zfcn_out,Z_fwdSeed,Z_fwdSens,Z_adjSeed,Z_adjSens,true,false);
+    b1 += Z_fwdSens[0][1];
+    b2 += Z_fwdSens[0][2];
+  }
+  
+  SXMatrixVector lfcn_out(LIN_NUM_OUT);
+  lfcn_out[LIN_LHESS] = B1;
+  lfcn_out[LIN_LGRAD] = b1;
+  lfcn_out[LIN_GJAC] = B2;
+  lfcn_out[LIN_GLIN] = b2;
+  lfcn_out[LIN_OBJ] = f_z;
+  lfcn_out[LIN_CON] = g_z; 
+  lfcn_out.insert(lfcn_out.end(),zfcn_out.begin(),zfcn_out.end());
+  SXFunction lfcn(zfcn_in,lfcn_out);
+  lfcn.init();
   
   // Step expansion
-  SXMatrix e = Z_fwdSens[1][0];
-  SXMatrixVector E_in = Z_in;
-  E_in.push_back(du);
-  E_in.push_back(dlam_g);
-  SXFunction E(E_in,e);
-  E.init();
+  SXMatrixVector efcn_in = zfcn_in;
+  efcn_in.push_back(du);
+  efcn_in.push_back(dlam_g);
+  SXFunction efcn(efcn_in,Z_fwdSens[1][0]);
+  efcn.init();
   
   // Current guess for the primal solution
   DMatrix x_k = x_init;
@@ -217,43 +231,22 @@ void liftedNewton(SXFunction &ffcn, SXFunction &gfcn, const DMatrix& x_min, cons
   copy(lam_hg_k.begin()+nv,lam_hg_k.end(),G.input(3).begin());
   G.evaluate();
   G.getOutput(d_k,0);
-
-  // Hessian of the lagrangian
-  SXFunction lgrad_fcn(x,lgrad);
-  lgrad_fcn.init();
-  SXMatrix lhess = lgrad_fcn.jac(0, 0, false, true);
-  
-  // Jacobian of the constraints
-  SXMatrix hg_jac = jacobian(hg,x);
-  
-  // Function to evaluate the linearization of the NLP, input ...
-  SXMatrixVector lfcn_in(LIN_NUM_IN);
-  lfcn_in[LIN_X] = x;
-  lfcn_in[LIN_LAM_X] = lam_x;
-  lfcn_in[LIN_LAM_HG] = lam_hg;
-
-  // ... output ...
-  SXMatrixVector lfcn_out(LIN_NUM_OUT);
-  lfcn_out[LIN_HL] = lhess;
-  lfcn_out[LIN_GL] = lgrad;
-  lfcn_out[LIN_JHG] = hg_jac;
-  lfcn_out[LIN_F] = f;
-  lfcn_out[LIN_HG] = hg;
-  
-  // .. and the function
-  SXFunction lfcn(lfcn_in,lfcn_out);
-  lfcn.init();
-    
-  // Allocate a QP solver
-  QPSolver qp_solver = qp_solver_creator(lhess.sparsity(),hg_jac.sparsity());
+      
+  // Allocate a QP solver (2)
+  QPSolver qp_solver = qp_solver_creator(B1.sparsity(),B2.sparsity());
   qp_solver.setOption(qp_solver_options);
   qp_solver.init();
 
-  // Allocate a QP solver (2)
-  QPSolver qp_solver2 = qp_solver_creator(B1.sparsity(),B2.sparsity());
-  qp_solver2.setOption(qp_solver_options);
-  qp_solver2.init();
+  // Objective
+  double f_k;
+  
+  // Primal step
+  DMatrix dx_k(x_k.sparsity());
 
+  // Dual step
+  DMatrix dlam_x_k(lam_x_k.sparsity());
+  DMatrix dlam_hg_k(lam_hg_k.sparsity());
+  
   int k=0;
   while(true){
     
@@ -265,123 +258,55 @@ void liftedNewton(SXFunction &ffcn, SXFunction &gfcn, const DMatrix& x_min, cons
 
     G.evaluate();
     G.getOutput(d_k,0);
+    f_k = G.output(3).toScalar();
+    const DMatrix& hg_k = G.output(2);
     
     // Get A_k and Bk
-    AB.setInput(G.input(0),0);
-    AB.setInput(d_k,1);
-    AB.setInput(G.input(2),2);
-    AB.setInput(G.input(3),3);
-    AB.setInput(G.input(4),4);
+    lfcn.setInput(G.input(0),0);
+    lfcn.setInput(d_k,1);
+    lfcn.setInput(G.input(2),2);
+    lfcn.setInput(G.input(3),3);
+    lfcn.setInput(G.input(4),4);
     
-    AB.evaluate();
-    const DMatrix& A_k = AB.output(0);
-    const DMatrix& B1_k = AB.output(1); // NOTE: //mux dissappears (constant term)
-    const DMatrix& B2_k = AB.output(2);
-    const DMatrix& a_k = AB.output(3);
-    const DMatrix& b1_k = AB.output(4);
-    const DMatrix& b2_k = AB.output(5);
+    lfcn.evaluate();
+    const DMatrix& B1_k = lfcn.output(LIN_LHESS);
+    const DMatrix& b1_k = lfcn.output(LIN_LGRAD);
+    const DMatrix& B2_k = lfcn.output(LIN_GJAC);
+    const DMatrix& b2_k = lfcn.output(LIN_GLIN);
 
-    qp_solver2.setInput(B1_k,QP_H);
-    qp_solver2.setInput(b1_k,QP_G);
-    qp_solver2.setInput(B2_k,QP_A);
-    std::transform(x_min.begin(),x_min.begin()+nu,x_k.begin(),qp_solver2.input(QP_LBX).begin(),std::minus<double>());
-    std::transform(x_max.begin(),x_max.begin()+nu,x_k.begin(),qp_solver2.input(QP_UBX).begin(),std::minus<double>());
-    std::transform(g_min.begin()+nv,g_min.end(), b2_k.begin(),qp_solver2.input(QP_LBA).begin(),std::minus<double>());
-    std::transform(g_max.begin()+nv,g_max.end(), b2_k.begin(),qp_solver2.input(QP_UBA).begin(),std::minus<double>());
+    qp_solver.setInput(B1_k,QP_H);
+    qp_solver.setInput(b1_k,QP_G);
+    qp_solver.setInput(B2_k,QP_A);
+    std::transform(x_min.begin(),x_min.begin()+nu,x_k.begin(),qp_solver.input(QP_LBX).begin(),std::minus<double>());
+    std::transform(x_max.begin(),x_max.begin()+nu,x_k.begin(),qp_solver.input(QP_UBX).begin(),std::minus<double>());
+    std::transform(g_min.begin()+nv,g_min.end(), b2_k.begin(),qp_solver.input(QP_LBA).begin(),std::minus<double>());
+    std::transform(g_max.begin()+nv,g_max.end(), b2_k.begin(),qp_solver.input(QP_UBA).begin(),std::minus<double>());
     
-    qp_solver2.evaluate();
-    const DMatrix& du_k = qp_solver2.output(QP_PRIMAL);
-    DMatrix dlam_u_k = qp_solver2.output(QP_LAMBDA_X);
-    DMatrix dlam_g_k = qp_solver2.output(QP_LAMBDA_A);
-    
-    // Get temporary vectors of the right dimensions
-    DMatrix dx_k1 = qp_solver.output(QP_PRIMAL);
-    dx_k1.setAll(numeric_limits<double>::quiet_NaN());
-
-    DMatrix dlam_x_k1 = qp_solver.output(QP_LAMBDA_X);
-    dlam_x_k1.setZero();
-    
-    DMatrix dlam_hg_k1 = qp_solver.output(QP_LAMBDA_A);
-    dlam_hg_k1.setAll(numeric_limits<double>::quiet_NaN());
-    
+    qp_solver.evaluate();
+    const DMatrix& du_k = qp_solver.output(QP_PRIMAL);
+    const DMatrix& dlam_u_k = qp_solver.output(QP_LAMBDA_X);
+    const DMatrix& dlam_g_k = qp_solver.output(QP_LAMBDA_A);
+        
     // Expand the step
-    E.setInput(AB.input(0),0);
-    E.setInput(AB.input(1),1);
-    E.setInput(AB.input(2),2);
-    E.setInput(AB.input(3),3);
+    efcn.setInput(lfcn.input(0),0);
+    efcn.setInput(lfcn.input(1),1);
+    efcn.setInput(lfcn.input(2),2);
+    efcn.setInput(lfcn.input(3),3);
     
-    E.setInput(AB.input(4),4);
-    E.setInput(du_k,5);
-    E.setInput(dlam_g_k,6);
-    E.evaluate();
-    const DMatrix& dv_k = E.output();
+    efcn.setInput(lfcn.input(4),4);
+    efcn.setInput(du_k,5);
+    efcn.setInput(dlam_g_k,6);
+    efcn.evaluate();
+    const DMatrix& dv_k = efcn.output();
     
     // Expanded primal step
-    copy(du_k.begin(),du_k.end(),dx_k1.begin());
-    copy(dv_k.begin(),dv_k.begin()+nv,dx_k1.begin()+nu);
+    copy(du_k.begin(),du_k.end(),dx_k.begin());
+    copy(dv_k.begin(),dv_k.begin()+nv,dx_k.begin()+nu);
 
-    // Expanded dual step (simple bounds)
-    copy(dlam_u_k.begin(),dlam_u_k.end(),dlam_x_k1.begin());
-    
-    // Expanded dual step (nonlinear bounds)
-    copy(dlam_g_k.begin(),dlam_g_k.end(),dlam_hg_k1.begin()+nv);
-    copy(dv_k.rbegin(),dv_k.rbegin()+nv,dlam_hg_k1.begin());
-
-    bool show_dx = false;
-    bool show_dlam_x = false;
-    bool show_dlam_hg = false;
-
-    if(show_dx){
-      cout << "dx_k1 = " << dx_k1 << endl;
-    }
-
-    if(show_dlam_x){
-      cout << "dlam_x_k1 = " << dlam_x_k1 << endl;
-    }
-      
-    
-    if(show_dlam_hg){
-      cout << "dlam_hg_k1 = " << dlam_hg_k1 << endl;
-    }
-    
-    // Evaluate functions
-    lfcn.setInput(x_k,LIN_X);
-    lfcn.setInput(lam_x_k,LIN_LAM_X);
-    lfcn.setInput(lam_hg_k,LIN_LAM_HG);
-    lfcn.evaluate();
-    const DMatrix& lhess_k = lfcn.output(LIN_HL);
-    const DMatrix& lgrad_k = lfcn.output(LIN_GL);
-    const DMatrix& hg_jac_k = lfcn.output(LIN_JHG);
-    double f_k = lfcn.output(LIN_F).toScalar();
-    const DMatrix& hg_k = lfcn.output(LIN_HG);
-    
-    // Solve QP
-    qp_solver.setInput(lhess_k,QP_H);
-    qp_solver.setInput(lgrad_k,QP_G);
-    qp_solver.setInput(hg_jac_k,QP_A);
-    qp_solver.setInput(x_min-x_k,QP_LBX);
-    qp_solver.setInput(x_max-x_k,QP_UBX);
-    qp_solver.setInput(g_min-hg_k,QP_LBA);
-    qp_solver.setInput(g_max-hg_k,QP_UBA);
-    qp_solver.evaluate();
-    DMatrix dx_k = qp_solver.output(QP_PRIMAL);
-    DMatrix dlam_x_k = qp_solver.output(QP_LAMBDA_X);
-    DMatrix dlam_hg_k = qp_solver.output(QP_LAMBDA_A);
-
-    
-//     dx_k = dx_k1;
-//     dlam_x_k = dlam_x_k1;
-//     dlam_hg_k = dlam_hg_k1;
-    
-    if(show_dx){
-      cout << "dx_k2 = " << dx_k << endl;
-    }
-    if(show_dlam_x){
-      cout << "dlam_x_k2 = " << dlam_x_k << endl;
-    }
-    if(show_dlam_hg){
-      cout << "dlam_hg_k2 = " << dlam_hg_k << endl;
-    }
+    // Expanded dual step
+    copy(dlam_u_k.begin(),dlam_u_k.end(),dlam_x_k.begin());
+    copy(dlam_g_k.begin(),dlam_g_k.end(),dlam_hg_k.begin()+nv);
+    copy(dv_k.rbegin(),dv_k.rbegin()+nv,dlam_hg_k.begin());
     
     // Take a full step
     x_k += dx_k;
@@ -423,7 +348,6 @@ void liftedNewton(SXFunction &ffcn, SXFunction &gfcn, const DMatrix& x_min, cons
     }
   }
   
-  double f_k = lfcn.output(LIN_F).toScalar();
   cout << "optimal cost:    " << f_k << endl;
   cout << "optimal control: " << x_k << endl;
   cout << "multipliers (x): " << lam_x_k << endl;
@@ -433,15 +357,7 @@ void liftedNewton(SXFunction &ffcn, SXFunction &gfcn, const DMatrix& x_min, cons
 int main(){
     
   cout << "program started" << endl;
-  
     
-  
-  // Automatic initialization
-  bool manual_init = true;
-
-  // Use the Gauss-Newton method
-  bool gauss_newton = false;
-  
   // QP-solver
   QPSolverCreator qp_solver_creator = Interfaces::QPOasesSolver::creator;
   Dictionary qp_solver_options;
@@ -596,11 +512,11 @@ int main(){
 //   cout << "I: multipliers (u): " << ipopt.output(NLP_LAMBDA_X) << endl;
 //   cout << "I: multipliers (g): " << ipopt.output(NLP_LAMBDA_G) << endl;
   
-  // Solve full-space problem
+  // Solve lifted problem
   liftedNewton(ffcn_full,gfcn_full,xv_min,xv_max,xv_init,gv_min,gv_max,v.size(),qp_solver_creator,qp_solver_options);
 
-  // Solve single-shooting problem
-  //liftedNewton(ffcn,gfcn,u_min,u_max,u_init,g_min,g_max,0,qp_solver_creator,qp_solver_options);
+  // Solve full-space problem
+  liftedNewton(ffcn_full,gfcn_full,xv_min,xv_max,xv_init,gv_min,gv_max,0,qp_solver_creator,qp_solver_options);
   
   
   return 0;
