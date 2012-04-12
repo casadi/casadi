60dc921b235c9c0dd99fe25af5a9eea2794979bc
Joel Andersson <j.a.e.andersson@gmail.com>
2012-04-12 09:13:14 +0000

    added test script for parameters in modelica models

---
+++ experimental/joel/modelica/parameters.py
@@ -0,0 +1,44 @@
+import casadi
+ocp = casadi.FlatOCP('modelDescription.xml')
+ocp.init()
+
+x = ocp.variable('x')
+x_start = ocp.variable('x_start')
+u = ocp.variable('u')
+u_cost = ocp.variable('u_cost')
+
+print(x_start.getStart()) # 1.0
+print(u_cost.getStart()) # 2.0?
+print(u.getMax()) # 0.2
+x_start.setStart(2)
+print(x_start.getStart()) # 2.0
+print(x.getStart()) # 2.0
+print(u.getMax()) # 0.4
+print(u_cost.getStart()) # 4.0
+u_cost.setStart(3) # Error since u_cost is a dependent parameter?
+x.setStart(4) # Not sure what we want here! What should happen if x_start changes value afterwards?
+
+# JModelica
+from pyjmi import CasadiModel
+from pymodelica import compile_fmux
+jn = compile_fmux("Parameters", "parameters.mop")
+model = CasadiModel(jn)
+import matplotlib.pyplot as plt
+res = model.optimize()
+x = res['x']
+u = res['u']
+time = res['time']
+plt.figure(1)
+plt.clf()
+plt.subplot(2, 1, 1)
+plt.plot(time, x)
+plt.xlabel('$t$')
+plt.ylabel('$x$')
+plt.grid(True)
+
+plt.subplot(2, 1, 2)
+plt.plot(time, u)
+plt.grid(True)
+plt.xlabel('$t$')
+plt.ylabel('$u$')
+plt.show()
