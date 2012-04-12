8f1b00a533a90b0b7db858897c2f36fd8cdae091
Joris Gillis <joris.gillis42@gmail.com>
2012-04-12 11:50:13 +0000

    testsuite: octave listens to CASADILIBDIR

--- test/internal/test_oct.py
+++ test/internal/test_oct.py
@@ -1,12 +1,17 @@
+
 import sys
 src = sys.argv[1]
 import os
 
 from testsuite import TestSuite
 
+path = os.getcwd() + '/../build/lib'
+if 'CASADILIBDIR' in sys.env:
+  path = sys.env['CASADILIBDIR']
+
 t = TestSuite(dirname=src,
   suffix="m",
-  command = lambda dir,fn:  ["octave",'--no-init-file','-p', os.getcwd() + '/../build/lib', fn],
+  command = lambda dir,fn:  ["octave",'--no-init-file','-p', path, fn],
   skipdirs=[".svn","ctemplate"],
   allowable_returncodes=[127],
   stderr_trigger = ["error"],
