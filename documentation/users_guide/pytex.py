7a9b0a9fc043a7f73a68d3caf276cd324c8dd9fa
Joris Gillis <joris.gillis42@gmail.com>
2012-05-07 21:48:07 +0000

    user_guide faulty repairing faulty commit. Had commited symbolic links

--- documentation/users_guide/pytex.py
+++ documentation/users_guide/pytex.py
@@ -1,1 +1,37 @@
-/home/jg/tools/latex-python-transclusion/pytex.py
+import glob
+import subprocess
+import os
+
+# Process all snippets in the current directory
+for f in glob.glob("pytex_*.py"):
+  logfilename = f[:-3]+'.log'
+  logfile = file(logfilename,'w')
+  
+  # Execute snippet
+  p = subprocess.Popen(['python',f],stdout=logfile)
+  p.wait()
+  
+  logfile = file(logfilename,'r')
+  
+  outfile = False
+  contents = False
+  for l in logfile.readlines():
+    if l.startswith('pytex snippet::'):
+      num=l[len('pytex snippet::'):].rstrip()
+      
+      # Remove logfiles that are empty
+      if outfile and not(contents):
+        outfile.close()
+        os.remove(outfile.name)
+    
+      outfile = file(f[:-3]+'_' + num + '.log','w')
+      contents = False
+    else:
+      if outfile:
+        contents = True
+        outfile.write(l)
+  os.remove(logfilename)
+  # Remove logfiles that are empty
+  if outfile and not(contents):
+    outfile.close()
+    os.remove(outfile.name)
