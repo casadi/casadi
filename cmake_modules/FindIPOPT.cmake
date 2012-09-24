73641a7edf8a7fc2c1bdfdadc97cccd6d08cacc6
Joel Andersson <j.a.e.andersson@gmail.com>
2012-09-24 18:46:03 +0000

    Added finding sIPOPT library in FindIPOPT.cmake, #447

--- cmake_modules/FindIPOPT.cmake
+++ cmake_modules/FindIPOPT.cmake
@@ -6,16 +6,28 @@ if(NEW_FIND_IPOPT)
   pkg_search_module(IPOPT ipopt)
   
   # Callback support
-  if(EXISTS ${IPOPT_INCLUDEDIR}/IpIpoptData.hpp AND EXISTS ${IPOPT_INCLUDEDIR}/IpOrigIpoptNLP.hpp AND EXISTS ${IPOPT_INCLUDEDIR}IpTNLPAdapter.hpp  AND EXISTS ${IPOPT_INCLUDEDIR}/IpDenseVector.hpp AND EXISTS ${IPOPT_INCLUDEDIR}/IpExpansionMatrix.hpp)
-    set(IPOPT_FOUND_EXTRA TRUE)
-  else ()
-    message(STATUS "Detected an ipopt configuration without development headers. Build will proceed, but without callback functionality. To enable it, see https://sourceforge.net/apps/trac/casadi/wiki/enableIpoptCallback")
-  endif ()
+  if(IPOPT_INCLUDEDIR)
+    if(EXISTS ${IPOPT_INCLUDEDIR}/IpIpoptData.hpp AND EXISTS ${IPOPT_INCLUDEDIR}/IpOrigIpoptNLP.hpp AND EXISTS ${IPOPT_INCLUDEDIR}IpTNLPAdapter.hpp  AND EXISTS ${IPOPT_INCLUDEDIR}/IpDenseVector.hpp AND EXISTS ${IPOPT_INCLUDEDIR}/IpExpansionMatrix.hpp)
+      set(IPOPT_FOUND_EXTRA TRUE)
+    else ()
+      message(STATUS "Detected an IPOPT configuration without development headers. Build will proceed, but without callback functionality. To enable it, see https://sourceforge.net/apps/trac/casadi/wiki/enableIpoptCallback")
+    endif ()
+  endif(IPOPT_INCLUDEDIR)
+  
+  # Find sIPOPT library and add this to the list of libraries
+  if(IPOPT_LIBRARIES)
+    find_library(SIPOPT_LIBRARY sipopt HINTS ${IPOPT_LIBDIR})
+    if(SIPOPT_LIBRARY)
+#       set(IPOPT_LIBRARIES "${SIPOPT_LIBRARY} ${IPOPT_LIBRARIES}")
+      set(WITH_SIPOPT TRUE)
+    else ()
+      message(STATUS "Detected an IPOPT configuration without support for parametric sensitivities (sIPOPT). Build will proceed, but without sIPOPT functionality.")
+    endif ()
+  endif(IPOPT_LIBRARIES)
   
   # Set standard flags
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(IPOPT DEFAULT_MSG IPOPT_LIBRARIES IPOPT_INCLUDE_DIRS)
-  mark_as_advanced(IPOPT_LIBRARIES IPOPT_INCLUDE_DIRS)
   
   # Add def (temporary solution)
   add_definitions(-DNEW_FIND_IPOPT)
