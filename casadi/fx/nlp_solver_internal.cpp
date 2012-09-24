c066fd04187a65aaf9e2c58888ec4a68be965aa3
Joris Gillis <joris.gillis42@gmail.com>
2012-09-24 12:22:50 +0000

    Correcting mistake in error message for nlp_solver

--- casadi/fx/nlp_solver_internal.cpp
+++ casadi/fx/nlp_solver_internal.cpp
@@ -419,7 +419,7 @@ void NLPSolverInternal::init(){
      for (int i=0;i<NLP_NUM_OUT;i++) {
        casadi_assert_message(callback_.input(i).sparsity()==output(i).sparsity(),
          "Callback function should have the output scheme of NLPSolver as input scheme. " << 
-         "Input #" << i << " (" << getSchemeEntryEnumName(SCHEME_NLPInput,i) <<  " aka '" << getSchemeEntryName(SCHEME_NLPInput,i) << "') was found to be " << callback_.input(i).dimString() << " instead of expected " << output(i).dimString() << "."
+         "Input #" << i << " (" << getSchemeEntryEnumName(SCHEME_NLPOutput,i) <<  " aka '" << getSchemeEntryName(SCHEME_NLPOutput,i) << "') was found to be " << callback_.input(i).dimString() << " instead of expected " << output(i).dimString() << "."
        );
        callback_.input(i).setAll(0);
      }
