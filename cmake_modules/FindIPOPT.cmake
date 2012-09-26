af383be52b562ab7df96ee053c8f397225afc463
Greg Horn <gregmainland@gmail.com>
2012-09-26 12:16:02 +0000

    FindIPOPT: parse IPOPT_LDFLAGS_OTHER and add osx frameworks to IPOPT_LIBRARIES

--- cmake_modules/FindIPOPT.cmake
+++ cmake_modules/FindIPOPT.cmake
@@ -2,6 +2,20 @@
 find_package(PkgConfig)
 pkg_search_module(IPOPT ipopt)
 
+# add osx frameworks to IPOPT_LIBRARIES
+if(IPOPT_LIBRARIES)
+  if(APPLE)
+    # turn "-framework;foo;-framework;bar;other" into "-framework foo;-framework bar;other"
+    STRING(REPLACE "-framework;" "-framework " IPOPT_LDFLAGS_OTHER "${IPOPT_LDFLAGS_OTHER}")
+    # take "-framework foo;-framework bar;other" and add only frameworks to IPOPT_LIBRARIES
+    foreach(arg ${IPOPT_LDFLAGS_OTHER})
+      if("${arg}" MATCHES "-framework .+")
+        SET(IPOPT_LIBRARIES "${IPOPT_LIBRARIES};${arg}")
+      endif("${arg}" MATCHES "-framework .+")
+    endforeach(arg ${IPOPT_LDFLAGS_OTHER})
+  endif(APPLE)
+endif(IPOPT_LIBRARIES)
+
 # Callback support
 if(IPOPT_INCLUDEDIR)
   if(EXISTS ${IPOPT_INCLUDEDIR}/IpIpoptData.hpp AND EXISTS ${IPOPT_INCLUDEDIR}/IpOrigIpoptNLP.hpp AND EXISTS ${IPOPT_INCLUDEDIR}/IpTNLPAdapter.hpp  AND EXISTS ${IPOPT_INCLUDEDIR}/IpDenseVector.hpp AND EXISTS ${IPOPT_INCLUDEDIR}/IpExpansionMatrix.hpp)
