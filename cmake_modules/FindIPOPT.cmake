ce7fa0bb3da66b3aca82ffd572d8ad2db234fdc9
Joris Gillis <joris.gillis42@gmail.com>
2012-09-27 17:06:26 +0000

    Fixing the warnings that where recently introduced in the build system

--- cmake_modules/FindIPOPT.cmake
+++ cmake_modules/FindIPOPT.cmake
@@ -2,6 +2,21 @@
 find_package(PkgConfig)
 pkg_search_module(IPOPT ipopt)
 
+# The following code canonicalizes paths in IPOPT_LIBRARY_DIRS:
+# e.g.  converts "foo/bar/..;foo/../foo"   to "foo;foo"
+# When a library is present in "foo" directory, "foo/bar/..;foo/../foo" would make CMake
+#  complain about this library being present in two different folders, 
+#  which would be a recipe for trouble where it not for
+#  the fact that these directories are really identical.
+#  It could be considered a workaround for a bug in CMake.
+set(IPOPT_LIBRARY_DIRS_BACKUP ${IPOPT_LIBRARY_DIRS})
+set(IPOPT_LIBRARY_DIRS "")
+
+foreach(arg ${IPOPT_LIBRARY_DIRS_BACKUP})
+  get_filename_component(CANONICAL_PATH ${arg} REALPATH)
+  SET(IPOPT_LIBRARY_DIRS ${IPOPT_LIBRARY_DIRS} "${CANONICAL_PATH}")
+endforeach()
+
 # add osx frameworks to IPOPT_LIBRARIES
 if(IPOPT_LIBRARIES)
   if(APPLE)
