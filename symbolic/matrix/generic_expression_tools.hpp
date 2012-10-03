bab119c8d774002627670e47691bc5e3fdd43464
Joris Gillis <joris.gillis42@gmail.com>
2012-10-03 18:12:17 +0000

    forgot to add one file

---
+++ symbolic/matrix/generic_expression_tools.hpp
@@ -0,0 +1,72 @@
+/*
+ *    This file is part of CasADi.
+ *
+ *    CasADi -- A symbolic framework for dynamic optimization.
+ *    Copyright (C) 2010 by Joel Andersson, Moritz Diehl, K.U.Leuven. All rights reserved.
+ *
+ *    CasADi is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    CasADi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with CasADi; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef GENERIC_EXPRESSION_TOOLS_HPP
+#define GENERIC_EXPRESSION_TOOLS_HPP
+
+#include "../casadi_math.hpp"
+
+namespace CasADi{
+
+//@ {
+/** \brief  Conditional operators */
+template<class T>
+T logic_and(const T &a, const T &b);
+
+template<class T>
+T logic_or(const T &a, const T &b);
+
+template<class T>
+T logic_not(const T &a);
+
+#ifndef SWIG
+// Implementations
+
+template<class T>
+T logic_and(const T &a, const T &b) { return a+b>=2; }
+
+template<class T>
+T logic_or(const T &a, const T &b) { return  logic_not(logic_and(logic_not(a),logic_not(b))); }
+
+template<class T>
+T logic_not(const T &a) { return  1-a; }
+
+#endif // SWIG
+
+} // namespace CasADi
+
+#ifdef SWIG
+
+// map the template name to the instantiated name
+#define GET_INST(T,function_name) \
+%template(function_name) CasADi::function_name < T >;
+
+// Define template instanciations
+#define GENERIC_EXPRESSION_TOOLS_TEMPLATES(T) \
+GET_INST(T,logic_and) \
+GET_INST(T,logic_or) \
+GET_INST(T,logic_not) \
+
+#endif //SWIG
+
+#endif // GENERIC_EXPRESSION_TOOLS_HPP
+
