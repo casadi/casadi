644c931c755a48634f01eddb1720d1cca2068b7b
Joris Gillis <joris.gillis42@gmail.com>
2012-10-17 12:14:48 +0000

    sphinx update

--- documentation/api-doc/sphinx/conf.py
+++ documentation/api-doc/sphinx/conf.py
@@ -37,7 +37,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append('../../build/swig/python/casadi')
+sys.path.append('../../build/python/casadi')
 
 # -- General configuration -----------------------------------------------------
 
