1a3baefa996dfce82dbfcb911da7d9e6de6111d5
Joris Gillis <joris.gillis42@gmail.com>
2012-11-26 19:34:40 +0100

    Remove feature [:,0] on Collection. This complicates traversal

--- test/python/tools.py
+++ test/python/tools.py
@@ -80,14 +80,14 @@ class Toolstests(casadiTestCase):
 
       self.checkarray(p.i_y,[[1,2],[3,4],[5,6]],"")
       
-      self.checkarray(p.i_y[:,0],[1,3,5],"")
-      self.checkarray(p.i_y[:,1],[2,4,6],"")
+      #self.checkarray(p.i_y[:,0],[1,3,5],"")
+      #self.checkarray(p.i_y[:,1],[2,4,6],"")
       
-      self.checkarray(p.i_y[0,:],[1,2],"")
-      self.checkarray(p.i_y[1,:],[3,4],"")
+      #self.checkarray(p.i_y[0,:],[1,2],"")
+      #self.checkarray(p.i_y[1,:],[3,4],"")
       
-      self.checkarray(p._i["y",0],[1,2],"")
-      self.checkarray(p._i["y",:,0],[1,3,5],"")
+      #self.checkarray(p._i["y",0],[1,2],"")
+      #self.checkarray(p._i["y",:,0],[1,3,5],"")
       
       p = Collection()
       p.x = [ssym("x")]
