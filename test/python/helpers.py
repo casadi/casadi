58117d59cda636baa40223027907af32bd7b44a5
Joris Gillis <joris.gillis42@gmail.com>
2012-11-26 23:18:21 +0100

    more work on checkfx unittest helper

--- test/python/helpers.py
+++ test/python/helpers.py
@@ -200,23 +200,32 @@ class casadiTestCase(unittest.TestCase):
     for i in range(len(pool.numpyoperators)):
       self.numpyEvaluationCheck(pool.casadioperators[i],pool.numpyoperators[i],x,x0,"%s:%s" % (name,pool.names[i]),"\n I tried to apply %s (%s) from test case '%s' to numerical value %s. But the result returned: " % (str(pool.casadioperators[i]),pool.names[i],name, str(x0)),fmod=fmod,setx0=setx0)
 
-  def checkfx(self,trial,solution,fwd=True,adj=True,digits=9,failmessage="",allow_empty=True,verbose=True):
-    
-    try:
-      trial.evaluate(fwd,adj)
-      solution.evaluate(fwd,adj)
-    except Exception as e:
-      raise Exception(str(e) + "\nThis occured for simple evaluate(%d,%d) for: %s" % (fwd,adj,failmessage) )
-      
-    self.assertEqual(trial.getNumOutputs(),solution.getNumOutputs(),failmessage+": trial has %d outputs while solution has %d." % (trial.getNumOutputs(),solution.getNumOutputs()) )
-    self.assertEqual(trial.getNumInputs(),solution.getNumInputs(),failmessage+": trial has %d inputs while solution has %d." % (trial.getNumInputs(),solution.getNumInputs()) )
+  def checkfx(self,trial,solution,fwd=True,adj=True,jac=True,digits=9,failmessage="",allow_empty=True,verbose=True):
 
+     
     for i in range(trial.getNumInputs()):
       if (allow_empty and (trial.input(i).empty() or solution.input(i).empty() )): continue
       message = "input(%d)" % i
       if verbose: print message + ": " + str(trial.input(i))
       self.checkarray(trial.input(i),solution.input(i),"",digits=digits,failmessage=failmessage+": "+ message)
-    
+
+    trial_inputs    = [ DMatrix(trial.input(k)) for k in range(trial.getNumInputs())]
+    solution_inputs = [ DMatrix(solution.input(k)) for k in range(solution.getNumInputs())] 
+        
+    try:
+      trial.evaluate(fwd,adj)
+      solution.evaluate(fwd,adj)
+    except Exception as e:
+      raise Exception(str(e) + "\nThis occured for simple evaluate(%d,%d) for: %s" % (fwd,adj,failmessage) )
+
+    for i in range(trial.getNumInputs()):
+      message = "input(%d) modified by evaluate" % i
+      self.checkarray(trial.input(i),trial_inputs[i],"",digits=digits,failmessage=failmessage+": "+ message)
+      self.checkarray(solution.input(i),solution_inputs[i],"",digits=digits,failmessage=failmessage+": "+ message)
+
+    self.assertEqual(trial.getNumOutputs(),solution.getNumOutputs(),failmessage+": trial has %d outputs while solution has %d." % (trial.getNumOutputs(),solution.getNumOutputs()) )
+    self.assertEqual(trial.getNumInputs(),solution.getNumInputs(),failmessage+": trial has %d inputs while solution has %d." % (trial.getNumInputs(),solution.getNumInputs()) )
+
     for i in range(trial.getNumOutputs()):
       message = "output(%d)" % i
       if verbose: print message + ": " + str(trial.output(i))
@@ -260,7 +269,7 @@ class casadiTestCase(unittest.TestCase):
             trial.evaluate(fwd,adj)
             solution.evaluate(fwd,adj)
           except Exception as e:
-            raise Exception(e.str() + "\nThis occured for simple evaluate(%d,%d) for adjSeed(%d)[%d]=1 for:" % (fwd,adj,i,j,failmessage) )
+            raise Exception(e.str() + "\nThis occured for simple evaluate(%d,%d) for adjSeed(%d)[%d]=1 for: %s" % (fwd,adj,i,j,failmessage) )
 
           for k in range(trial.getNumInputs()):
             if (allow_empty and (trial.input(k).empty() or solution.input(k).empty() )): continue
@@ -270,3 +279,22 @@ class casadiTestCase(unittest.TestCase):
             
           trial.adjSeed(i).setAll(0)
           solution.adjSeed(i).setAll(0)
+
+    for i in range(trial.getNumInputs()):
+      message = "input(%d) modified by evaluate" % i
+      self.checkarray(trial.input(i),trial_inputs[i],"",digits=digits,failmessage=failmessage+": "+ message)
+      self.checkarray(solution.input(i),solution_inputs[i],"",digits=digits,failmessage=failmessage+": "+ message)
+    
+    return # disable jac for now
+    if jac:
+      for i in range(trial.getNumInputs()):
+        if (allow_empty and (trial.input(i).empty() or solution.input(i).empty() )): continue
+        for j in range(trial.getNumOutputs()):
+          trialjac = trial.jacobian(i,j)
+          trialjac.init()
+          for k in range(trial.getNumInputs()): trialjac.input(k).set(trial_inputs[k])
+          solutionjac = solution.jacobian(i,j)
+          solutionjac.init()
+          for k in range(solution.getNumInputs()): solutionjac.input(k).set(solution_inputs[k])
+          
+          self.checkfx(trialjac,solutionjac,fwd=False,adj=False,jac=False,digits=digits,failmessage="(%s).jacobian(%d,%d)" % (failmessage,i,j),allow_empty=verbose,verbose=verbose)
