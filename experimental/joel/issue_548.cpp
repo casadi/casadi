de001eb855b789e334df894b68bd649db893fe9e
Joel Andersson <j.a.e.andersson@gmail.com>
2012-12-12 14:33:07 -1000

    Simplified failing code for #548

--- experimental/joel/issue_548.cpp
+++ experimental/joel/issue_548.cpp
@@ -5,11 +5,13 @@ using namespace CasADi;
 using namespace std;
 
 int main(){
-  int nEnd = 1000;
-  for(int dIn=1; dIn<nEnd; ++dIn){
+  int nBegin = 65;
+  int nEnd = 66;
+  for(int dIn=nBegin; dIn<nEnd; ++dIn){
     cout << "trying "<< dIn << "/" << nEnd << "inputs: " << (100.0*dIn)/nEnd << "\%" << endl;
     SXMatrix xIn = ssym("x",dIn);
     FX f = SXFunction(xIn,pow(mul(trans(xIn),SXMatrix::ones(dIn)),2.));
+    f.setOption("verbose",true);
     f.init();
     FX h = f.hessian();
   }
