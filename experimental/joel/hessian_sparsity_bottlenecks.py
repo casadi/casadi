97b93f792134e6b77e88267c6693d90b3f4d2a47
Joel Andersson <j.a.e.andersson@gmail.com>
2013-01-21 08:28:50 +0100

    Added note on properties of the Hessian sparsity pattern in the hessian_sparsity_bottlecks.py example

--- experimental/joel/hessian_sparsity_bottlenecks.py
+++ experimental/joel/hessian_sparsity_bottlenecks.py
@@ -14,3 +14,19 @@ hfcn = fcn.hessian()
 # Visualize the sparsity pattern
 #hfcn.init()
 #hfcn.output().sparsity().spy()
+
+
+# Note about the bipartitive graph defined by the Hessian sparsity pattern:
+#
+# First row has:
+#   O(n) distance-1 neighbors
+#   O(n^2) distance-2 neighbors
+#   O(n^3) distance-3 neighbors
+#
+# Other rows have:
+#   O(1) distance-1 neighbor
+#   O(n) distance-2 neighbors
+#   O(n^2) distance-3 neighbors
+
+# Averarge number of distance-3 neighbors: O(n^2)   # Cf. Gebremedhin2005, StarColoringAlg1
+# Averarge number of distance-2 neighbors: O(n),    # Cf. Gebremedhin2005, StarColoringAlg2
