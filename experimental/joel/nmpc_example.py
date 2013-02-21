567c22f8a695ac5bac824d805902ba067cf46b5b
Joel Andersson <j.a.e.andersson@gmail.com>
2013-02-21 15:22:17 +0100

    Added minimal SCPgen example

---
+++ experimental/joel/nmpc_example.py
@@ -0,0 +1,49 @@
+#
+#     This file is part of CasADi.
+# 
+#     CasADi -- A symbolic framework for dynamic optimization.
+#     Copyright (C) 2010 by Joel Andersson, Moritz Diehl, K.U.Leuven. All rights reserved.
+# 
+#     CasADi is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License as published by the Free Software Foundation; either
+#     version 3 of the License, or (at your option) any later version.
+# 
+#     CasADi is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+# 
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with CasADi; if not, write to the Free Software
+#     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+# 
+from casadi import *
+
+n  = 30              # Num. intervals
+u = msym("u",n)      # Control
+J = inner_prod(u,u)  # NLP objective
+G = MX.zeros(0,1)    # NLP constraints
+x = 0.02             # State
+for k in range(n):
+    x = x +  0.1*(x*(x+1) + u[k])
+    x.lift(0.)       # Zero initial guess
+    J = J + x*x      # Add to objective
+    G.append(x)      # Add to constraints
+
+f = MXFunction([u],[J]) # Objective function
+g = MXFunction([u],[G]) # Constraint function
+S = SCPgen(f,g)      # NLP solver instance
+S.setOption("qp_solver",QPOasesSolver)
+S.setOption("qp_solver_options",\
+                    {"printLevel":"none"})
+S.init()
+    
+# Pass bounds and solve
+S.setInput(-1.0, NLP_LBX)  # u_min
+S.setInput( 1.0, NLP_UBX)  # u_max
+S.setInput(-1.0, NLP_LBG)  # x_min
+S.setInput( 1.0, NLP_UBG)  # x_max
+S.solve()
+
