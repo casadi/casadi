71c1fe4519873403bada2ca0dfe2f0584bc15560
Joel Andersson <j.a.e.andersson@gmail.com>
2013-04-23 15:21:28 +0200

    Fixes #680: The bug was in vecNZ and not in reshape

--- symbolic/mx/mx_tools.cpp
+++ symbolic/mx/mx_tools.cpp
@@ -216,7 +216,11 @@ namespace CasADi{
   }
 
   MX vecNZ(const MX& x) {
-    return reshape(trans(x),sp_dense(x.size(),1));
+    if(x.dense()){
+      return flatten(trans(x));
+    } else {
+      return trans(x)->getGetNonzeros(sp_dense(x.size()),range(x.size()));
+    }
   }
 
   MX if_else(const MX &cond, const MX &if_true, const MX &if_false){
