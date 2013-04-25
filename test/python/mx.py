20e36cbde0667f2a26066a156ade9c5f5704c660
Joris Gillis <joris.gillis42@gmail.com>
2013-04-25 18:46:55 +0200

    SAfely re-adding some unittests that were disabled in 600e08f8d21e4a87

--- test/python/mx.py
+++ test/python/mx.py
@@ -96,9 +96,9 @@ class MXtests(casadiTestCase):
     self.pool.append(lambda x: arccos(x[0]),arccos,"arccos")
     self.pool.append(lambda x: exp(x[0]),exp,"exp")
     self.pool.append(lambda x: log(x[0]),log,"log")
-#    self.pool.append(lambda x: x[0]**0,lambda x : x**0,"x^0") # ill-posed since the numerical values contain zeros giving the undefined 0^0
+    self.pool.append(lambda x: x[0]**0,lambda x : x**0,"x^0",flags={'nozero'})
     self.pool.append(lambda x: x[0]**1,lambda x : x**1,"^1")
-#    self.pool.append(lambda x: x[0]**(-2),lambda x : x**(-2),"^-2") # ill-posed since the numerical values contain zeros giving the undefined 0^-2
+    self.pool.append(lambda x: x[0]**(-2),lambda x : x**(-2),"^-2",flags={'nozero'})
     self.pool.append(lambda x: x[0]**(0.3),lambda x : x**(0.3),"^0.3")
     self.pool.append(lambda x: floor(x[0]),floor,"floor")
     self.pool.append(lambda x: ceil(x[0]),ceil,"ceil")
@@ -708,7 +708,7 @@ class MXtests(casadiTestCase):
       if scipy_available:
         x0=DMatrix(3,4,[1,2,1],[0,2,2,3],[0.738,0.1,0.99]).toCsr_matrix()
         
-        self.numpyEvaluationCheckPool(self.pool,[x],array(x0.todense()),name="MX",setx0=x0)
+        self.numpyEvaluationCheckPool(self.pool,[x],array(x0.todense()),name="MX",setx0=x0,excludeflags={'nozero'})
         self.numpyEvaluationCheckPool(self.matrixpool,[x],array(x0.todense()),name="MX",setx0=x0)
       else:
         x0=DMatrix(3,4,[1,2,1],[0,2,2,3],[0.738,0.1,0.99]).toArray()
