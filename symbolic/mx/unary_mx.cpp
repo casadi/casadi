casadi/casadi

commit 2d4091f4f935057d70b341392b231afbd8c73c05

Fixes #689

symbolic/mx/unary_mx.cpp
@@ -203,8 +203,8 @@ namespace CasADi{
     switch(op_){
     case OP_NEG:
       if(op==OP_ADD) return y->getBinary(OP_SUB,dep(),scY,scX);
-      else if(op==OP_MUL) return -y->getBinary(OP_MUL,dep(),scY,scX);
-      else if(op==OP_DIV) return -y->getBinary(OP_DIV,dep(),scY,scX);
+      else if(op==OP_MUL) return -dep()->getBinary(OP_MUL,y,scY,scX);
+      else if(op==OP_DIV) return -dep()->getBinary(OP_DIV,y,scY,scX);
       break;
     case OP_TWICE:
       if(op==OP_SUB && y.isEqual(dep(),maxDepth())) return dep();

symbolic/mx/mx_node.cpp
@@ -463,6 +463,8 @@ namespace CasADi{
       if(y.isEqual(this,maxDepth())) return MX::zeros(sparsity());
       break;
     case OP_DIV:
+      if(y->isZero()) return MX::nan(sparsity());
+      // fall-through
     case OP_EQ:
     case OP_LE:
       if(y.isEqual(this,maxDepth())) return MX::ones(sparsity());

commit 6eddf5bcf00ea7e6dc767e5137e8d12f9c853f4c

Fixed bug introduced in 2d4091f4f935057d70b341392b231afbd8c73c05

symbolic/mx/unary_mx.cpp
@@ -203,8 +203,8 @@ namespace CasADi{
     switch(op_){
     case OP_NEG:
       if(op==OP_ADD) return y->getBinary(OP_SUB,dep(),scY,scX);
-      else if(op==OP_MUL) return -dep()->getBinary(OP_MUL,y,scY,scX);
-      else if(op==OP_DIV) return -dep()->getBinary(OP_DIV,y,scY,scX);
+      else if(op==OP_MUL) return -dep()->getBinary(OP_MUL,y,scX,scY);
+      else if(op==OP_DIV) return -dep()->getBinary(OP_DIV,y,scX,scY);
       break;
     case OP_TWICE:
       if(op==OP_SUB && y.isEqual(dep(),maxDepth())) return dep();