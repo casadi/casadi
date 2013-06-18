a87c2438e35d3e0d1212435145ee9528394e07c3
Joris Gillis <joris.gillis42@gmail.com>
2013-06-18 18:54:17 +0200

    Added CSparseCholesky linear solver

---
+++ interfaces/csparse/csparse_cholesky_internal.hpp
@@ -0,0 +1,88 @@
+/*
+ *    This file is part of CasADi.
+ *
+ *    CasADi -- A symbolic framework for dynamic optimization.
+ *    Copyright (C) 2010 by Joel Andersson, Moritz Diehl, K.U.Leuven. All rights reserved.
+ *
+ *    CasADi is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    CasADi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with CasADi; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef CSPARSE_CHOLESKY_INTERNAL_HPP
+#define CSPARSE_CHOLESKY_INTERNAL_HPP
+
+extern "C"{
+#include "cs.h"
+}
+
+#include "csparse_cholesky.hpp"
+#include "symbolic/fx/linear_solver_internal.hpp"
+
+namespace CasADi{
+
+/**
+ @copydoc LinearSolver_doc
+*/
+class CSparseCholeskyInternal : public LinearSolverInternal{
+  public:
+    // Create a linear solver given a sparsity pattern and a number of right hand sides
+    CSparseCholeskyInternal(const CRSSparsity& sp);
+
+    // Copy constructor
+    CSparseCholeskyInternal(const CSparseCholeskyInternal& linsol);
+
+    // Destructor
+    virtual ~CSparseCholeskyInternal();
+    
+    // Initialize the solver
+    virtual void init();
+
+    // Factorize the matrix
+    virtual void prepare();
+    
+    // Solve the system of equations
+    virtual void solve(double* x, int nrhs, bool transpose);
+
+    // Solve the system of equations Lx = b
+    void solveL(double* x, int nrhs, bool transpose);
+    
+    /// Obtain a symbolic Cholesky factorization
+    CRSSparsity getFactorizationSparsity() const;
+    
+    /// Obtain a numeric Cholesky factorization
+    DMatrix getFactorization() const;
+    
+    // Clone
+    virtual CSparseCholeskyInternal* clone() const;
+    
+    // The tranpose of linear system in form (CCS)
+    cs AT_;
+
+    // The symbolic factorization
+    css *S_;
+    
+    // The numeric factorization
+    csn *L_;
+    
+    // Temporary
+    std::vector<double> temp_;
+
+    
+};
+
+} // namespace CasADi
+
+#endif //CSPARSE_CHOLESKY_INTERNAL_HPP
+
--- test/python/linearsolver.py
+++ test/python/linearsolver.py
@@ -19,27 +19,41 @@
 #     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 # 
 # 
-from mx import *
-from sx import *
-from typemaps import *
-from integration import *
-from ocp import *
-from socpsolver import *
-from nlp import *
-from implicitfunction import *
-from ad import *
-from sparsity import *
-from linearsolver import *
-from matrix import *
-from qpsolver import *
-from sdp import *
-from misc import *
-from qcqpsolver import *
-from fx import *
-from lpsolver import *
-from tools import *
-from simulator import *
-from vectortools import *
+from casadi import *
+import casadi as c
+from numpy import *
+import unittest
+from types import *
+from helpers import *
+import random
+
+class LinearSolverTests(casadiTestCase):
+
+  def test_cholesky(self):
+    random.seed(1)
+    n = 10
+    L = self.randDMatrix(n,n,sparsity=0.2) +  c.diag(range(n))
+    M = mul(L,L.T)
+
+    S = CSparseCholesky(M.sparsity())
+
+    S.init()
+    S.getFactorizationSparsity().spy()
+
+    S.setInput(M,0)
+
+  def test_cholesky2(self):
+    random.seed(0)
+    n = 10
+    L = c.diag(range(n))
+    M = mul(L,L.T)
+
+    print L
+    S = CSparseCholesky(M.sparsity())
+    
+
+    S.init()
+    S.getFactorizationSparsity().spy()
 
 if __name__ == '__main__':
     unittest.main()
