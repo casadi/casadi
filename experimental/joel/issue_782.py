751eedc7a350371d2cd22e1447494aee95ae6de4
Joel Andersson <j.a.e.andersson@gmail.com>
2013-06-20 17:37:01 +0200

    Concerning #782: isolating error (4)

--- experimental/joel/issue_782.py
+++ experimental/joel/issue_782.py
@@ -9,7 +9,7 @@ z[[1,0]]*=x
 fun = MXFunction([x],[z])
 fun.init()
 
-for f,sym,Function,X in [(fun,msym,MXFunction,MX),(fun.expand(),ssym,SXFunction,SXMatrix)]:
+for f,sym,Function,X in [(fun,msym,MXFunction,MX),]:
   f.init()
   print Function
 
@@ -20,12 +20,17 @@ for f,sym,Function,X in [(fun,msym,MXFunction,MX),(fun.expand(),ssym,SXFunction,
   vf = Function([aseeds],[adjsens])
   vf.init()
 
-  a = sym("a",2)
-  _,_,[[adjsens2]] = vf.eval([X.ones(2)],[],[[a]])
+  print vf
+  
+  vf.setInput([1,1])
+  vf.setAdjSeed([0,1])
+  vf.evaluate(0,1)
+  print vf.getAdjSens()
+  
+  _,_,[[adjsens2]] = vf.eval([X.ones(2)],[],[[X([0,1])]])
 
-  vf2 = Function([a],[adjsens2])
+  vf2 = Function([],[adjsens2])
   vf2.init()
-  vf2.setInput([0,1])
   vf2.evaluate()
 
   print vf2.getOutput()
