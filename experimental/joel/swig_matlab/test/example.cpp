40c55c6f6bfe91aa6ede358d42676fcff8a7367f
Joel Andersson <j.a.e.andersson@gmail.com>
2013-10-30 12:03:46 +0100

    Updated swig test example

---
+++ experimental/joel/swig_matlab/test/example.cpp
@@ -0,0 +1,16 @@
+#include "example.hpp"
+ 
+int fact(int n){
+  if (n <= 1) return 1;
+  else return n*fact(n-1);
+}
+
+int my_mod(int x, int y){
+  return (x%y);
+}
+
+char *get_time(){
+  time_t ltime;
+  time(&ltime);
+  return ctime(&ltime);
+}
