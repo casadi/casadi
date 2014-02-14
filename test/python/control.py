45df52a3084a93957be114eb78fd9e8eda969622
Joris Gillis <joris.gillis42@gmail.com>
2014-02-14 13:46:09 +0100

    Removing a double unittest

--- test/python/control.py
+++ test/python/control.py
@@ -97,9 +97,6 @@ class ControlTests(casadiTestCase):
 
           self.checkarray(a0,a1)
 
-          
-          self.checkfx(solver,refsol,sens_der=False,hessian=False,evals=False)
-
           self.checkfx(solver,refsol,sens_der=False,hessian=False,evals=1)
   
   @memory_heavy()
