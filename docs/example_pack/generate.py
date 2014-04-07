17710d089f7ea893a5eaec2b78799a00ef6919c0
Joris Gillis <joris.gillis42@gmail.com>
2014-04-07 16:43:54 +0200

    #1051 misc fixes

--- docs/example_pack/generate.py
+++ docs/example_pack/generate.py
@@ -27,13 +27,13 @@ c) start spyder, open vdp_single_shooting.py and press "run".
     
 """ % (release,releasedocurl))
 
-base = "../../examples/python"
+base = "../examples/python"
 for root, dirs, files in os.walk(base): # Walk directory tree
   for f in files:
     if f.endswith(".py"):
        zf.write(os.path.join(root,f),f,compress_type=compression)
 
-base = "../../documents"
+base = "../documents"
 for root, dirs, files in os.walk(base): # Walk directory tree
   for f in files:
     if f.endswith(".pdf"):
