ae12fbc3d8f655fa662102fbe06d773f8a20fb12
Joris Gillis <joris.gillis42@gmail.com>
2014-08-06 20:15:51 +0200

    remove hack to avoid pylab memory leak. Nowadays we just list false positives for valgrind

--- test/internal/test_py.py
+++ test/internal/test_py.py
@@ -30,7 +30,7 @@ def setdummybackend(dir):
   """
   Set the matplotlib backend to a dummy value, so that no graphs are generated.
   """
-  shutil.copy('internal/pylab.py',dir)
+  #shutil.copy('internal/pylab.py',dir)
   f=file(dir+'/matplotlibrc','w')
   f.write('backend : Template')
   f.close()
