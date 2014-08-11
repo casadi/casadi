ebd5bb543abeff6f7edf6d3cd67cf472314f5eb4
Joel Andersson <j.a.e.andersson@gmail.com>
2014-08-11 09:49:09 +0200

    #1143 Added XML parser base class

---
+++ casadi/core/misc/xml_file.cpp
@@ -0,0 +1,38 @@
+/*
+ *    This file is part of CasADi.
+ *
+ *    CasADi -- A symbolic framework for dynamic optimization.
+ *    Copyright (C) 2010 by Joel Andersson, Moritz Diehl, K.U.Leuven. All rights reserved.
+ *
+ *    CasADi is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    CasADi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with CasADi; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "xml_file_internal.hpp"
+
+using namespace std;
+namespace casadi {
+
+  XmlFile::XmlFile() {
+  }
+
+  XmlFile::XmlFile(const std::string& name) {
+    assignNode(XmlFileInternal::getPlugin(name).creator());
+  }
+
+  XmlFile::~XmlFile() {
+  }
+
+} // namespace casadi
