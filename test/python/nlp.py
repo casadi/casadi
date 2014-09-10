5d002177e3381434b7e6b674ee473f0bb42fcd1b
jgillis <joris.gillis42@gmail.com>
2014-09-10 17:02:44 +0200

    #1209 Disabling knitro again

--- test/python/nlp.py
+++ test/python/nlp.py
@@ -76,12 +76,14 @@ try:
 except:
   pass
 
+"""
 try:
   NlpSolver.loadPlugin("knitro")
   solvers.append(("knitro",{}))
   print "Will test knitro"
 except:
   pass
+"""
 
 class NLPtests(casadiTestCase):
 
