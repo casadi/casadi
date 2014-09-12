5054e2e4bb9cf1f831b9cbef1c74431143cb6fbf
Joris Gillis <joris.gillis42@gmail.com>
2014-09-12 20:15:58 +0200

    Tentative fix for Windows

--- casadi/core/function/plugin_interface.hpp
+++ casadi/core/function/plugin_interface.hpp
@@ -105,14 +105,14 @@ namespace casadi {
 #ifdef _WIN32
     HINSTANCE handle = LoadLibrary(TEXT(lib.c_str()));
     if (!handle) {
-      errors += "\n  Tried " + lib + ":\n    Error code (WIN32): " + GetLastError();
+      errors += std::string("\n  Tried ") + lib + ":\n    Error code (WIN32): " + GetLastError();
 
       #ifdef PLUGIN_EXTRA_SEARCH_PATH
       // Try the second search path
       lib = PLUGIN_EXTRA_SEARCH_PATH "\\" + lib;
       handle = LoadLibrary(TEXT(lib.c_str()));
       if (!handle) {
-        errors += "\n  Tried: " + lib + ":\n    Error code (WIN32): " + GetLastError();
+        errors += std::string("\n  Tried: ") + lib + ":\n    Error code (WIN32): " + GetLastError();
       }
       #endif // PLUGIN_EXTRA_SEARCH_PATH
     }
