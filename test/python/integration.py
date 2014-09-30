f46f88434c78ebde6b4171e243d429aa5e85cccb
Greg Horn <gregmainland@gmail.com>
2014-09-30 20:52:40 +0200

    only test oldcollocation if it's available

--- test/python/integration.py
+++ test/python/integration.py
@@ -51,8 +51,13 @@ except:
 
 integrators.append(("collocation",["dae","ode"],{"implicit_solver":"kinsol","number_of_finite_elements": 18}))
 
-integrators.append(("oldcollocation",["dae","ode"],{"implicit_solver":"kinsol","number_of_finite_elements": 18,"startup_integrator":"cvodes"}))
-#integrators.append(("oldcollocation",["dae","ode"],{"implicit_solver":"nlp","number_of_finite_elements": 100,"startup_integrator":"cvodes","implicit_solver_options": {"nlp_solver": "ipopt","linear_solver_creator": "csparse"}}))
+try:
+  Integrator.loadPlugin("oldcollocation")
+  integrators.append(("oldcollocation",["dae","ode"],{"implicit_solver":"kinsol","number_of_finite_elements": 18,"startup_integrator":"cvodes"}))
+  #integrators.append(("oldcollocation",["dae","ode"],{"implicit_solver":"nlp","number_of_finite_elements": 100,"startup_integrator":"cvodes","implicit_solver_options": {"nlp_solver": "ipopt","linear_solver_creator": "csparse"}}))
+except:
+  pass
+
 integrators.append(("rk",["ode"],{"number_of_finite_elements": 1000}))
 
 print "Will test these integrators:"
