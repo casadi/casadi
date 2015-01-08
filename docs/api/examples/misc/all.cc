1f3b5b7f56f9b3b49dc25e114c8271a6a1487ec9
Joel Andersson <j.a.e.andersson@gmail.com>
2015-01-08 16:20:07 +0100

    #1285 Fixed broken api example

--- docs/api/examples/misc/all.cc
+++ docs/api/examples/misc/all.cc
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
 {
   DMatrix d = DMatrix::zeros(3,1);
 
-  casadi_assert(!all(d));
-  casadi_assert(!any(d));
+  casadi_assert(!all(d).toScalar());
+  casadi_assert(!any(d).toScalar());
   std::cout << d << "   all: " << all(d) << "  any: " <<  any(d) << std::endl;
   
   
@@ -17,16 +17,16 @@ int main(int argc, char *argv[])
   d(1) = 0;
   d(2) = 1;
   
-  casadi_assert(!all(d));
-  casadi_assert(any(d));
+  casadi_assert(!all(d).toScalar());
+  casadi_assert(any(d).toScalar());
   std::cout << d << "   all: " << all(d) << "  any: " <<  any(d) << std::endl;
   
   d(0) = 1;
   d(1) = 1;
   d(2) = 1;
   
-  casadi_assert(all(d));
-  casadi_assert(any(d));
+  casadi_assert(all(d).toScalar());
+  casadi_assert(any(d).toScalar());
   std::cout << d << "   all: " << all(d) << "  any: " <<  any(d) << std::endl;
   
   return 0;
