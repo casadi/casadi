54c6608c52979fdb299f4db8f3ff8ea84c0cae6c
Joel Andersson <j.a.e.andersson@gmail.com>
2015-05-25 09:24:18 +0200

    #1431 Fixed CPLEX interface

--- casadi/interfaces/cplex/cplex_interface.hpp
+++ casadi/interfaces/cplex/cplex_interface.hpp
@@ -60,7 +60,7 @@ namespace casadi {
 
     /** \brief  Create a new QP Solver */
     static QpSolverInternal* creator(const QPStructure& st)
-    { return new CplexInterface(st);}
+    { return new CplexInterface(st.data);}
 
     /// Constructor using sparsity patterns
     explicit CplexInterface(const std::vector<Sparsity>& st);
