casadi/casadi

commit b1d87f8a3d540fb70b783e2975abe643f6525835

#1451 Debugged Integrator segfault

swig/casadi.i
@@ -720,11 +720,7 @@ bool PyObjectHasClassName(PyObject* p, const char * name) {
     }
 
     GUESTOBJECT * from_ptr(const bool *a) {
-#ifdef SWIGPYTHON
-      return PyBool_FromLong(*a);
-#else
-      return 0;
-#endif // SWIGPYTHON
+      return SWIG_From_bool(*a);
     }
   } // namespace casadi
   }
@@ -1187,13 +1183,15 @@ bool PyObjectHasClassName(PyObject* p, const char * name) {
       }
 
       // Try to convert to different types
-      if (to_generic<int>(p, m)
+      if (to_generic<bool>(p, m)
+          || to_generic<int>(p, m)
           || to_generic<double>(p, m)
           || to_generic<std::string>(p, m)
           || to_generic<std::vector<int> >(p, m)
           || to_generic<std::vector<double> >(p, m)
           || to_generic<std::vector<std::string> >(p, m)
-          || to_generic<casadi::Function>(p, m)) {
+          || to_generic<casadi::Function>(p, m)
+          || to_generic<casadi::GenericType::Dict>(p, m)) {
         return true;
       }
 

casadi/core/function/integrator.cpp
@@ -38,8 +38,10 @@ namespace casadi {
     Function g;
     Dict opts2 = opts;
     Dict::const_iterator it=opts2.find("rdae");
-    if (it!=opts2.end()) g = it->second;
-    opts2.erase(it);
+    if (it!=opts2.end()) {
+      g = it->second;
+      opts2.erase(it);
+    }
 
     // Create an initialize
     assignNode(IntegratorInternal::getPlugin(solver).creator(f, g));

test/matlab/sensitivity_analysis.m
@@ -50,8 +50,8 @@ for ode=0:1
     beta = 0.1;   % fuel consumption rate
       
     % Differential equation
-    ode = [v,
-           (u-alpha*v*v)/m,
+    ode = [v;
+           (u-alpha*v*v)/m;
            -beta*u*u];
       
     % Quadrature
@@ -126,6 +126,6 @@ for ode=0:1
     disp(opts)
 
     % Integrator
-    %I = Integrator('myintegrator', MyIntegrator, ffcn); % segfault
+    I = Integrator('myintegrator', MyIntegrator, ffcn, opts);
   end
 end

commit 9138b9f11899717dba03ba476c7b6f74cc38b04a

issue #1451: Fixed bug introduced in b1d87f8a3d540fb70b783e2975abe643f6525835

swig/casadi.i
@@ -1220,8 +1220,7 @@ bool PyObjectHasClassName(PyObject* p, const char * name) {
       }
 
       // Try to convert to different types
-      if (to_generic<bool>(p, m)
-          || to_generic<int>(p, m)
+      if (to_generic<int>(p, m)
           || to_generic<double>(p, m)
           || to_generic<std::string>(p, m)
           || to_generic<std::vector<int> >(p, m)