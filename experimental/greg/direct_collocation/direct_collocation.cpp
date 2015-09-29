79436f910c395e376645e7df3fc49d2773de3cd3
Greg Horn <greghorn@google.com>
2015-09-28 20:25:05 -0700

    add direct collocation files without content

---
+++ experimental/greg/direct_collocation/direct_collocation.cpp
@@ -0,0 +1,49 @@
+/*
+ *    This file is part of CasADi.
+ *
+ *    CasADi -- A symbolic framework for dynamic optimization.
+ *    Copyright (C) 2010-2014 Joel Andersson, Joris Gillis, Moritz Diehl,
+ *                            K.U. Leuven. All rights reserved.
+ *    Copyright (C) 2011-2014 Greg Horn
+ *
+ *    CasADi is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    CasADi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with CasADi; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+
+#include "casadi/core/polynomial.hpp"
+#include "casadi/core/std_vector_tools.hpp"
+#include "casadi/core/function/sx_function.hpp"
+#include "casadi/core/function/function.hpp"
+#include "direct_collocation.hpp"
+
+using namespace std;
+namespace casadi {
+
+  DirectCollocation::DirectCollocation(const Dict& f)
+      : Function(  ) {
+    addOption("interpolation_order",           OT_INTEGER,  3,
+              "Order of the interpolating polynomials");
+    addOption("collocation_scheme",            OT_STRING,  "radau",
+              "Collocation scheme", "radau|legendre");
+    addOption("map_strategy",         OT_STRING,  "unrolled",
+              "Collocation scheme", "unrolled|symbolic_serial|symbolic_openmp");
+    setOption("name", "unnamed_direct_collocation");
+  }
+
+  DirectCollocation::getNlp() {
+  }
+
+} // namespace casadi
---
+++ experimental/greg/direct_collocation/direct_collocation.hpp
@@ -0,0 +1,62 @@
+/*
+ *    This file is part of CasADi.
+ *
+ *    CasADi -- A symbolic framework for dynamic optimization.
+ *    Copyright (C) 2010-2014 Joel Andersson, Joris Gillis, Moritz Diehl,
+ *                            K.U. Leuven. All rights reserved.
+ *    Copyright (C) 2011-2014 Greg Horn
+ *
+ *    CasADi is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    CasADi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with CasADi; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+
+#ifndef CASADI_DIRECT_COLLOCATION_HPP
+#define CASADI_DIRECT_COLLOCATION_HPP
+
+#include "casadi/core/function/mx_function.hpp"
+#include "casadi/core/function/implicit_function.hpp"
+#include "casadi/core/misc/integration_tools.hpp"
+//#include "casadi/core/misc/integration_tools.hpp"
+
+/// \cond INTERNAL
+namespace casadi {
+
+  class DirectCollocation :
+        public Function {
+  public:
+
+    /// Constructor
+    explicit DirectCollocation(const Dict& f);
+
+    /// Destructor
+    ~DirectCollocation();
+
+    /// Setup F and G
+    Function getNlp();
+
+  private:
+    // Interpolation order
+    int deg_;
+
+    // Number of shooting intervals
+    int n_;
+
+  };
+
+} // namespace casadi
+/// \endcond
+
+#endif // CASADI_DIRECT_COLLOCATION_HPP
