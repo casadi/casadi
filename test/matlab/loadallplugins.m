1c8c4e761fee1b4c753a1422d7eff301d470e496
Joel Andersson <j.a.e.andersson@gmail.com>
2015-10-19 14:10:44 +0200

    Issue #1585: Bugfix loadallplugins.m

--- test/matlab/loadallplugins.m
+++ test/matlab/loadallplugins.m
@@ -4,6 +4,18 @@ for pl = strsplit(CasadiMeta.getPlugins(),';')
   out  = strsplit(pl{:},'::');
   cls  = out{1};
   name = out{2};
-  eval([cls '.loadPlugin(''' name ''')'])
+  if strcmp(cls, 'Integrator')
+    eval(['Function.load_integrator(''' name ''')'])
+  elseif strcmp(cls, 'NlpSolver')
+    eval(['Function.load_nlp_solver(''' name ''')'])
+  elseif strcmp(cls, 'QpSolver')
+    eval(['Function.load_qp_solver(''' name ''')'])
+  elseif strcmp(cls, 'ImplicitFunction')
+    eval(['Function.load_rootfinder(''' name ''')'])
+  else
+    eval([cls '.loadPlugin(''' name ''')'])
+  end
+
+
 end
 
