21f2c1d20382978f4be0352b66a1f70815736c20
Joel Andersson <j.a.e.andersson@gmail.com>
2015-11-13 17:23:22 -0600

    Workaround for Windows compilation failure

--- casadi/core/std_vector_tools.hpp
+++ casadi/core/std_vector_tools.hpp
@@ -273,7 +273,11 @@ namespace casadi {
   /// Checks if array does not contain NaN or Inf
   template<typename T>
   bool is_regular(const std::vector<T> &v) {
-    return std::all_of(v.begin(), v.end(), [](T v) { return std::isfinite(v);});
+    for (auto&& vk : v) {
+      if (vk!=vk || vk==std::numeric_limits<T>::infinity() ||
+          vk==-std::numeric_limits<T>::infinity()) return false;
+    }
+    return true;
   }
 
 } // namespace casadi
