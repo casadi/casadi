8e96f622dacaa77483aaf6541b96c8c809b7a0d8
Joel Andersson <j.a.e.andersson@gmail.com>
2015-11-19 22:55:55 -0600

    Added simple multiple shooting example (2)

--- docs/examples/python/dae_multiple_shooting.py
+++ docs/examples/python/dae_multiple_shooting.py
@@ -65,68 +65,63 @@ dae = {'x':x, 'z':z, 'p':u, 'ode':f_x, 'alg':f_z, 'quad':f_q}
 opts = {'tf':0.5} # interval length
 I = integrator('I', 'idas', dae, opts)
 
-# NLP decision variables and bounds
-W = []
-lbW = []
-ubW = []
+# Number of intervals
+nk = 20
 
-# NLP constraints (equality constraints)
-G = []
+# Start with an empty NLP
+w = []   # List of variables
+lbw = [] # Lower bounds on w
+ubw = [] # Upper bounds on w
+G = []   # Constraints
+J = 0    # Cost function
 
-# Construct graph of integrator calls
-Xk = MX([0,1])
-J = 0
+# Initial conditions
+Xk = MX.sym('X0', 2)
+w.append(Xk)
+lbw += [ 0, 1 ]
+ubw += [ 0, 1 ]
 
 # Loop over all intervals
-nk = 20
 for k in range(nk):
   # Local control
-  Uk = MX.sym('U_' + str(k), u.size())
-  W.append(Uk)
-  lbW += [-0.75]
-  ubW += [ 1.00]
+  Uk = MX.sym('U'+str(k))
+  w.append(Uk)
+  lbw += [-0.75]
+  ubw += [ 1.00]
 
   # Call integrator function
   Ik = I({'x0':Xk, 'p':Uk})
   Xk = Ik['xf']
-  J = J + Ik['qf']   # Sum up quadratures
+  J = J + Ik['qf'] # Sum quadratures
 
-  # "Lift" the variable, i.e. treat as a NLP variable
-  X_next = MX.sym('X_' + str(k), x.size())
-  W.append(X_next)
-  lbW += [-inf, -inf]
-  ubW += [ inf,  inf]
-  G += [Xk-X_next]
-  Xk = X_next
+  # "Lift" the variable
+  X_prev = Xk
+  Xk = MX.sym('X'+str(k+1), 2)
+  w.append(Xk)
+  lbw += [-inf, -inf]
+  ubw += [ inf,  inf]
+  G.append(X_prev - Xk)
 
 # Allocate an NLP solver
-nlp = {'x':vertcat(W), 'f':J, 'g':vertcat(G)}
+nlp = {'x':vertcat(w), 'f':J, 'g':vertcat(G)}
 opts = {'linear_solver':'ma27'}
 solver = nlpsol('solver', 'ipopt', nlp, opts)
 
 # Pass bounds, initial guess and solve NLP
-sol = solver({'lbx' : lbW, # Lower variable bound
-              'ubx' : ubW,  # Upper variable bound
+sol = solver({'lbx' : lbw, # Lower variable bound
+              'ubx' : ubw,  # Upper variable bound
               'lbg' : 0.0,  # Lower constraint bound
               'ubg' : 0.0,  # Upper constraint bound
               'x0'  : 0.0}) # Initial guess
-x0_opt = sol['x'][0::3]
-x1_opt = sol['x'][1::3]
-u_opt = sol['x'][2::3]
-
-# Time grid
-import numpy as NP
-tgrid_x = NP.linspace(0,10,nk+1)
-tgrid_u = NP.linspace(0,10,nk)
 
 # Plot the results
 import matplotlib.pyplot as plt
 plt.figure(1)
 plt.clf()
-plt.plot(tgrid_x,vertcat([0, x0_opt]),'--')
-plt.plot(tgrid_x,vertcat([1, x1_opt]),'-')
-plt.plot(tgrid_u,u_opt,'-.')
-plt.title('Van der Pol optimization - single shooting')
+plt.plot(linspace(0., 10., nk+1), sol['x'][0::3],'--')
+plt.plot(linspace(0., 10., nk+1), sol['x'][1::3],'-')
+plt.plot(linspace(0., 10., nk), sol['x'][2::3],'-.')
+plt.title('Van der Pol optimization - multiple shooting')
 plt.xlabel('time')
 plt.legend(['x0 trajectory','x1 trajectory','u trajectory'])
 plt.grid()
