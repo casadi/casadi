41bf1e6afa8309c62f28287ac1efbe24204110c3
Joel Andersson <j.a.e.andersson@gmail.com>
2016-01-03 19:20:28 -0600

    Issue #1615: Refactored Kinsol interface

--- casadi/interfaces/sundials/kinsol_interface.hpp
+++ casadi/interfaces/sundials/kinsol_interface.hpp
@@ -47,6 +47,36 @@
 
 /// \cond INTERNAL
 namespace casadi {
+  /// Forward declaration
+  struct KinsolInterface;
+
+  // Memory
+  struct CASADI_ROOTFINDER_KINSOL_EXPORT KinsolMemory : public WorkMemory {
+    /// Function object
+    const KinsolInterface& self;
+
+    /// Constructor
+    KinsolMemory(const KinsolInterface& s);
+
+    /// Destructor
+    virtual ~KinsolMemory();
+
+    /// KINSOL memory block
+    void* mem;
+
+    /// Variable
+    N_Vector u;
+
+    /// For timings
+    clock_t time1, time2;
+
+    /// Accumulated time since last reset:
+    double t_func; // time spent in the residual function
+    double t_jac; // time spent in the Jacobian function
+    double t_lsolve; // preconditioner/linear solver solve function
+    double t_lsetup_jac; // preconditioner/linear solver setup function, generate Jacobian
+    double t_lsetup_fac; // preconditioner setup function, factorize Jacobian
+  };
 
   /** \brief \pluginbrief{Rootfinder,kinsol}
   *
@@ -124,54 +154,22 @@ namespace casadi {
 
     /** \brief Free allocated memory block */
     virtual void free_mem(void* mem);
-  };
-
-  // Memory
-  struct CASADI_ROOTFINDER_KINSOL_EXPORT KinsolMemory : public Memory {
-    /// Shared memory
-    KinsolInterface& self;
-
-    /// Constructor
-    KinsolMemory(KinsolInterface& s);
-
-    /// Destructor
-    virtual ~KinsolMemory();
-
-    /// KINSOL memory block
-    void* mem_;
-
-    /// Function arguments
-    const double** arg_;
-    double** res_;
-    int* iw_;
-    double* w_;
-
-    /// Variable
-    N_Vector u_;
-
-    /// For timings
-    clock_t time1_, time2_;
-
-    /// Accumulated time since last reset:
-    double t_func_; // time spent in the residual function
-    double t_jac_; // time spent in the Jacobian function
-    double t_lsolve_; // preconditioner/linear solver solve function
-    double t_lsetup_jac_; // preconditioner/linear solver setup function, generate Jacobian
-    double t_lsetup_fac_; // preconditioner setup function, factorize Jacobian
 
     /** \brief Callback functions */
-    void func(N_Vector u, N_Vector fval);
-    void djac(long N, N_Vector u, N_Vector fu, DlsMat J, N_Vector tmp1, N_Vector tmp2);
-    void bjac(long N, long mupper, long mlower, N_Vector u, N_Vector fu, DlsMat J,
-              N_Vector tmp1, N_Vector tmp2);
-    void jtimes(N_Vector v, N_Vector Jv, N_Vector u, int* new_u);
-    void psetup(N_Vector u, N_Vector uscale, N_Vector fval, N_Vector fscale,
-                N_Vector tmp1, N_Vector tmp2);
-    void psolve(N_Vector u, N_Vector uscale, N_Vector fval, N_Vector fscale, N_Vector v,
-                N_Vector tmp);
-    void lsetup(KINMem kin_mem);
-    void lsolve(KINMem kin_mem, N_Vector x, N_Vector b, double *res_norm);
-    void ehfun(int error_code, const char *module, const char *function, char *msg);
+    void func(KinsolMemory& m, N_Vector u, N_Vector fval) const;
+    void djac(KinsolMemory& m, long N, N_Vector u, N_Vector fu,
+              DlsMat J, N_Vector tmp1, N_Vector tmp2) const;
+    void bjac(KinsolMemory& m, long N, long mupper, long mlower, N_Vector u, N_Vector fu, DlsMat J,
+              N_Vector tmp1, N_Vector tmp2) const;
+    void jtimes(KinsolMemory& m, N_Vector v, N_Vector Jv, N_Vector u, int* new_u) const;
+    void psetup(KinsolMemory& m, N_Vector u, N_Vector uscale, N_Vector fval, N_Vector fscale,
+                N_Vector tmp1, N_Vector tmp2) const;
+    void psolve(KinsolMemory& m, N_Vector u, N_Vector uscale,
+                N_Vector fval, N_Vector fscale, N_Vector v, N_Vector tmp) const;
+    void lsetup(KinsolMemory& m, KINMem kin_mem) const;
+    void lsolve(KinsolMemory& m, KINMem kin_mem, N_Vector x, N_Vector b, double *res_norm) const;
+    void ehfun(KinsolMemory& m, int error_code, const char *module,
+               const char *function, char *msg) const;
 
     /** \brief Wrappers to callback functions*/
     static int func_wrapper(N_Vector u, N_Vector fval, void *user_data);
