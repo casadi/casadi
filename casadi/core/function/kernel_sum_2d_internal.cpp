c3e8c6ce9f0640185e707a4e8bdd9f1ab086760b
jgillis <joris.gillis42@gmail.com>
2016-02-05 15:34:51 +0100

    Update kernel_sum_2d_internal.cpp

--- casadi/core/function/kernel_sum_2d_internal.cpp
+++ casadi/core/function/kernel_sum_2d_internal.cpp
@@ -28,6 +28,14 @@
 #include "../profiling.hpp"
 #include <sstream>
 
+#ifdef _MSC_VER
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#else
+#include <stdint.h>
+#endif
+
+
 using namespace std;
 
 namespace casadi {
