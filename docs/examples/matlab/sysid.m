604f5d92581e5b66be5b96a1f1298e07b4b25e8d
Joel Andersson <j.a.e.andersson@gmail.com>
2016-03-18 00:38:48 -0500

    Updated sysid.py to working syntax Unlike Python code, no GN or JIT

--- docs/examples/matlab/sysid.m
+++ docs/examples/matlab/sysid.m
@@ -38,31 +38,30 @@ params = [M;c;k;k_NL];
 rhs = [dy; (u-k_NL*y.^3-k*y-c*dy)/M];
 
 % Form an ode function
-ode = MXFunction('ode',{states,controls,params},{rhs});
+ode = Function('ode',{states,controls,params},{rhs});
 
 %%%%%%%%%%%% Creating a simulator %%%%%%%%%%
 N_steps_per_sample = 10;
 dt = 1/fs/N_steps_per_sample;
 
 % Build an integrator for this system: Runge Kutta 4 integrator
-k1 = ode({states,controls,params});
-k2 = ode({states+dt/2.0*k1{1},controls,params});
-k3 = ode({states+dt/2.0*k2{1},controls,params});
-k4 = ode({states+dt*k3{1},controls,params});
+k1 = ode(states,controls,params);
+k2 = ode(states+dt/2.0*k1,controls,params);
+k3 = ode(states+dt/2.0*k2,controls,params);
+k4 = ode(states+dt*k3,controls,params);
 
-states_final = states+dt/6.0*(k1{1}+2*k2{1}+2*k3{1}+k4{1});
+states_final = states+dt/6.0*(k1+2*k2+2*k3+k4);
 
 % Create a function that simulates one step propagation in a sample
-one_step = MXFunction('one_step',{states, controls, params},{states_final});
+one_step = Function('one_step',{states, controls, params},{states_final});
 
 X = states;
 for i=1:N_steps_per_sample
-    Xn = one_step({X, controls, params});
-    X = Xn{1};
+    X = one_step(X, controls, params);
 end
 
 % Create a function that simulates all step propagation on a sample
-one_sample = MXFunction('one_sample',{states, controls, params}, {X});
+one_sample = Function('one_sample',{states, controls, params}, {X});
 
 % speedup trick: expand into scalar operations
 one_sample = one_sample.expand();
@@ -74,9 +73,8 @@ all_samples = one_sample.mapaccum('all_samples', N);
 % Choose an excitation signal
 u_data = 0.1*rand(N,1);
 
-x0 = DMatrix([0,0]);
-all_samples_out = all_samples({x0, u_data, repmat(param_truth,1,N) });
-X_measured = all_samples_out{1};
+x0 = DM([0,0]);
+X_measured = all_samples(x0, u_data, repmat(param_truth,1,N));
 
 y_data = X_measured(1,:)';
 
@@ -84,34 +82,14 @@ y_data = X_measured(1,:)';
 
 % Note, it is in general a good idea to scale your decision variables such
 % that they are in the order of ~0.1..100
-all_samples_out = all_samples({x0, u_data, repmat(params.*scale,1,N) });
-X_symbolic = all_samples_out{1};
+X_symbolic = all_samples(x0, u_data, repmat(params.*scale,1,N));
 
 e = y_data-X_symbolic(01,:)';
 
-% just-in-time compilation for extra speedup
-options = struct;
-options.jit = true;
-options.jit_options = struct('flags',char('-O3',''));
+nlp = struct('x', params, 'f', 0.5*dot(e,e));
+solver = nlpsol('solver','ipopt', nlp);
 
-nlp = MXFunction('nlp', nlpIn('x',params), nlpOut('f',0.5*inner_prod(e,e)),options);
-
-gradF = nlp.gradient();
-jacG = nlp.jacobian('x','g');
-
-gradF.derivative(0, 1);
-
-J = jacobian(e,params);
-sigma = MX.sym('sigma');
-hessLag = MXFunction('H',hessLagIn('x',params,'lam_f',sigma),hessLagOut('hess',sigma*J'*J),options);
-
-options = struct;
-options.hess_lag = hessLag;
-options.grad_f = gradF;
-options.jac_g = jacG;
-solver = NlpSolver('solver','ipopt', nlp, options);
-
-sol = solver(struct('x0',param_guess));
+sol = solver('x0',param_guess);
 
 sol.x.*scale
 
