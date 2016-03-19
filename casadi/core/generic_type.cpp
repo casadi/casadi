15c30022b482f4e35815ea7180192ae59a88abb1
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-03-19 20:29:08 +0100

    Fixing Visual Studio compilation

--- casadi/core/generic_type.cpp
+++ casadi/core/generic_type.cpp
@@ -31,6 +31,13 @@
 #include "function/function.hpp"
 #include "functor.hpp"
 
+#ifdef _MSC_VER
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#else
+#include <stdint.h>
+#endif
+
 using namespace std;
 
 namespace casadi {
