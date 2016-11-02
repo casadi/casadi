3c996b60378f93b3ba14025e5dbbf65547002894
Joel Andersson <j.a.e.andersson@gmail.com>
2016-11-02 11:12:33 -0500

    Issue #1893 Changed sysid horizon back to 10000

--- docs/examples/python/sysid.py
+++ docs/examples/python/sysid.py
@@ -13,7 +13,7 @@ from casadi import *
 # This form is well-suited for the Gauss-Newton Hessian approximation.
 
 ############ SETTINGS #####################
-N = 100  # Number of samples
+N = 10000  # Number of samples
 fs = 610.1 # Sampling frequency [hz]
 
 param_truth = DM([5.625e-6,2.3e-4,1,4.69])
@@ -63,13 +63,10 @@ for i in range(N_steps_per_sample):
 # Create a function that simulates all step propagation on a sample
 one_sample = Function('one_sample',[states, controls, params], [X])
 
-# speedup trick: expand into scalar operations
-one_sample = one_sample.expand('one_sample_sx')
-
 ############ Simulating the system ##########
-assert(N%10==0)
-all_samples1 = one_sample.mapaccum("all_samples1", N//10)
-all_samples = all_samples1.mapaccum("all_samples", 10)
+assert(N%100==0)
+all_samples1 = one_sample.mapaccum("all_samples1", N//100)
+all_samples = all_samples1.mapaccum("all_samples", 100)
 
 # Choose an excitation signal
 numpy.random.seed(0)
@@ -86,10 +83,10 @@ y_data = X_measured[0,:].T
 
 # Use just-in-time compilation to speed up the evaluation
 if Importer.has_plugin('clang'):
-  opts = {'jit':True}
+  with_jit = True
 else:
   print("WARNING; running without jit. This may result in very slow evaluation times")
-  opts = {}
+  with_jit = False
 
 ############ Create a Gauss-Newton solver ##########
 def gauss_newton(e,nlp,V):
@@ -97,8 +94,8 @@ def gauss_newton(e,nlp,V):
   H = triu(mtimes(J.T, J))
   sigma = MX.sym("sigma")
   hessLag = Function('nlp_hess_l',{'x':V,'lam_f':sigma, 'hess_gamma_x_x':sigma*H},
-                     ['x','p','lam_f','lam_g'], ['hess_gamma_x_x'], opts)
-  return nlpsol("solver","ipopt", nlp, {"hess_lag":hessLag})
+                     ['x','p','lam_f','lam_g'], ['hess_gamma_x_x'])
+  return nlpsol("solver","ipopt", nlp, {"hess_lag":hessLag, "jit":with_jit})
 
 
 ############ Identifying the simulated system: single shooting strategy ##########
