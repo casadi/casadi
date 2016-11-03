33684f9ecc0bc619d7cdaf3de60192c176a6f117
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-11-03 15:50:07 +0100

    Cleanup experimental folder

--- experimental/andrew/mathml/examples.m
+++
@@ -1,8 +0,0 @@
-% MATLAB script to generate some mathml files using matlab symbolic toolbox
-syms x y z
-f = cos(2*x+y)+sin(x);
-s = feval(symengine,'generate::MathML',f);
-s = char(s);
-fid=fopen('example1.xml','w');
-fprintf(fid,'%s',s);
-fclose(fid);
