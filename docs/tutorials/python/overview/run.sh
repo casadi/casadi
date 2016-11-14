30698a9761195f827b191d261b2d099b13654c28
Joel Andersson <j.a.e.andersson@gmail.com>
2016-11-14 16:39:20 -0600

    Removed docs/tutorials

--- docs/tutorials/python/overview/run.sh
+++
@@ -1,4 +0,0 @@
-dot -Tpng < graph.dot > overview.png
-echo "<html>" > overview.html
-dot -Tcmapx < graph.dot >> overview.html
-echo "<IMG SRC='overview.png' USEMAP='#overview' /></html>" >> overview.html
--- docs/tutorials/python/publish.sh
+++
@@ -1,3 +0,0 @@
-echo "Please enter sf username"
-read username
-rsync -avP -e ssh pdf/ "$username,casadi@web.sourceforge.net:/home/groups/c/ca/casadi/htdocs/tutorials/"
