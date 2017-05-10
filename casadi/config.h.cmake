e1c9ec4f1bab2f68e14de0c91a4b15096acb007a
jgillis <joris.gillis42@gmail.com>
2017-05-10 22:26:25 +0200

    lintian fix

--- casadi/config.h.cmake
+++ casadi/config.h.cmake
@@ -21,8 +21,8 @@
  *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
-#ifndef CASADI_CONFIG_H_
-#define CASADI_CONFIG_H_
+#ifndef BUILD_CASADI_CONFIG_H_
+#define BUILD_CASADI_CONFIG_H_
 
 #define CASADI_MAJOR_VERSION ${CASADI_MAJOR_VERSION}
 #define CASADI_MINOR_VERSION ${CASADI_MINOR_VERSION}
@@ -40,4 +40,4 @@
 #define CASADI_PLUGINS "${CASADI_PLUGINS}"  // NOLINT(whitespace/line_length)
 #define CASADI_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}"  // NOLINT(whitespace/line_length)
 
-#endif // CASADI_CONFIG_H_
+#endif // BUILD_CASADI_CONFIG_H_
