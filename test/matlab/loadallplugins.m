904dc9e183a8a374b124424f016fc7a2d41328c7
jgillis <joris.gillis42@gmail.com>
2017-05-11 09:50:23 +0200

    Update loadallplugins.m

--- test/matlab/loadallplugins.m
+++ test/matlab/loadallplugins.m
@@ -2,8 +2,8 @@ import casadi.*
 
 for pl = strsplit(CasadiMeta.getPlugins(),';')
   out  = strsplit(pl{:},'::');
-  cls  = out{1};
-  name = out{2};
+  cls  = out{1}
+  name = out{2}
 
 if strcmp(cls,'Importer') || strcmp(cls,'XmlFile') || strcmp(cls,'Linsol')
   eval([cls '.load_plugin(''' name ''')'])
