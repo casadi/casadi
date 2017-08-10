casadi/casadi

commit fdbb98ce631b786b57cb73a7a4da262a0891c330

Issue #1749 Inline functions with free variables by default This might resolve bugs of the type 'expression has free variables'

casadi/core/mx_function.cpp
@@ -572,7 +572,7 @@ namespace casadi {
     }
 
     // Create call unless always_inline is true
-    if (never_inline || !always_inline) {
+    if (!should_inline(never_inline, always_inline)) {
       return FunctionInternal::eval_mx(arg, res, false, true);
     }
 

casadi/core/mx_function.hpp
@@ -123,7 +123,7 @@ namespace casadi {
 
     /** Inline calls? */
     bool should_inline(bool always_inline, bool never_inline) const override {
-      return always_inline;
+      return always_inline || has_free();
     }
 
     /** \brief Evaluate symbolically, SX type*/
@@ -131,7 +131,7 @@ namespace casadi {
 
     /** \brief Evaluate symbolically, MX type */
     void eval_mx(const MXVector& arg, MXVector& res,
-                         bool always_inline, bool never_inline) const override;
+                 bool always_inline, bool never_inline) const override;
 
     /** \brief Calculate forward mode directional derivatives */
     void ad_forward(const std::vector<std::vector<MX> >& fwdSeed,

commit 91d699236117f3772c56da902ab39aee597602fd

Issue #1749 Resolving bug caused by fdbb98ce631b786b57cb73a7a4da262a0891c330

casadi/core/function_internal.cpp
@@ -416,19 +416,27 @@ namespace casadi {
     print_dimensions(stream);
   }
 
-  void FunctionInternal::repr(ostream &stream) const {
+  std::string FunctionInternal::definition() const {
+    stringstream s;
+
     // Print name
-    stream << name_ << ":";
+    s << name_ << ":";
     // Print input arguments
     for (int i=0; i<n_in(); ++i) {
-      stream << (i==0 ? "(" : ",") << name_in(i);
+      s << (i==0 ? "(" : ",") << name_in(i);
     }
-    stream << ")->";
+    s << ")->";
     // Print output arguments
     for (int i=0; i<n_out(); ++i) {
-      stream << (i==0 ? "(" : ",") << name_out(i);
+      s << (i==0 ? "(" : ",") << name_out(i);
     }
-    stream << ")";
+    s << ")";
+
+    return s.str();
+  }
+
+  void FunctionInternal::repr(ostream &stream) const {
+    stream << definition();
   }
 
   Function FunctionInternal::wrap() const {

casadi/core/function_internal.hpp
@@ -407,6 +407,9 @@ namespace casadi {
     /** \brief  Print */
     void repr(std::ostream &stream) const override;
 
+    /** \brief Get function signature: name:(inputs)->(outputs) */
+    std::string definition() const;
+
     /** \brief Check if the numerical values of the supplied bounds make sense */
     virtual void checkInputs() const {}
 

casadi/core/mx_function.cpp
@@ -572,7 +572,7 @@ namespace casadi {
     }
 
     // non-inlining call is implemented in the base-class
-    if (!should_inline(never_inline, always_inline)) {
+    if (!should_inline(always_inline, never_inline)) {
       return FunctionInternal::eval_mx(arg, res, false, true);
     }
 
@@ -1303,7 +1303,10 @@ namespace casadi {
 
   bool MXFunction::should_inline(bool always_inline, bool never_inline) const {
     // If inlining has been specified
-    casadi_assert_message(!(always_inline && never_inline), "Inconsistent options");
+    casadi_assert_message(!(always_inline && never_inline),
+      "Inconsistent options for " + definition());
+    casadi_assert_message(!(never_inline && has_free()),
+      "Must inline " + definition());
     if (always_inline) return true;
     if (never_inline) return false;
     // Functions with free variables must be inlined