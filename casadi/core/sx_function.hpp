70e7596e9a70dfdcf40054707e1b335def89b6f2
Joel Andersson <j.a.e.andersson@gmail.com>
2017-09-02 17:30:14 -0500

    Removed OpenCL from source Broken since long, needs clean reimplementation

--- casadi/core/sx_function.hpp
+++ casadi/core/sx_function.hpp
@@ -28,13 +28,6 @@
 
 #include "x_function.hpp"
 
-#ifdef WITH_OPENCL
-#ifdef __APPLE__
-#include <OpenCL/opencl.h>
-#else
-#include <CL/cl.h>
-#endif
-#endif // WITH_OPENCL
 /// \cond INTERNAL
 
 namespace casadi {
@@ -47,36 +40,6 @@ namespace casadi {
       struct { int i1, i2; };
     };
   };
-
-#ifdef WITH_OPENCL
-  /** \brief Singleton for the sparsity propagation kernel
-      TODO: Move to a separate file and make non sparsity pattern specific
-      \author Joel Andersson
-      \date 2013
-  */
-  class SparsityPropagationKernel {
-  public:
-    // Default constructor
-    SparsityPropagationKernel();
-
-    // Destructor
-    ~SparsityPropagationKernel();
-
-    // Copy constructor and equality operator
-    // (not implemented, declared to prevent use of the default ones)
-    SparsityPropagationKernel(const SparsityPropagationKernel& sparsityPropagationKernel);
-    SparsityPropagationKernel&
-      operator=(const SparsityPropagationKernel& sparsityPropagationKernel);
-
-    // Data members (all public)
-    cl_device_id device_id;
-    cl_context context;
-    cl_command_queue command_queue;
-    cl_platform_id platform_id;
-    cl_uint ret_num_devices;
-    cl_uint ret_num_platforms;
-  };
-#endif // WITH_OPENCL
 
 /** \brief  Internal node class for SXFunction
     Do not use any internal class directly - always use the public Function
@@ -241,50 +204,6 @@ class CASADI_EXPORT SXFunction :
 
   /// With just-in-time compilation for the sparsity propagation
   bool just_in_time_sparsity_;
-
-#ifdef WITH_OPENCL
-  // Initialize sparsity propagation using OpenCL
-  void allocOpenCL();
-
-  // Propagate sparsity using OpenCL
-  void evaluateOpenCL();
-
-  // Free memory for sparsity propagation using OpenCL
-  void freeOpenCL();
-
-  // Initialize sparsity propagation using OpenCL
-  void spAllocOpenCL();
-
-  // Propagate sparsity using OpenCL
-  void spEvaluateOpenCL(bool fwd);
-
-  // Free memory for sparsity propagation using OpenCL
-  void spFreeOpenCL();
-
-  // Compile OpenCL program
-  static void compileProgram(cl_program program);
-
-  // Execute OpenCL kernel
-  static void executeKernel(cl_kernel kernel);
-
-  // OpenCL memory object for the numerical evaluation
-  cl_program program_;
-
-  // OpenCL memory object for the sparsity propagation
-  cl_program sp_program_;
-
-  // Buffers and kernels for numerical evaluation
-  std::vector<cl_mem> input_memobj_, output_memobj_;
-  cl_kernel kernel_;
-
-  // Buffers and kernels for sparsity propagation
-  std::vector<cl_mem> sp_input_memobj_, sp_output_memobj_;
-  cl_kernel sp_forward_kernel_, sp_adj_kernel_;
-
-  // OpenCL context. TODO: Nothing class specific in this class, move to a central location
-  static SparsityPropagationKernel sparsity_propagation_kernel_;
-
-#endif // WITH_OPENCL
 };
 
 
