fa815e5b216397a6f8a861d9893c87a034a1c31c
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-09-30 10:06:25 +0200

    inline matrix::get_str

--- casadi/core/matrix.hpp
+++ casadi/core/matrix.hpp
@@ -748,7 +748,7 @@ namespace casadi {
     void disp(std::ostream& stream, bool more=false) const;
 
     /// Get string representation
-    std::string get_str(bool more=false) const {
+    inline std::string get_str(bool more=false) const {
       std::stringstream ss;
       disp(ss, more);
       return ss.str();
