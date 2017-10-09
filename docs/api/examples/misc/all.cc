009055b1c051145d84250316955c71572d5081a7
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-10-09 09:34:32 +0200

    travis fix

--- docs/api/examples/misc/all.cc
+++ docs/api/examples/misc/all.cc
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
 {
   DM d = DM::zeros(3,1);
 
-  casadi_assert(!all(d).scalar());
-  casadi_assert(!any(d).scalar());
+  casadi_assert(!all(d).scalar(), "");
+  casadi_assert(!any(d).scalar(), "");
   std::cout << d << "   all: " << all(d) << "  any: " <<  any(d) << std::endl;
   
   
@@ -16,16 +16,16 @@ int main(int argc, char *argv[])
   d(1) = 0;
   d(2) = 1;
   
-  casadi_assert(!all(d).scalar());
-  casadi_assert(any(d).scalar());
+  casadi_assert(!all(d).scalar(), "");
+  casadi_assert(any(d).scalar(), "");
   std::cout << d << "   all: " << all(d) << "  any: " <<  any(d) << std::endl;
   
   d(0) = 1;
   d(1) = 1;
   d(2) = 1;
   
-  casadi_assert(all(d).scalar());
-  casadi_assert(any(d).scalar());
+  casadi_assert(all(d).scalar(), "");
+  casadi_assert(any(d).scalar(), "");
   std::cout << d << "   all: " << all(d) << "  any: " <<  any(d) << std::endl;
   
   return 0;
