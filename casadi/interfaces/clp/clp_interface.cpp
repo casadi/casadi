83481ee517a226e84d13f2a3668a51f0a062e0a1
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2018-01-14 22:03:37 +0100

    Clp: messagehandler

--- casadi/interfaces/clp/clp_interface.cpp
+++ casadi/interfaces/clp/clp_interface.cpp
@@ -174,6 +174,16 @@ namespace casadi {
     }
   }
 
+  class CasadiHandler : public CoinMessageHandler {
+    public:
+      virtual int print() ;
+  };
+
+  int CasadiHandler::print() {
+    uout() << messageBuffer() << std::endl;
+    return 0;
+  }
+
   void ClpInterface::init(const Dict& opts) {
     // Call the init method of the base class
     Conic::init(opts);
@@ -240,6 +250,9 @@ namespace casadi {
     ClpSimplex model;
     model.loadProblem(A_.size2(), A_.size1(), A_.colind(), A_.row(), A,
                       lbx, ubx, g, lba, uba, nullptr);
+
+    CasadiHandler ch;
+    model.passInMessageHandler(&ch);
 
     m->fstats.at("preprocessing").toc();
     m->fstats.at("solver").tic();
