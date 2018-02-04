944f08e4f626e0b399b3e294815f6adf5c3e35cb
jgillis <joris.gillis42@gmail.com>
2018-02-04 11:33:52 +0100

    VC2010 fixes

--- casadi/core/runtime/casadi_to_mex.hpp
+++ casadi/core/runtime/casadi_to_mex.hpp
@@ -2,29 +2,34 @@
 // SYMBOL "to_mex"
 template<typename T1>
 mxArray* casadi_to_mex(const casadi_int* sp, const T1* x) {
-  casadi_int nrow = *sp++;
-  casadi_int ncol = *sp++;
-  casadi_int nnz = sp[ncol];
-  const casadi_int *colind = sp;
-  const casadi_int *row = sp+ncol+1;
+  casadi_int nrow, ncol, nnz, c, k;
+  const casadi_int *colind, *row;
+  mxArray *p;
+#ifndef CASADI_MEX_NO_SPARSE
+  casadi_int i;
+  mwIndex *j;
+  double *d;
+#endif /* CASADI_MEX_NO_SPARSE */
+  nrow = *sp++;
+  ncol = *sp++;
+  nnz = sp[ncol];
+  colind = sp;
+  row = sp+ncol+1;
 #ifndef CASADI_MEX_NO_SPARSE
   if (nnz!=nrow*ncol) {
-    mxArray*p = mxCreateSparse(nrow, ncol, nnz, mxREAL);
-    casadi_int i;
-    mwIndex* j;
+    p = mxCreateSparse(nrow, ncol, nnz, mxREAL);
     for (i=0, j=mxGetJc(p); i<=ncol; ++i) *j++ = *colind++;
     for (i=0, j=mxGetIr(p); i<nnz; ++i) *j++ = *row++;
     if (x) {
-      double* d = (double*)mxGetData(p);
+      d = (double*)mxGetData(p);
       for (i=0; i<nnz; ++i) *d++ = casadi_to_double(*x++);
     }
     return p;
   }
 #endif /* CASADI_MEX_NO_SPARSE */
-  mxArray* p = mxCreateDoubleMatrix(nrow, ncol, mxREAL);
+  p = mxCreateDoubleMatrix(nrow, ncol, mxREAL);
   if (x) {
-    double* d = (double*)mxGetData(p);
-    casadi_int c, k;
+    d = (double*)mxGetData(p);
     for (c=0; c<ncol; ++c) {
       for (k=colind[c]; k<colind[c+1]; ++k) {
         d[row[k]+c*nrow] = casadi_to_double(*x++);
