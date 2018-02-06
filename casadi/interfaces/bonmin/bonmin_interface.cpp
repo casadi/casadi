casadi/casadi

commit c79704828c934dc2037b67dc67d54843dd6e0573

Issue #2079 Refactored NlpsolMemory::x

casadi/interfaces/bonmin/bonmin_interface.cpp
@@ -221,7 +221,6 @@ namespace casadi {
     Nlpsol::set_work(mem, arg, res, iw, w);
 
     // Work vectors
-    m->xk = w; w += nx_;
     m->lam_gk = w; w += ng_;
     m->lam_xk = w; w += nx_;
     m->gk = w; w += ng_;
@@ -383,7 +382,6 @@ namespace casadi {
 
     // Save results to outputs
     casadi_copy(&m->fk, 1, m->f);
-    casadi_copy(m->xk, nx_, m->x);
     casadi_copy(m->lam_gk, ng_, m->lam_g);
     casadi_copy(m->lam_xk, nx_, m->lam_x);
     casadi_copy(m->gk, ng_, m->g);
@@ -411,7 +409,7 @@ namespace casadi {
       if (!fcallback_.is_null()) {
         m->fstats.at("callback_fun").tic();
         if (full_callback) {
-          casadi_copy(x, nx_, m->xk);
+          casadi_copy(x, nx_, m->x);
           for (casadi_int i=0; i<nx_; ++i) {
             m->lam_xk[i] = z_U[i]-z_L[i];
           }
@@ -468,9 +466,6 @@ namespace casadi {
   finalize_solution(BonminMemory* m, TMINLP::SolverReturn status,
       const double* x, double obj_value) const {
     try {
-      // Get primal solution
-      casadi_copy(x, nx_, m->xk);
-
       // Get optimal cost
       m->fk = obj_value;
 
@@ -520,7 +515,7 @@ namespace casadi {
     try {
       // Initialize primal variables
       if (init_x) {
-        casadi_copy(m->x0, nx_, x);
+        casadi_copy(m->x, nx_, x);
       }
 
       // Initialize dual variables (simple bounds)

casadi/core/nlpsol.cpp
@@ -340,6 +340,9 @@ namespace casadi {
       }
     }
 
+    // Allocate work vectors
+    alloc_w(nx_, true);
+
     if (!fcallback_.is_null()) {
       // Consistency checks
       casadi_assert_dev(!fcallback_.is_null());
@@ -394,7 +397,7 @@ namespace casadi {
     for (casadi_int i=0; i<nx_; ++i) {
       double lb = m->lbx ? m->lbx[i] : 0;
       double ub = m->ubx ? m->ubx[i] : 0;
-      double x0 = m->x0 ? m->x0[i] : 0;
+      double x0 = m->x[i];
       casadi_assert(lb <= ub && lb!=inf && ub!=-inf,
           "Ill-posed problem detected: "
           "LBX[" + str(i) + "] <= UBX[" + str(i) + "] was violated. "
@@ -446,8 +449,31 @@ namespace casadi {
     for (auto&& s : m->fstats) s.second.reset();
     m->fstats.at(name_).tic();
 
+    // Get input pointers
+    const double *x0 = arg[NLPSOL_X0];
+    m->p = arg[NLPSOL_P];
+    m->lbx = arg[NLPSOL_LBX];
+    m->ubx = arg[NLPSOL_UBX];
+    m->lbg = arg[NLPSOL_LBG];
+    m->ubg = arg[NLPSOL_UBG];
+    m->lam_x0 = arg[NLPSOL_LAM_X0];
+    m->lam_g0 = arg[NLPSOL_LAM_G0];
+    arg += NLPSOL_NUM_IN;
+
+    // Get output pointers
+    double *x = res[NLPSOL_X];
+    m->f = res[NLPSOL_F];
+    m->g = res[NLPSOL_G];
+    m->lam_x = res[NLPSOL_LAM_X];
+    m->lam_g = res[NLPSOL_LAM_G];
+    m->lam_p = res[NLPSOL_LAM_P];
+    res += NLPSOL_NUM_OUT;
+
     // Reset the solver, prepare for solution
     setup(m, arg, res, iw, w);
+
+    // Set initial guess
+    casadi_copy(x0, nx_, m->x);
 
     // Set multipliers to nan
     casadi_fill(m->lam_x, nx_, nan);
@@ -455,14 +481,13 @@ namespace casadi {
     casadi_fill(m->lam_p, np_, nan);
 
     // Solve the NLP
-    if (solve(m)) {
-      casadi_warning("NLP solution failed");
-      return 1;
-    }
+    int flag = solve(m);
+
+    // Get optimal solution
+    casadi_copy(m->x, nx_, x);
 
     // Calculate multiplers
     if (calc_multipliers_) {
-      casadi_assert(m->x!=0, "Not implemented");
       casadi_assert(ng_==0 || m->lam_g!=0, "Not implemented");
       double lam_f = 1.;
       m->arg[0] = m->x;
@@ -495,32 +520,15 @@ namespace casadi {
     // Finalize/print statistics
     m->fstats.at(name_).toc();
     if (print_time_)  print_fstats(m);
-    return 0;
+    return flag;
   }
 
   void Nlpsol::set_work(void* mem, const double**& arg, double**& res,
                         casadi_int*& iw, double*& w) const {
     auto m = static_cast<NlpsolMemory*>(mem);
 
-    // Get input pointers
-    m->x0 = arg[NLPSOL_X0];
-    m->p = arg[NLPSOL_P];
-    m->lbx = arg[NLPSOL_LBX];
-    m->ubx = arg[NLPSOL_UBX];
-    m->lbg = arg[NLPSOL_LBG];
-    m->ubg = arg[NLPSOL_UBG];
-    m->lam_x0 = arg[NLPSOL_LAM_X0];
-    m->lam_g0 = arg[NLPSOL_LAM_G0];
-    arg += NLPSOL_NUM_IN;
-
-    // Get output pointers
-    m->x = res[NLPSOL_X];
-    m->f = res[NLPSOL_F];
-    m->g = res[NLPSOL_G];
-    m->lam_x = res[NLPSOL_LAM_X];
-    m->lam_g = res[NLPSOL_LAM_G];
-    m->lam_p = res[NLPSOL_LAM_P];
-    res += NLPSOL_NUM_OUT;
+    // Allocate memory
+    m->x = w; w += nx_;
   }
 
   std::vector<std::string> nlpsol_options(const std::string& name) {

casadi/core/nlpsol_impl.hpp
@@ -36,11 +36,17 @@ namespace casadi {
 
   /** \brief Integrator memory */
   struct CASADI_EXPORT NlpsolMemory : public OracleMemory {
+    // Bounds, given parameter values
+    const double *lbx, *ubx, *lbg, *ubg, *p;
+
+    // Current primal solution
+    double *x;
+
     // Inputs
-    const double *x0, *p, *lbx, *ubx, *lbg, *ubg, *lam_x0, *lam_g0;
+    const double *lam_x0, *lam_g0;
 
     // Outputs
-    double *x, *f, *g, *lam_x, *lam_g, *lam_p;
+    double *f, *g, *lam_x, *lam_g, *lam_p;
 
     // number of iterations
     casadi_int n_iter;

casadi/interfaces/ampl/ampl_interface.cpp
@@ -289,8 +289,7 @@ namespace casadi {
     // Primal intial guess
     nl << "x" << nx_ << "\n";
     for (casadi_int i=0; i<nx_; ++i) {
-      double x0 = m->x0 ? m->x0[i] : 0;
-      nl << i << " " << x0 << "\n";
+      nl << i << " " << m->x[i] << "\n";
     }
 
 

casadi/interfaces/blocksqp/blocksqp.cpp
@@ -464,7 +464,6 @@ namespace casadi {
 
     // Allocate memory
     alloc_w(Asp_.nnz(), true); // jac
-    alloc_w(nx_, true); // xk
     alloc_w(nx_, true); // lam_xk
     alloc_w(ng_, true); // lam_gk
     alloc_w(ng_, true); // gk
@@ -519,7 +518,6 @@ namespace casadi {
 
     // Temporary memory
     m->jac = w; w += Asp_.nnz();
-    m->xk = w; w += nx_;
     m->lam_xk = w; w += nx_;
     m->lam_gk = w; w += ng_;
     m->gk = w; w += ng_;
@@ -631,7 +629,6 @@ namespace casadi {
     initializeFilter(m);
 
     // Primal-dual initial guess
-    casadi_copy(m->x0, nx_, m->xk);
     casadi_copy(m->lam_x0, nx_, m->lam_xk);
     casadi_scal(nx_, -1., m->lam_xk);
     casadi_copy(m->lam_g0, ng_, m->lam_gk);
@@ -648,8 +645,6 @@ namespace casadi {
     if (m->f) *m->f = m->obj;
     // Get constraints at solution
     casadi_copy(m->gk, ng_, m->g);
-    // Get primal solution
-    casadi_copy(m->xk, nx_, m->x);
     // Get dual solution (simple bounds)
     if (m->lam_x) {
       casadi_copy(m->lam_xk, nx_, m->lam_x);
@@ -920,8 +915,8 @@ namespace casadi {
                                    casadi_norm_inf(ng_, m->lam_gk)));
 
     // norm of constraint violation
-    m->cNorm  = lInfConstraintNorm(m, m->xk, m->gk);
-    m->cNormS = m->cNorm /(1.0 + casadi_norm_inf(nx_, m->xk));
+    m->cNorm  = lInfConstraintNorm(m, m->x, m->gk);
+    m->cNormS = m->cNorm /(1.0 + casadi_norm_inf(nx_, m->x));
 
     if (m->tol <= opttol_ && m->cNormS <= nlinfeastol_)
       return true;
@@ -1033,9 +1028,9 @@ namespace casadi {
     m->alpha = alpha;
     m->nSOCS = nSOCS;
 
-    // Set new xk by accepting the current trial step
+    // Set new x by accepting the current trial step
     for (casadi_int k=0; k<nx_; k++) {
-      m->xk[k] = m->trial_xk[k];
+      m->x[k] = m->trial_xk[k];
       m->dxk[k] = alpha * deltaXi[k];
     }
 
@@ -1103,7 +1098,7 @@ namespace casadi {
     for (casadi_int k=0; k<10; k++) {
       // Compute new trial point
       for (casadi_int i=0; i<nx_; i++)
-        m->trial_xk[i] = m->xk[i] + alpha * m->dxk[i];
+        m->trial_xk[i] = m->x[i] + alpha * m->dxk[i];
 
       // Compute problem functions at trial point
       casadi_int info = evaluate(m, m->trial_xk, &objTrial, m->gk);
@@ -1136,14 +1131,14 @@ namespace casadi {
     double cNormTrial=0, objTrial, dfTdeltaXi=0;
 
     // Compute ||constr(xi)|| at old point
-    double cNorm = lInfConstraintNorm(m, m->xk, m->gk);
+    double cNorm = lInfConstraintNorm(m, m->x, m->gk);
 
     // Backtracking line search
     casadi_int k;
     for (k=0; k<max_line_search_; k++) {
       // Compute new trial point
       for (casadi_int i=0; i<nx_; i++)
-        m->trial_xk[i] = m->xk[i] + alpha * m->dxk[i];
+        m->trial_xk[i] = m->x[i] + alpha * m->dxk[i];
 
       // Compute grad(f)^T * deltaXi
       dfTdeltaXi = 0.0;
@@ -1279,7 +1274,7 @@ namespace casadi {
 
       // Set new SOC trial point
       for (casadi_int i=0; i<nx_; i++) {
-        m->trial_xk[i] = m->xk[i] + deltaXiSOC[i];
+        m->trial_xk[i] = m->x[i] + deltaXiSOC[i];
       }
 
       // Compute objective and ||constr(trialXiSOC)||_1 at SOC trial point
@@ -1367,7 +1362,7 @@ namespace casadi {
     // For shooting methods that means setting consistent values for
     // shooting nodes by one forward integration.
     for (casadi_int k=0; k<nx_; k++) // input: last successful step
-      m->trial_xk[k] = m->xk[k];
+      m->trial_xk[k] = m->x[k];
 
     // FIXME(@jaeandersson) Not implemented
     return -1;
@@ -1383,7 +1378,7 @@ namespace casadi {
 
     // Compute new trial point
     for (casadi_int i=0; i<nx_; i++)
-      m->trial_xk[i] = m->xk[i] + m->dxk[i];
+      m->trial_xk[i] = m->x[i] + m->dxk[i];
 
     // Compute objective and ||constr(trial_xk)|| at trial point
     std::vector<double> trialConstr(ng_, 0.);
@@ -2206,14 +2201,14 @@ namespace casadi {
     for (casadi_int i=0; i<nx_; i++) {
       double lbx = m->lbx ? m->lbx[i] : 0;
       if (lbx != inf) {
-        m->lbx_qp[i] = lbx - m->xk[i];
+        m->lbx_qp[i] = lbx - m->x[i];
       } else {
         m->lbx_qp[i] = inf;
       }
 
       double ubx = m->ubx ? m->ubx[i] : 0;
       if (ubx != inf) {
-        m->ubx_qp[i] = ubx - m->xk[i];
+        m->ubx_qp[i] = ubx - m->x[i];
       } else {
         m->ubx_qp[i] = inf;
       }
@@ -2322,9 +2317,6 @@ namespace casadi {
    * algorithms except for the Jacobian
    */
   void Blocksqp::reset_sqp(BlocksqpMemory* m) const {
-    // current iterate
-    casadi_fill(m->xk, nx_, 0.);
-
     // dual variables (for general constraints and variable bounds)
     casadi_fill(m->lam_xk, nx_, 0.);
     casadi_fill(m->lam_gk, ng_, 0.);
@@ -2473,7 +2465,7 @@ namespace casadi {
   evaluate(BlocksqpMemory* m,
            double *f, double *g,
            double *grad_f, double *jac_g) const {
-    m->arg[0] = m->xk; // x
+    m->arg[0] = m->x; // x
     m->arg[1] = m->p; // p
     m->res[0] = f; // f
     m->res[1] = g; // g

casadi/interfaces/blocksqp/blocksqp.hpp
@@ -85,7 +85,6 @@ namespace casadi {
     double lambdaStepNorm;  // norm of step in dual variables
     double tol;  // current optimality tolerance
 
-    double *xk; // variable vector
     double *lam_xk, *lam_gk; // dual variables
     double *gk;  // constraint vector
 

casadi/interfaces/ipopt/ipopt_interface.cpp
@@ -278,7 +278,6 @@ namespace casadi {
     Nlpsol::set_work(mem, arg, res, iw, w);
 
     // Work vectors
-    m->xk = w; w += nx_;
     m->lam_gk = w; w += ng_;
     m->lam_xk = w; w += nx_;
     m->gk = w; w += ng_;
@@ -365,7 +364,6 @@ namespace casadi {
 
     // Save results to outputs
     casadi_copy(&m->fk, 1, m->f);
-    casadi_copy(m->xk, nx_, m->x);
     casadi_copy(m->lam_gk, ng_, m->lam_g);
     casadi_copy(m->lam_xk, nx_, m->lam_x);
     casadi_copy(m->gk, ng_, m->g);
@@ -393,7 +391,7 @@ namespace casadi {
       if (!fcallback_.is_null()) {
         m->fstats.at("callback_fun").tic();
         if (full_callback) {
-          casadi_copy(x, nx_, m->xk);
+          casadi_copy(x, nx_, m->x);
           for (casadi_int i=0; i<nx_; ++i) {
             m->lam_xk[i] = z_U[i]-z_L[i];
           }
@@ -451,7 +449,7 @@ namespace casadi {
                     int iter_count) const {
     try {
       // Get primal solution
-      casadi_copy(x, nx_, m->xk);
+      casadi_copy(x, nx_, m->x);
 
       // Get optimal cost
       m->fk = obj_value;
@@ -499,7 +497,7 @@ namespace casadi {
     try {
       // Initialize primal variables
       if (init_x) {
-        casadi_copy(m->x0, nx_, x);
+        casadi_copy(m->x, nx_, x);
       }
 
       // Initialize dual variables (simple bounds)

casadi/interfaces/ipopt/ipopt_interface.hpp
@@ -73,7 +73,7 @@ namespace casadi {
     void* app;
 
     // Current solution
-    double *xk, lam_fk, *lam_gk, *lam_xk;
+    double *lam_fk, *lam_gk, *lam_xk;
 
     // Current calculated quantities
     double fk, *gk, *grad_fk, *jac_gk, *hess_lk, *grad_lk;

commit 36e66f44de07fe577e44ea60bc0aa5ddca7ab93d

Issue #2079 Fixed bug in Bonmin Introduced in c79704828c934dc2037b67dc67d54843dd6e0573

casadi/interfaces/bonmin/bonmin_interface.cpp
@@ -457,6 +457,9 @@ namespace casadi {
   finalize_solution(BonminMemory* m, TMINLP::SolverReturn status,
       const double* x, double obj_value) const {
     try {
+      // Get primal solution
+      casadi_copy(x, nx_, m->x);
+
       // Get optimal cost
       m->f = obj_value;
 