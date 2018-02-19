6e4426a35b298f41ad30729cfce8c5c489046e3d
Joel Andersson <j.a.e.andersson@gmail.com>
2018-02-19 14:51:08 -0600

    Issue #724 Fixed handling of inactive constraints

--- casadi/solvers/conic_as.cpp
+++ casadi/solvers/conic_as.cpp
@@ -611,11 +611,75 @@ namespace casadi {
     casadi_fill(gk, na_, 0.);
     casadi_mv(a, A_, xk, gk, 0);
 
-    // Calculate optimal cost
+    // Calculate cost
     fk = casadi_bilin(h, H_, xk, xk)/2. + casadi_dot(nx_, xk, g);
 
+    // Check if there is any active set change
+    bool has_change = false;
+    for (i=0; i<nx_+na_ && !has_change; ++i) has_change = w[i]==tau;
+
+    // Look for largest x bound violation
+    double maxviol = 0.;
+    casadi_int imaxviol;
+    for (i=0; i<nx_; ++i) {
+      lb = lbx ? lbx[i] : 0.;
+      ub = ubx ? ubx[i] : 0.;
+      if (xk[i] > ub+maxviol) {
+        maxviol = xk[i]-ub;
+        imaxviol = i;
+      } else if (xk[i] < lb-maxviol) {
+        maxviol = lb-xk[i];
+        imaxviol = i;
+      }
+    }
+
+    // Look for largest a bound violation
+    for (i=0; i<na_; ++i) {
+      lb = lba ? lba[i] : 0.;
+      ub = uba ? uba[i] : 0.;
+      if (gk[i] > ub+maxviol) {
+        maxviol = gk[i]-ub;
+        imaxviol = nx_+i;
+      } else if (gk[i] < lb-maxviol) {
+        maxviol = lb-gk[i];
+        imaxviol = nx_+i;
+      }
+    }
+
     // Print iteration progress:
-    print("Iteration %d: fk=%g, tau=%g\n", iter, fk, tau);
+    print("Iteration %d: fk=%g, tau=%g, |pr|=%g\n", iter, fk, tau, maxviol);
+
+    // Keep iterating?
+    if (has_change) continue;
+
+    // Terminate successfully?
+    if (maxviol<1e-10) break;
+
+    // Constraint on x or g?
+    if (imaxviol<nx_) {
+      // No offset
+      i = imaxviol;
+      // If already active constraint, terminate
+      if (lam_xk[i]!=0.) break;
+      // Add constraint to active set
+      if (xk[i] < lb) {
+        lam_xk[i] = fmin(lam_xk[i], -DMIN); break;
+      } else {
+        lam_xk[i] = fmax(lam_xk[i],  DMIN); break;
+      }
+    } else {
+      // Remove offset
+      i = imaxviol-nx_;
+
+      // If already active constraint, terminate
+      if (lam_ak[i]!=0.) break;
+      // Add constraint to active set
+      if (gk[i] < lb) {
+        lam_ak[i] = -DMIN;
+      } else {
+        lam_ak[i] = DMIN;
+      }
+    }
   }
 
     // Calculate optimal cost
