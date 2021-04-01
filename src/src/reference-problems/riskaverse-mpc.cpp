162c14ef751d478b3b5726f9319010838f3fc766
Pieter P <pieter.pas@outlook.com>
2021-04-01 22:27:24 +0200

    Add riskaverse test problem

---
+++ src/src/reference-problems/riskaverse-mpc.cpp
@@ -0,0 +1,128 @@
+#include <memory>
+#include <panoc-alm/reference-problems/riskaverse-mpc.hpp>
+
+namespace pa {
+namespace problems {
+
+struct RiskaverseProblem {
+    unsigned nu = 2;
+    unsigned nx = 4;
+    unsigned ns = 2;
+    unsigned ny = 3;
+    unsigned n  = nu + ns + ny;
+    unsigned m  = 3;
+    real_t Ts   = 0.05;
+
+    mat A;
+    mat B;
+
+    mat Q;
+    mat R;
+
+    vec x0;
+
+    template <class VecX>
+    auto u(VecX &&x) const {
+        return x.segment(0, nu);
+    }
+    template <class VecX>
+    auto s(VecX &&x) const {
+        return x.segment(nu, ns);
+    }
+    template <class VecX>
+    auto y(VecX &&x) const {
+        return x.segment(nu + ns, ny);
+    }
+
+    Box get_C() const {
+        Box C{vec(n), vec(n)};
+        u(C.lowerbound).fill(-10);
+        u(C.upperbound).fill(10);
+        s(C.lowerbound).fill(-inf);
+        s(C.upperbound).fill(inf);
+        y(C.lowerbound).fill(0);
+        y(C.upperbound).fill(inf);
+        return C;
+    }
+
+    Box get_D() const {
+        Box D{vec(m), vec(m)};
+        D.lowerbound.fill(-inf);
+        D.upperbound.fill(0);
+        return D;
+    }
+
+    using Diag = Eigen::DiagonalMatrix<real_t, Eigen::Dynamic, Eigen::Dynamic>;
+
+    RiskaverseProblem() {
+        A       = pa::mat::Identity(nx, nx);
+        A(0, 2) = Ts;
+        A(1, 3) = Ts;
+        B       = pa::mat::Zero(nx, nu);
+        B(2, 0) = Ts;
+        B(3, 1) = Ts;
+
+        Q = Diag(nx);
+        Q.diagonal().fill(10);
+        R = Diag(nu);
+        R.diagonal().fill(1);
+
+        x0 = vec(nx);
+        x0.fill(10);
+    }
+
+    auto mpc_dynamics(const vec &x, const vec &u) const {
+        return A * x + B * u;
+    };
+
+    real_t f(const vec &ux) const { return s(ux)(0); }
+    void grad_f(const vec &ux, vec &grad_f) const {
+        (void)ux;
+        grad_f.setZero();
+        s(grad_f)(0) = 1;
+    }
+    void g(const vec &ux, vec &g_u) const {
+        g_u(0) = y(ux)(0) - y(ux)(1) - s(ux)(0);
+        g_u(1) =
+            mpc_dynamics(x0, u(ux)).dot(Q * mpc_dynamics(x0, u(ux))) - s(ux)(1);
+        g_u(2) = x0.dot(Q * x0) + u(ux).dot(R * u(ux)) -
+                 (y(ux)(0) - y(ux)(1) - y(ux)(2) - s(ux)(1));
+    }
+    void grad_g(const vec &ux, const vec &v, vec &grad_u_v) const {
+        pa::mat grad      = pa::mat::Zero(n, m);
+        s(grad.col(0))(0) = -1;
+        y(grad.col(0))(0) = 1;
+        y(grad.col(0))(1) = -1;
+
+        u(grad.col(1))    = 2 * B.transpose() * (Q * (A * x0 + B * u(ux)));
+        s(grad.col(1))(1) = -1;
+
+        u(grad.col(2))    = 2 * (R * u(ux));
+        s(grad.col(2))(1) = 1;
+        y(grad.col(2))(0) = -1;
+        y(grad.col(2))(1) = 1;
+        y(grad.col(2))(2) = 1;
+
+        grad_u_v = grad * v;
+    }
+};
+
+Problem riskaverse_mpc_problem() {
+    auto rptr = std::make_shared<RiskaverseProblem>();
+    auto &r   = *rptr;
+    return Problem{
+        r.n,
+        r.m,
+        r.get_C(),
+        r.get_D(),
+        [rptr](const vec &ux) { return rptr->f(ux); },
+        [rptr](const vec &ux, vec &g_u) { rptr->grad_f(ux, g_u); },
+        [rptr](const vec &ux, vec &g_u) { rptr->g(ux, g_u); },
+        [rptr](const vec &ux, const vec &v, vec &grad_u_v) {
+            rptr->grad_g(ux, v, grad_u_v);
+        },
+    };
+}
+
+} // namespace problems
+} // namespace pa
--- test/test-alm-riskmpc.cpp
+++ test/test-alm-riskmpc.cpp
@@ -1,93 +1,22 @@
+#include <iomanip>
 #include <panoc-alm/decl/alm.hpp>
 #include <panoc-alm/inner/decl/panoc.hpp>
 #include <panoc-alm/inner/directions/decl/lbfgs.hpp>
+#include <panoc-alm/reference-problems/riskaverse-mpc.hpp>
 
 #include "eigen-matchers.hpp"
 
 TEST(ALM, riskaverse) {
     using namespace pa;
 
-    unsigned nu = 2;
-    unsigned nx = 4;
-    unsigned ns = 2;
-    unsigned ny = 3;
-    unsigned n  = nu + ns + ny;
-    unsigned m  = 3;
-
-    auto u = [nu](auto &&x) { return x.segment(0, nu); };
-    auto s = [nu, ns](auto &&x) { return x.segment(nu, ns); };
-    auto y = [nu, ns, ny](auto &&x) { return x.segment(nu + ns, ny); };
-
-    Box C{vec(n), vec(n)};
-    u(C.lowerbound).fill(-10);
-    u(C.upperbound).fill(10);
-    s(C.lowerbound).fill(-inf);
-    s(C.upperbound).fill(inf);
-    y(C.lowerbound).fill(0);
-    y(C.upperbound).fill(inf);
-    Box D{vec(m), vec(m)};
-    D.lowerbound.fill(-inf);
-    D.upperbound.fill(0);
-
-    real_t Ts = 0.05;
-
-    pa::mat A = pa::mat::Identity(nx, nx);
-    A(0, 2)   = Ts;
-    A(1, 3)   = Ts;
-    pa::mat B = pa::mat::Zero(nx, nu);
-    B(2, 0)   = Ts;
-    B(3, 1)   = Ts;
-
-    auto f = [&](const vec &x, const vec &u) { return A * x + B * u; };
-
-    using Diag = Eigen::DiagonalMatrix<real_t, Eigen::Dynamic, Eigen::Dynamic>;
-
-    auto Q = Diag(nx);
-    Q.diagonal().fill(10);
-    auto R = Diag(nu);
-    R.diagonal().fill(1);
-
-    auto x0 = vec(nx);
-    x0.fill(10);
-
-    auto obj_f  = [&](const vec &ux) { return s(ux)(0); };
-    auto grad_f = [&](const vec &ux, vec &grad_f) {
-        (void)ux;
-        grad_f.fill(0);
-        s(grad_f)(0) = 1;
-    };
-    auto g = [&](const vec &ux, vec &g_u) {
-        g_u(0) = y(ux)(0) - y(ux)(1) - s(ux)(0);
-        g_u(1) = f(x0, u(ux)).dot(Q * f(x0, u(ux))) - s(ux)(1);
-        g_u(2) = x0.dot(Q * x0) + u(ux).dot(R * u(ux)) -
-                 (y(ux)(0) - y(ux)(1) - y(ux)(2) - s(ux)(1));
-    };
-    auto grad_g = [&](const vec &ux, const vec &v, vec &grad_u_v) {
-        pa::mat grad      = pa::mat::Zero(n, m);
-        s(grad.col(0))(0) = -1;
-        y(grad.col(0))(0) = 1;
-        y(grad.col(0))(1) = -1;
-
-        u(grad.col(1))    = 2 * B.transpose() * (Q * (A * x0 + B * u(ux)));
-        s(grad.col(1))(1) = -1;
-
-        u(grad.col(2))    = 2 * (R * u(ux));
-        s(grad.col(2))(1) = 1;
-        y(grad.col(2))(0) = -1;
-        y(grad.col(2))(1) = 1;
-        y(grad.col(2))(2) = 1;
-
-        grad_u_v = grad * v;
-    };
-
-    Problem p{n, m, C, D, obj_f, grad_f, g, grad_g};
+    Problem p = problems::riskaverse_mpc_problem();
     ProblemWithCounters pc(p);
 
     ALMParams almparam;
     almparam.ε        = 1e-8;
     almparam.δ        = 1e-8;
     almparam.Δ        = 20; ///< Factor used in updating the penalty parameters
-    almparam.Σ₀       = 0;   ///< Initial penalty parameter
+    almparam.Σ₀       = 0;  ///< Initial penalty parameter
     almparam.σ₀       = 1e-2; ///< Initial penalty parameter factor
     almparam.ε₀       = 1e-1; ///< Initial tolerance on x
     almparam.θ        = 0.25;
@@ -98,8 +27,6 @@ TEST(ALM, riskaverse) {
     almparam.preconditioning = false;
 
     PANOCParams panocparam;
-    panocparam.Lipschitz.ε                    = 1e-11;
-    panocparam.Lipschitz.δ                    = 1e-11;
     panocparam.lbfgs_mem                      = 20;
     panocparam.max_iter                       = 1000;
     panocparam.update_lipschitz_in_linesearch = true;
@@ -111,9 +38,9 @@ TEST(ALM, riskaverse) {
 
     ALMSolver<> solver{almparam, {panocparam, lbfgsparam}};
 
-    vec x(n);
+    vec x(p.n);
     x.fill(0);
-    vec λ(m);
+    vec λ(p.m);
     λ.fill(0);
 
     ALMSolver<>::Stats stats;
@@ -127,12 +54,13 @@ TEST(ALM, riskaverse) {
     }
     auto end = std::chrono::high_resolution_clock::now();
 
-    std::cout << "u = " << x.topRows(nu).transpose() << std::endl;
-    std::cout << "x = " << x.bottomRows(nx).transpose() << std::endl;
+    std::cout << std::setprecision(17);
+    std::cout << "u = " << x.topRows(2).transpose() << std::endl;
+    std::cout << "x = " << x.bottomRows(5).transpose() << std::endl;
     std::cout << "λ = " << λ.transpose() << std::endl;
-    std::cout << "f(x) = " << obj_f(x) << std::endl;
-    auto gx = vec(m);
-    g(x, gx);
+    std::cout << "f(x) = " << p.f(x) << std::endl;
+    auto gx = vec(p.m);
+    p.g(x, gx);
     std::cout << "g(x) = " << gx.transpose() << std::endl;
     std::cout << "Inner: " << stats.inner_iterations
               << ", Outer: " << stats.outer_iterations << std::endl;
@@ -148,5 +76,6 @@ TEST(ALM, riskaverse) {
 
     std::cout << "Status: " << stats.status << std::endl;
 
-    // EXPECT_NEAR(x(0), -0.454545, 1e-4);
+    EXPECT_NEAR(x(0), -4.87804878, 5e-5);
+    EXPECT_NEAR(x(1), -4.87804878, 5e-5);
 }
