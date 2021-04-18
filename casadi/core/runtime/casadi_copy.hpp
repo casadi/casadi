d72b0f61c84b4f07a22d11d66b3fc010b7734065
Joris Gillis <joris@yacoda.com>
2021-04-18 13:50:34 +0200

    memcpy

--- casadi/core/runtime/casadi_copy.hpp
+++ casadi/core/runtime/casadi_copy.hpp
@@ -5,7 +5,8 @@ void casadi_copy(const T1* __restrict__ x, casadi_int n, T1* __restrict__ y) {
   casadi_int i;
   if (y) {
     if (x) {
-      for (i=0; i<n; ++i) *y++ = *x++;
+      //for (i=0; i<n; ++i) *y++ = *x++;
+      memcpy(y, x, n*sizeof(T1));
     } else {
       for (i=0; i<n; ++i) *y++ = 0.;
     }
