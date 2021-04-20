1edb86288251670d810f80cd0eefa15284cb754f
Pieter Pas <pieter.pas@outlook.com>
2021-04-20 22:31:43 +0200

    Final changes for poster

--- examples/poster/pga-vs-panoc-vs-newton.py
+++ examples/poster/pga-vs-panoc-vs-newton.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.ticker import LogLocator, NullFormatter
+from matplotlib import patheffects 
 import yaml
 plt.rcParams.update({
     "text.usetex": True,
@@ -8,11 +8,12 @@ plt.rcParams.update({
     "font.size": 15,
 })
 
-show_ψ = True
+show_ψ = False
 
-xlim, ylim = [2.95, 3.15], [1.85, 2.10]
-# xlim, ylim = [2.85, 3.15], [1.95, 2.25]
-levels = np.arange(0, 1, 0.025)
+# xlim, ylim = [2.95, 3.15], [1.85, 2.10]
+# xlim, ylim = [2.6, 3.4], [1.9, 3.1]
+xlim, ylim = [2.5, 3.5], [1.9, 3.1]
+levels = np.arange(0, 56, 1)
 
 def meshplot(f, xlim, ylim, delta):
     x = np.arange(xlim[0], xlim[1], delta)
@@ -26,7 +27,7 @@ def f(x, y):
     return (x**2 + y - 11)**2 + (x + y**2 - 7)**2
 
 
-X, Y, Z = meshplot(f, xlim, ylim, 0.0005)
+X, Y, Z = meshplot(f, xlim, ylim, 0.001)
 
 def load_results(name, long = False):
     if long: name = "long-" + name
@@ -43,7 +44,7 @@ labels = [  r"Projected gradient",
             r"2\textsuperscript{nd} order PANOC with L-BFGS",
             r"2\textsuperscript{nd} order PANOC with exact Newton"]
 
-x_star = np.array([3.01339473316225652574, 1.95])
+x_star = np.array([2.97248423531758647429, 2.1])
 
 fig, ax = plt.subplots(1, 1, sharex='all', sharey='all', figsize=(6.6, 7.6))
 for id, d in enumerate(data):
@@ -52,16 +53,18 @@ for id, d in enumerate(data):
         Σ = np.array(d[it]["Σ"])
         print(len(x))
         ax.contour(X, Y, Z, levels)
+        cg1 = ax.contour(X, Y, -Y, [-2.1], colors='black', linewidths=0.8, linestyles='-')
+        plt.setp(cg1.collections,
+                path_effects=[patheffects.withTickedStroke(spacing=7, linewidth=0.8)])
         ax.set_xlim(xlim)
         ax.set_ylim(ylim)
         lbl = f'{labels[id]} ({len(x) - 1} it.)'
         ax.plot(x[:, 0], x[:, 1], '.-', color=colors[id], label=lbl, markersize=8)
 ax.set_aspect('equal')
 ax.set_xticks(np.linspace(*xlim, 5, endpoint=True))
-ax.hlines(1.95, *xlim, linestyles=':', colors='k')
 ax.plot(3, 2, 'kx')
-ax.legend()
-ax.set_title('Himmelblau with box constraints')
+ax.legend(loc='upper center')
+ax.set_title('Himmelblau with box constraint')
 plt.tight_layout()
 plt.savefig("pga-vs-panoc-vs-newton.pdf")
 
@@ -80,6 +83,7 @@ for id, d in enumerate(data):
     lbl = f'{labels[id]} ({len(x) - 1} it.)'
     ax.semilogy(err, '.-', color=colors[id], label=lbl, markersize=8)
 ax.set_xlim([0, 15])
+ax.set_ylim([1e-12, 1])
 ax.set_title('Himmelblau convergence')
 ax.set_xlabel('Iteration')
 ax.set_ylabel(r'$\left\|x - x^\star \right\|_2$')
