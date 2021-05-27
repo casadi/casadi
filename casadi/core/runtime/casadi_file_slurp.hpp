a6e84a2f2973a5aab72b7cf6960334c3d9b79d42
Joris Gillis <joris@yacoda.com>
2021-05-27 22:35:30 +0200

    harden against gcc 9.3

--- casadi/core/runtime/casadi_file_slurp.hpp
+++ casadi/core/runtime/casadi_file_slurp.hpp
@@ -8,7 +8,7 @@ int casadi_file_slurp(const char* fname, casadi_int n, T1* x) {
   if (!fp) return 1;
   for (i=0;i<n;++i) {
     // C-VERBOSE
-    constexpr char SCANF_REAL[4] = sizeof(T1)==sizeof(float) ? "%g\0" : "%lg";
+    constexpr char SCANF_REAL[4] = "%lg";
     if (fscanf(fp, SCANF_REAL, x++)<=0) return 2;
   }
   fclose(fp);
