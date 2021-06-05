4ff717f0ca27aed2a439bae80e7f0383247a98e7
Pieter Pas <pieter.pas@outlook.com>
2021-06-05 16:30:29 +0200

    Cleanup examples

--- examples/CasADi/Rosenbrock/main.cpp
+++ examples/CasADi/Rosenbrock/main.cpp
@@ -7,67 +7,70 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
+    using pa::inf;
+    using pa::vec;
+
     const char *so_name =
         "examples/CasADi/Rosenbrock/librosenbrock_functions.so";
     if (argc > 1)
         so_name = argv[1];
 
-    constexpr auto inf = std::numeric_limits<double>::infinity();
-    using pa::real_t;
-    using pa::vec;
-
     pa::Problem p;
+    // Specify the problem dimensions
     p.n = 3;
     p.m = 1;
-    p.C = pa::Box{
-        vec::Constant(3, inf),
-        vec::Constant(3, -inf),
-    };
-    p.D = pa::Box{
-        vec::Constant(1, 0.),
-        vec::Constant(1, 0.),
-    };
+
+    // Specify the bounds
+    p.C.upperbound = vec::Constant(3, inf);
+    p.C.lowerbound = vec::Constant(3, -inf);
+    p.D.upperbound = vec::Constant(1, 0.);
+    p.D.lowerbound = vec::Constant(1, 0.);
+
+    // Load the functions
     p.f           = load_CasADi_objective(so_name);
     p.grad_f      = load_CasADi_gradient_objective(so_name);
     p.g           = load_CasADi_constraints(so_name);
     p.grad_g_prod = load_CasADi_gradient_constraints_prod(so_name);
 
+    // Settings for the outer augmented Lagrangian method
     pa::ALMParams almparam;
-    almparam.ε        = 1e-8;
-    almparam.δ        = 1e-8;
-    almparam.Δ        = 10;
-    almparam.Σ₀       = 2;
-    almparam.ε₀       = 1;
-    almparam.θ        = 0.25;
-    almparam.ρ        = 1e-1;
-    almparam.M        = 1e9;
-    almparam.Σ_max     = 1e9;
-    almparam.max_iter = 100;
+    almparam.ε              = 1e-8; // tolerance
+    almparam.δ              = 1e-8;
+    almparam.Δ              = 10;
+    almparam.max_iter       = 20;
+    almparam.print_interval = 1;
 
+    // Settings for the inner PANOC solver
     pa::PANOCParams panocparam;
-    panocparam.Lipschitz.ε = 1e-6;
-    panocparam.Lipschitz.δ = 1e-12;
-    panocparam.max_iter    = 500;
-
+    panocparam.max_iter       = 500;
+    panocparam.print_interval = 10;
+    // Settings for the L-BFGS algorithm used by PANOC
     pa::LBFGSParams lbfgsparam;
     lbfgsparam.memory = 10;
 
-    pa::ALMSolver<> solver{almparam, {panocparam, lbfgsparam}};
+    // Create an ALM solver using PANOC as inner solver
+    pa::ALMSolver<pa::PANOCSolver<>> solver{
+        almparam,                 // params for outer solver
+        {panocparam, lbfgsparam}, // inner solver
+    };
 
+    // Initial guess
     vec x(3);
     x << 2.5, 3.0, 0.75;
     vec y(1);
     y << 1;
 
+    // Solve the problem
     auto stats = solver(p, y, x);
 
+    // Print the results
+    std::cout << "status: " << stats.status << std::endl;
     std::cout << "x = " << x.transpose() << std::endl;
     std::cout << "y = " << y.transpose() << std::endl;
     vec g(p.m);
     p.g(x, g);
     std::cout << "g = " << g.transpose() << std::endl;
     std::cout << "f = " << p.f(x) << std::endl;
-
     std::cout << "inner: " << stats.inner.iterations << std::endl;
     std::cout << "outer: " << stats.outer_iterations << std::endl;
 }
