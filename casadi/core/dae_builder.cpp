d58a792fb52da915d2f47686055f89c373859fcd
Joel Andersson <joel@jaeandersson.com>
2021-08-25 21:41:45 -0500

    Issue #2779 Fixed ecc7e6289ab7e890cdd689f450420e8c74cbc4cb

--- casadi/core/dae_builder.cpp
+++ casadi/core/dae_builder.cpp
@@ -770,63 +770,19 @@ Function DaeBuilder::fmu_fun(const std::string& name,
     const std::vector<std::string>& name_out,
     const Dict& opts) const {
   try {
-
-    std::vector<std::vector<size_t>> id_in2(name_in.size());
-    std::vector<std::vector<size_t>> id_out2(name_out.size());
-{
-  // Collect input indices
-  for (size_t k = 0; k < name_in.size(); ++k) {
-    id_in2[k] = (*this)->ind_in(name_in[k]);
-  }
-  // Collect output indices
-  for (size_t k = 0; k < name_out.size(); ++k) {
-    if (name_out[k] == "ode") {
-      // Provide state derivative
-      id_out2[k] = (*this)->x_;
-      for (size_t& i : id_out2[k]) i = (*this)->variable(i).antiderivative;
-    } else if (name_out[k] == "alg") {
-      // Provide residual variables
-      casadi_assert(nz() == 0, "Not implemented)");
-    } else if (name_out[k] == "ydef") {
-      // Provide output
-      id_out2[k] = (*this)->y_;
-    } else {
-      casadi_error("Cannot handle " + name_out[k]);
-    }
-  }
-}
-
-
     // Collect input indices
     std::vector<std::vector<size_t>> id_in(comp_in.size());
     for (size_t k = 0; k < comp_in.size(); ++k) {
       id_in[k].reserve(comp_in[k].size());
       for (const std::string& s : comp_in[k]) id_in[k].push_back(find(s));
-
-      casadi_assert(id_in[k].size() == id_in2[k].size(), "Here: " + str(k));
-      for (size_t i = 0; i < id_in[k].size(); ++i) {
-        casadi_assert(id_in[k][i] == id_in2[k][i], "Here: " + str(k) + ", " + str(i));
-      }
-
     }
-
     // Collect output indices
     std::vector<std::vector<size_t>> id_out(comp_out.size());
     for (size_t k = 0; k < comp_out.size(); ++k) {
       id_out[k].reserve(comp_out[k].size());
       for (const std::string& s : comp_out[k]) id_out[k].push_back(find(s));
-
-      casadi_assert(id_out[k].size() == id_out2[k].size(), "Here: " + str(k));
-      for (size_t i = 0; i < id_out[k].size(); ++i) {
-        casadi_assert(id_out[k][i] == id_out2[k][i], "Here: " + str(k) + ", " + str(i));
-      }
-
     }
     // Call internal routine
-
-
-
-
     return (*this)->fmu_fun(name, id_in, id_out, name_in, name_out, opts);
   } catch (std::exception& e) {
     THROW_ERROR("fmu_fun", e.what());
