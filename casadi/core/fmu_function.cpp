b95573c6ed5e49562c610bcdd24f3e96c9342fc0
Joel Andersson <joel@jaeandersson.com>
2021-09-19 09:31:59 -0500

    Issue #2779 Moved setting variable to FmuFunction constructor

--- casadi/core/fmu_function.cpp
+++ casadi/core/fmu_function.cpp
@@ -167,6 +167,67 @@ int Fmu::checkout() {
   m.c = instantiate();
   // Reset solver
   setup_experiment(mem);
+  // Initialize inputs and parameters before initialization
+  for (const Variable& v : self_.variables_) {
+    // Skip if the wrong type
+    if (v.causality != Variable::PARAMETER && v.causality != Variable::INPUT) continue;
+    // If nan - variable has not been set - keep default value
+    if (std::isnan(v.value)) continue;
+    // Convert to expected type
+    fmi2ValueReference vr = v.value_reference;
+    // Return status
+    fmi2Status status;
+    // Get value
+    switch (v.type) {
+      case Variable::REAL:
+        {
+          // Real
+          fmi2Real value = v.value;
+          status = set_real_(m.c, &vr, 1, &value);
+          if (status != fmi2OK) {
+            casadi_warning("fmi2SetReal failed for " + v.name + ", value: " + str(v.value));
+            return -1;
+          }
+          break;
+        }
+      case Variable::INTEGER:
+      case Variable::ENUM:
+        {
+          // Integer: Convert to double
+          fmi2Integer value = static_cast<casadi_int>(v.value);
+          status = set_integer_(m.c, &vr, 1, &value);
+          if (status != fmi2OK) {
+            casadi_warning("fmi2SetInteger failed for " + v.name + ", value: " + str(v.value));
+            return -1;
+          }
+          break;
+        }
+      case Variable::BOOLEAN:
+        {
+          // Boolean: Convert to double
+          fmi2Boolean value = static_cast<casadi_int>(v.value);
+          status = set_boolean_(m.c, &vr, 1, &value);
+          if (status != fmi2OK) {
+            casadi_warning("fmi2SetBoolean failed for " + v.name + ", value: " + str(v.value));
+            return -1;
+          }
+          break;
+        }
+      case Variable::STRING:
+        {
+          // String
+          fmi2String value = v.stringvalue.c_str();
+          status = set_string_(m.c, &vr, 1, &value);
+          if (status != fmi2OK) {
+            casadi_warning("fmi2SetString failed for " + v.name + ", value: " + v.stringvalue);
+            return -1;
+          }
+          break;
+        }
+      default:
+        casadi_warning("Ignoring " + v.name + ", type: " + to_string(v.type));
+    }
+  }
   // Always initialize before first call
   m.need_init = true;
   // Allocate/reset value buffer
@@ -293,65 +354,6 @@ int Fmu::eval(int mem, const FmuFunction& f) {
     // DaeBuilder instance
     casadi_assert(f.dae_.alive(), "DaeBuilder instance has been deleted");
     auto dae = static_cast<DaeBuilderInternal*>(f.dae_->raw_);
-    // Initialize inputs and parameters before initialization
-    for (const Variable& v : dae->variables_) {
-      // Skip if the wrong type
-      if (v.causality != Variable::PARAMETER && v.causality != Variable::INPUT) continue;
-      // If nan - variable has not been set - keep default value
-      if (std::isnan(v.value)) continue;
-      // Convert to expected type
-      fmi2ValueReference vr = v.value_reference;
-      // Get value
-      switch (v.type) {
-        case Variable::REAL:
-          {
-            // Real
-            fmi2Real value = v.value;
-            status = set_real_(m.c, &vr, 1, &value);
-            if (status != fmi2OK) {
-              casadi_warning("fmi2SetReal failed for " + v.name + ", value: " + str(v.value));
-              return 1;
-            }
-            break;
-          }
-        case Variable::INTEGER:
-        case Variable::ENUM:
-          {
-            // Integer: Convert to double
-            fmi2Integer value = static_cast<casadi_int>(v.value);
-            status = set_integer_(m.c, &vr, 1, &value);
-            if (status != fmi2OK) {
-              casadi_warning("fmi2SetInteger failed for " + v.name + ", value: " + str(v.value));
-              return 1;
-            }
-            break;
-          }
-        case Variable::BOOLEAN:
-          {
-            // Boolean: Convert to double
-            fmi2Boolean value = static_cast<casadi_int>(v.value);
-            status = set_boolean_(m.c, &vr, 1, &value);
-            if (status != fmi2OK) {
-              casadi_warning("fmi2SetBoolean failed for " + v.name + ", value: " + str(v.value));
-              return 1;
-            }
-            break;
-          }
-        case Variable::STRING:
-          {
-            // String
-            fmi2String value = v.stringvalue.c_str();
-            status = set_string_(m.c, &vr, 1, &value);
-            if (status != fmi2OK) {
-              casadi_warning("fmi2SetString failed for " + v.name + ", value: " + v.stringvalue);
-              return 1;
-            }
-            break;
-          }
-        default:
-          casadi_warning("Ignoring " + v.name + ", type: " + to_string(v.type));
-      }
-    }
     // Set all variables before initialization
     status = set_real_(m.c, get_ptr(m.vr_in_), n_set, get_ptr(m.v_in_));
     if (status != fmi2OK) {
