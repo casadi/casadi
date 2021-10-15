86a82642b1b9512821676ff027cc27167922dd46
Pieter Pas <pieter.pas@outlook.com>
2021-10-15 10:42:20 +0200

    Build Python wheel with debug info, add GDB source file mappings for debugging in Python

--- scripts/ci/wheel-local.sh
+++ scripts/ci/wheel-local.sh
@@ -3,8 +3,8 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 set -ex
 
-image=panocpy-wheel-local-img2
-container=panocpy-wheel-local-cnt2
+image=panocpy-wheel-local-img6
+container=panocpy-wheel-local-cnt6
 
 if [ -z $(docker image ls -q $image) ]; then
     docker create --interactive \
@@ -61,7 +61,7 @@ FC=gfortran \
 CXXFLAGS="-march=skylake -static-libstdc++ -static-libgcc" \
 CFLAGS="-march=skylake -static-libgcc" \
 LDFLAGS="-static-libstdc++ -static-libgcc" \
-    python setup.py bdist_wheel --build-type Release -j$(nproc) --generator Ninja --skip-generator-test
+    python setup.py bdist_wheel --build-type RelWithDebInfo -j$(nproc) --generator Ninja --skip-generator-test
 cpv=$(echo $PYTHON_VERSION | awk -F. '{print $1 $2}')
 LD_LIBRARY_PATH=$VIRTUAL_ENV/lib \
     auditwheel repair --plat manylinux_2_27_x86_64 \
