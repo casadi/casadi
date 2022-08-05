5cfc5bc78c744c8a50c97c4142036002a65f4270
Pieter Pas <pieter.pas@outlook.com>
2022-08-05 15:21:22 +0200

    [Docs] Add logo

---
+++ doxygen/logo/make_images.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+latexmk -pdflatex alpaqa_favicon.tex
+latexmk -pdflatex alpaqa_logo.tex
+inkscape alpaqa_favicon.pdf --pdf-poppler --export-text-to-path --export-plain-svg -o alpaqa_favicon.svg
+inkscape alpaqa_logo.pdf --pdf-poppler --export-text-to-path --export-plain-svg -o alpaqa_logo.svg
+inkscape alpaqa_favicon.pdf --pdf-poppler --export-type png -w 256 -h 256
