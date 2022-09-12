90684a8848025d18ee027b0bb5c04d9115171677
david0oo <david.kiessling@kuleuven.be>
2022-09-12 21:56:57 +0200

    added feasible solver files

---
+++ casadi/core/runtime/casadi_feasiblesqpmethod.hpp
@@ -0,0 +1,110 @@
+// NOLINT(legal/copyright)
+
+// C-REPLACE "casadi_nlpsol_prob<T1>" "struct casadi_nlpsol_prob"
+
+// SYMBOL "feasiblesqpmethod_prob"
+template<typename T1>
+struct casadi_feasiblesqpmethod_prob {
+  const casadi_nlpsol_prob<T1>* nlp;
+  // Sparsity patterns
+  const casadi_int *sp_h, *sp_a, *sp_hr;
+  casadi_int merit_memsize;
+  casadi_int max_iter_ls;
+};
+// C-REPLACE "casadi_feasiblesqpmethod_prob<T1>" "struct casadi_feasiblesqpmethod_prob"
+
+
+// SYMBOL "feasiblesqpmethod_data"
+template<typename T1>
+struct casadi_feasiblesqpmethod_data {
+  // Problem structure
+  const casadi_feasiblesqpmethod_prob<T1>* prob;
+
+  T1* z_cand;
+  // Lagrange gradient in the next iterate
+  T1 *gLag, *gLag_old;
+  // Gradient of the objective
+  T1 *gf;
+  // Bounds of the QP
+  T1 *lbdz, *ubdz;
+  // QP solution
+  T1 *dx, *dlam;
+  // Hessian approximation
+  T1 *Bk;
+  // Jacobian
+  T1* Jk;
+  // merit_mem
+  T1* merit_mem;
+};
+// C-REPLACE "casadi_feasiblesqpmethod_data<T1>" "struct casadi_feasiblesqpmethod_data"
+
+
+// SYMBOL "sqpmethod_work"
+template<typename T1>
+void casadi_feasiblesqpmethod_work(const casadi_feasiblesqpmethod_prob<T1>* p,
+    casadi_int* sz_iw, casadi_int* sz_w) {
+  // Local variables
+  casadi_int nnz_h, nnz_a, nx, ng;
+  nnz_h = p->sp_h[2+p->sp_h[1]];
+  nnz_a = p->sp_a[2+p->sp_a[1]];
+  nx = p->nlp->nx;
+  ng = p->nlp->ng;
+
+  // Reset sz_w, sz_iw
+  *sz_w = *sz_iw = 0;
+  if (p->max_iter_ls>0) *sz_w += nx + ng; // z_cand
+  // Lagrange gradient in the next iterate
+  *sz_w += nx; // gLag
+  *sz_w += nx; // gLag_old
+  // Gradient of the objective
+  *sz_w += nx; // gf
+  // Bounds of the QP
+  *sz_w += nx + ng; // lbdz
+  *sz_w += nx + ng; // ubdz
+  // QP solution
+  *sz_w += nx; // dx
+  *sz_w += nx + ng; // dlam
+  // Hessian approximation
+  *sz_w += nnz_h; // Bk
+  // Jacobian
+  *sz_w += nnz_a; // Jk
+  // merit_mem
+  if (p->max_iter_ls>0) *sz_w += p->merit_memsize;
+}
+
+// SYMBOL "sqpmethod_init"
+template<typename T1>
+void casadi_feasiblesqpmethod_init(casadi_feasiblesqpmethod_data<T1>* d, casadi_int** iw, T1** w) {
+  // Local variables
+  casadi_int nnz_h, nnz_a, nx, ng;
+  const casadi_feasiblesqpmethod_prob<T1>* p = d->prob;
+  // Get matrix number of nonzeros
+  nnz_h = p->sp_h[2+p->sp_h[1]];
+  nnz_a = p->sp_a[2+p->sp_a[1]];
+  nx = p->nlp->nx;
+  ng = p->nlp->ng;
+  if (p->max_iter_ls>0) {
+    d->z_cand = *w;
+    *w += nx + ng;
+  }
+  // Lagrange gradient in the next iterate
+  d->gLag = *w; *w += nx;
+  d->gLag_old = *w; *w += nx;
+  // Gradient of the objective
+  d->gf = *w; *w += nx;
+  // Bounds of the QP
+  d->lbdz = *w; *w += nx + ng;
+  d->ubdz = *w; *w += nx + ng;
+  // QP solution
+  d->dx = *w; *w += nx;
+  d->dlam = *w; *w += nx + ng;
+  // Hessian approximation
+  d->Bk = *w; *w += nnz_h;
+  // Jacobian
+  d->Jk = *w; *w += nnz_a;
+  // merit_mem
+  if (p->max_iter_ls>0) {
+    d->merit_mem = *w;
+    *w += p->merit_memsize;
+  }
+}
---
+++ casadi/solvers/feasiblesqpmethod.hpp
@@ -0,0 +1,201 @@
+/*
+ *    This file is part of CasADi.
+ *
+ *    CasADi -- A symbolic framework for dynamic optimization.
+ *    Copyright (C) 2010-2014 Joel Andersson, Joris Gillis, Moritz Diehl,
+ *                            K.U. Leuven. All rights reserved.
+ *    Copyright (C) 2011-2014 Greg Horn
+ *
+ *    CasADi is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    CasADi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with CasADi; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+
+#ifndef CASADI_FEASIBLESQPMETHOD_HPP
+#define CASADI_FEASIBLESQPMETHOD_HPP
+
+#include "casadi/core/nlpsol_impl.hpp"
+#include <casadi/solvers/casadi_nlpsol_feasiblesqpmethod_export.h>
+
+/** \defgroup plugin_Nlpsol_feasiblesqpmethod
+ An implementation of FP-SQP
+*/
+
+/** \pluginsection{Nlpsol,sqpmethod} */
+
+/// \cond INTERNAL
+namespace casadi {
+
+  struct CASADI_NLPSOL_FEASIBLESQPMETHOD_EXPORT FeasiblesqpmethodMemory : public NlpsolMemory {
+    // Problem data structure
+    casadi_feasiblesqpmethod_data<double> d;
+    /// Hessian regularization
+    double reg;
+
+    /// Linesearch parameters
+    double sigma;
+
+    casadi_int merit_ind;
+    /// Last return status
+    const char* return_status;
+
+    /// Iteration count
+    int iter_count;
+  };
+
+  /** \brief  \pluginbrief{Nlpsol,feasiblesqpmethod}
+  *  @copydoc NLPSolver_doc
+  *  @copydoc plugin_Nlpsol_feasiblesqpmethod
+  */
+  class CASADI_NLPSOL_FEASIBLESQPMETHOD_EXPORT Feasiblesqpmethod : public Nlpsol {
+  public:
+    explicit Feasiblesqpmethod(const std::string& name, const Function& nlp);
+    ~FeasibleSqpmethod() override;
+
+    // Get name of the plugin
+    const char* plugin_name() const override { return "sqpmethod";}
+
+    // Name of the class
+    std::string class_name() const override { return "Sqpmethod";}
+
+    /** \brief  Create a new NLP Solver */
+    static Nlpsol* creator(const std::string& name, const Function& nlp) {
+      return new Sqpmethod(name, nlp);
+    }
+
+    ///@{
+    /** \brief Options */
+    static const Options options_;
+    const Options& get_options() const override { return options_;}
+    ///@}
+
+    /// Get all statistics
+    Dict get_stats(void* mem) const override;
+
+    // Initialize the solver
+    void init(const Dict& opts) override;
+
+    /** \brief Create memory block */
+    void* alloc_mem() const override { return new SqpmethodMemory();}
+
+    /** \brief Initalize memory block */
+    int init_mem(void* mem) const override;
+
+    /** \brief Free memory block */
+    void free_mem(void *mem) const override { delete static_cast<SqpmethodMemory*>(mem);}
+
+    /** \brief Set the (persistent) work vectors */
+    void set_work(void* mem, const double**& arg, double**& res,
+                          casadi_int*& iw, double*& w) const override;
+
+    // Solve the NLP
+    int solve(void* mem) const override;
+
+    // Memory structure
+    casadi_sqpmethod_prob<double> p_;
+
+    /// QP solver for the subproblems
+    Function qpsol_;
+
+    /// Exact Hessian?
+    bool exact_hessian_;
+
+    /// Maximum block size of Hessian
+    casadi_int block_size_ = 0;
+
+    /// Maximum, minimum number of SQP iterations
+    casadi_int max_iter_, min_iter_;
+
+    /// Memory size of L-BFGS method
+    casadi_int lbfgs_memory_;
+
+    /// Tolerance of primal and dual infeasibility
+    double tol_pr_, tol_du_;
+
+    /// Minimum step size allowed
+    double min_step_size_;
+
+    /// Linesearch parameters
+    ///@{
+    double c1_;
+    double beta_;
+    casadi_int max_iter_ls_;
+    casadi_int merit_memsize_;
+    ///@}
+
+    // Print options
+    bool print_header_, print_iteration_, print_status_;
+
+    // Hessian Sparsity
+    Sparsity Hsp_;
+
+    // Jacobian sparsity
+    Sparsity Asp_;
+
+    /// Data for convexification
+    ConvexifyData convexify_data_;
+
+    /// convexify?
+    bool convexify_;
+
+    /** \brief Generate code for the function body */
+    void codegen_body(CodeGenerator& g) const override;
+
+    /** \brief Generate code for the declarations of the C function */
+    void codegen_declarations(CodeGenerator& g) const override;
+
+    /// Access Conic
+    const Function getConic() const { return qpsol_;}
+
+    /// Print iteration header
+    void print_iteration() const;
+
+    /// Print iteration
+    void print_iteration(casadi_int iter, double obj, double pr_inf, double du_inf,
+                         double dx_norm, double rg, casadi_int ls_trials, bool ls_success) const;
+
+    // Solve the QP subproblem
+    virtual void solve_QP(SqpmethodMemory* m, const double* H, const double* g,
+                          const double* lbdz, const double* ubdz,
+                          const double* A,
+                          double* x_opt, double* dlam) const;
+
+
+    // Solve the QP subproblem
+    void codegen_qp_solve(CodeGenerator& cg, const std::string& H, const std::string& g,
+              const std::string& lbdz, const std::string& ubdz,
+              const std::string& A, const std::string& x_opt, const std::string& dlam) const;
+
+    /// A documentation string
+    static const std::string meta_doc;
+
+
+    /** \brief Serialize an object without type information */
+    void serialize_body(SerializingStream &s) const override;
+
+    /** \brief Deserialize into MX */
+    static ProtoFunction* deserialize(DeserializingStream& s) { return new Sqpmethod(s); }
+
+  protected:
+    /** \brief Deserializing constructor */
+    explicit Sqpmethod(DeserializingStream& s);
+
+  private:
+    void set_sqpmethod_prob();
+  };
+
+} // namespace casadi
+/// \endcond
+#endif // CASADI_SQPMETHOD_HPP
