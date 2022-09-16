f8471bf4ea913a424ab5de45f9215cc1ce1dfcf9
Joris Gillis <joris.gillis42@gmail.com>
2022-09-16 22:06:37 +0200

    Docstring updates

--- casadi/core/calculus.hpp
+++ casadi/core/calculus.hpp
@@ -259,9 +259,7 @@ namespace casadi {
   ///@}
 
   ///@{
-  /** \brief  CasADi additions
-
-      \identifier{1g5} */
+  /** CasADi additions */
   inline double simplify(double x) { return x;}
   inline double constpow(double x, double y) { return pow(x, y);}
   inline double printme(double x, double y) {
