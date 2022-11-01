bedf5527bdb68254bcb44637195ca9df3a55a33a
Pieter Pas <pieter.pas@outlook.com>
2022-11-01 22:44:07 +0100

    Resolve conversion warning quadmath

--- src/include/alpaqa/util/quadmath/quadmath-print.hpp
+++ src/include/alpaqa/util/quadmath/quadmath-print.hpp
@@ -10,7 +10,7 @@
 namespace std {
 inline ostream &operator<<(ostream &os, __float128 f) {
     char buf[128];
-    int precision          = os.precision();
+    auto precision         = static_cast<int>(os.precision());
     [[maybe_unused]] int n = quadmath_snprintf(buf, sizeof(buf), "%#.*Qg", precision, f);
     assert((size_t)n < sizeof buf);
     return os << buf;
