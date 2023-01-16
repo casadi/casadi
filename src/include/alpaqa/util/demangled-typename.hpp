df81667d3d94ad0435a9e11ca45ad3d153aa14e3
Pieter Pas <pieter.pas@outlook.com>
2023-01-16 14:25:21 +0100

    Export demangled_typename

--- src/include/alpaqa/util/demangled-typename.hpp
+++ src/include/alpaqa/util/demangled-typename.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <alpaqa/export.hpp>
+
 #include <string>
 #include <typeinfo>
 
 /// Get the pretty name of the given type as a string.
-std::string demangled_typename(const std::type_info &t);
+ALPAQA_EXPORT std::string demangled_typename(const std::type_info &t);
