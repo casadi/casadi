78eb75a69273859cdb9239c4137839d374259287
Joris Gillis <joris.gillis42@gmail.com>
2023-02-01 15:07:58 +0100

    Codesigning for octave

--- misc/codesign.py
+++ misc/codesign.py
@@ -8,7 +8,7 @@ dir = sys.argv[2]
 # Recursively look for all shared libraries in `dir`
 for root, dirs, files in os.walk(dir):
     for file in files:
-        if file.endswith(".dylib") or file.endswith(".mexmaci64") or file.endswith(".mexmaca64") or file.endswith(".so"):
+        if file.endswith(".dylib") or file.endswith(".mexmaci64") or file.endswith(".mexmaca64") or file.endswith(".so") or file.endswith(".mex"):
             # Get the full path to the library
             path = os.path.join(root, file)
 
