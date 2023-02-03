b2936639402d61eb3dbfe9f7fa94ba72c77509b4
Pieter Pas <pieter.pas@outlook.com>
2023-02-03 01:16:32 +0100

    CMake installation paths, postfix

--- src/cmake/Install.cmake
+++ src/cmake/Install.cmake
@@ -1,54 +1,61 @@
 include(GNUInstallDirs)
 include(${PROJECT_SOURCE_DIR}/cmake/Debug.cmake)
 
-set(INSTALL_CMAKE_DIR "${CMAKE_INSTALL_LIBDIR}/cmake/alpaqa")
+set(ALPAQA_INSTALL_CMAKEDIR "${CMAKE_INSTALL_LIBDIR}/cmake/alpaqa"
+    CACHE STRING "Installation directory for CMake configuration files")
+set(ALPAQA_INSTALL_BINDIR "${CMAKE_INSTALL_BINDIR}"
+    CACHE STRING "Installation directory for binaries and DLLs")
+set(ALPAQA_INSTALL_LIBDIR "${CMAKE_INSTALL_LIBDIR}"
+    CACHE STRING "Installation directory for archives and libraries")
+set(ALPAQA_INSTALL_INCLUDEDIR "${CMAKE_INSTALL_INCLUDEDIR}"
+    CACHE STRING "Installation directory for headers")
 
 # Add the alpaqa library to the "export-set", install the library files
 install(TARGETS ${ALPAQA_INSTALL_TARGETS} warnings
     EXPORT alpaqaTargets
-    RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}"
+    RUNTIME DESTINATION "${ALPAQA_INSTALL_BINDIR}"
         COMPONENT shlib
-    LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}"
+    LIBRARY DESTINATION "${ALPAQA_INSTALL_LIBDIR}"
         COMPONENT shlib
-    ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}" 
+    ARCHIVE DESTINATION "${ALPAQA_INSTALL_LIBDIR}" 
         COMPONENT lib)
 
 # Install the header files
 install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/alpaqa/include/"
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+    DESTINATION "${ALPAQA_INSTALL_INCLUDEDIR}"
         COMPONENT dev
     FILES_MATCHING REGEX "/.*\.(h|[hti]pp)$")
 install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/interop/casadi/include/"
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+    DESTINATION "${ALPAQA_INSTALL_INCLUDEDIR}"
         COMPONENT dev
     FILES_MATCHING REGEX "/.*\.(h|[hti]pp)$")
 install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/interop/dl/include/"
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+    DESTINATION "${ALPAQA_INSTALL_INCLUDEDIR}"
         COMPONENT dev
     FILES_MATCHING REGEX "/.*\.(h|[hti]pp)$")
 install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/interop/dl-api/include/"
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+    DESTINATION "${ALPAQA_INSTALL_INCLUDEDIR}"
         COMPONENT dl_dev
     FILES_MATCHING REGEX "/.*\.(h|[hti]pp)$")
 install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/export/alpaqa"
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+    DESTINATION "${ALPAQA_INSTALL_INCLUDEDIR}"
         COMPONENT dev
     FILES_MATCHING REGEX "/.*\.(h|[hti]pp)$")
 
 # Install the debug files
-foreach(TGT IN LISTS ALPAQA_INSTALL_TARGETS)
-    get_target_property(TGT_TYPE ${TGT} TYPE)
-    if (${TGT_TYPE} STREQUAL "SHARED_LIBRARY")
-        alpaqa_install_debug_syms(${TGT} debug
-                                  ${CMAKE_INSTALL_LIBDIR}
-                                  ${CMAKE_INSTALL_BINDIR})
+foreach(target IN LISTS ALPAQA_INSTALL_TARGETS)
+    get_target_property(target_type ${target} TYPE)
+    if (${target_type} STREQUAL "SHARED_LIBRARY")
+        alpaqa_install_debug_syms(${target} debug
+                                  ${ALPAQA_INSTALL_LIBDIR}
+                                  ${ALPAQA_INSTALL_BINDIR})
     endif()
 endforeach()
 
 # Install the export set for use with the install tree
 install(EXPORT alpaqaTargets 
     FILE alpaqaTargets.cmake
-    DESTINATION "${INSTALL_CMAKE_DIR}" 
+    DESTINATION "${ALPAQA_INSTALL_CMAKEDIR}" 
         COMPONENT dev
     NAMESPACE alpaqa::)
 
@@ -57,7 +64,7 @@ include(CMakePackageConfigHelpers)
 configure_package_config_file(
     "${CMAKE_CURRENT_SOURCE_DIR}/cmake/Config.cmake.in"
     "${PROJECT_BINARY_DIR}/alpaqaConfig.cmake"
-    INSTALL_DESTINATION "${INSTALL_CMAKE_DIR}"
+    INSTALL_DESTINATION "${ALPAQA_INSTALL_CMAKEDIR}"
     NO_SET_AND_CHECK_MACRO
     NO_CHECK_REQUIRED_COMPONENTS_MACRO)
 write_basic_package_version_file(
@@ -69,7 +76,7 @@ write_basic_package_version_file(
 install(FILES
     "${PROJECT_BINARY_DIR}/alpaqaConfig.cmake"
     "${PROJECT_BINARY_DIR}/alpaqaConfigVersion.cmake"
-    DESTINATION "${INSTALL_CMAKE_DIR}" 
+    DESTINATION "${ALPAQA_INSTALL_CMAKEDIR}" 
         COMPONENT dev)
 
 # Add all targets to the build tree export set
