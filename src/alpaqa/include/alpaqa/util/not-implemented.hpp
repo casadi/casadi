5f79afa72656e7432f3ba4db0b00fe66f2279647
Pieter Pas <pieter.pas@outlook.com>
2023-02-26 10:13:56 +0100

    Export not_implemented_error

--- src/alpaqa/include/alpaqa/util/not-implemented.hpp
+++ src/alpaqa/include/alpaqa/util/not-implemented.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
+#include <alpaqa/export.h>
 #include <stdexcept>
 
 namespace alpaqa {
 
-struct not_implemented_error : std::logic_error {
+struct ALPAQA_EXPORT not_implemented_error : std::logic_error {
     using std::logic_error::logic_error;
 };
 
