1bfba89982b2d6d6f81a70d21c03ff848524df5c
Pieter Pas <pieter.pas@outlook.com>
2023-03-02 15:48:36 +0100

    IPOPT_INTERFACE_EXPORT → IPOPT_ADAPTER_EXPORT

--- src/interop/ipopt/include/alpaqa/ipopt-adapter.hpp
+++ src/interop/ipopt/include/alpaqa/ipopt-adapter.hpp
@@ -8,7 +8,7 @@
 namespace alpaqa {
 
 /// Based on https://coin-or.github.io/Ipopt/INTERFACES.html
-class IPOPT_INTERFACE_EXPORT IpoptAdapter : public Ipopt::TNLP {
+class IPOPT_ADAPTER_EXPORT IpoptAdapter : public Ipopt::TNLP {
   public:
     USING_ALPAQA_CONFIG(DefaultConfig);
     using Problem = TypeErasedProblem<config_t>;
@@ -36,35 +36,29 @@ class IPOPT_INTERFACE_EXPORT IpoptAdapter : public Ipopt::TNLP {
     bool get_bounds_info(Index n, Number *x_l, Number *x_u, Index m,
                          Number *g_l, Number *g_u) override;
 
-    bool get_starting_point(Index n, bool init_x, Number *x,
-                            [[maybe_unused]] bool init_z,
-                            [[maybe_unused]] Number *z_L,
-                            [[maybe_unused]] Number *z_U,
-                            [[maybe_unused]] Index m,
-                            [[maybe_unused]] bool init_lambda,
-                            [[maybe_unused]] Number *lambda) override;
+    bool get_starting_point(Index n, bool init_x, Number *x, bool init_z,
+                            Number *z_L, Number *z_U, Index m, bool init_lambda,
+                            Number *lambda) override;
 
-    bool eval_f(Index n, const Number *x, [[maybe_unused]] bool new_x,
+    bool eval_f(Index n, const Number *x, bool new_x,
                 Number &obj_value) override;
 
-    bool eval_grad_f(Index n, const Number *x, [[maybe_unused]] bool new_x,
+    bool eval_grad_f(Index n, const Number *x, bool new_x,
                      Number *grad_f) override;
 
-    bool eval_g(Index n, const Number *x, [[maybe_unused]] bool new_x, Index m,
+    bool eval_g(Index n, const Number *x, bool new_x, Index m,
                 Number *g) override;
 
-    bool eval_jac_g(Index n, const Number *x, [[maybe_unused]] bool new_x,
-                    Index m, Index nele_jac, Index *iRow, Index *jCol,
+    bool eval_jac_g(Index n, const Number *x, bool new_x, Index m,
+                    Index nele_jac, Index *iRow, Index *jCol,
                     Number *values) override;
 
-    bool eval_h(Index n, const Number *x, [[maybe_unused]] bool new_x,
-                Number obj_factor, Index m, const Number *lambda,
-                [[maybe_unused]] bool new_lambda, Index nele_hess, Index *iRow,
-                Index *jCol, Number *values) override;
+    bool eval_h(Index n, const Number *x, bool new_x, Number obj_factor,
+                Index m, const Number *lambda, bool new_lambda, Index nele_hess,
+                Index *iRow, Index *jCol, Number *values) override;
 
     void finalize_solution(Ipopt::SolverReturn status, Index n, const Number *x,
-                           [[maybe_unused]] const Number *z_L,
-                           [[maybe_unused]] const Number *z_U, Index m,
+                           const Number *z_L, const Number *z_U, Index m,
                            const Number *g, const Number *lambda,
                            Number obj_value, const Ipopt::IpoptData *ip_data,
                            Ipopt::IpoptCalculatedQuantities *ip_cq) override;
@@ -76,8 +70,8 @@ class IPOPT_INTERFACE_EXPORT IpoptAdapter : public Ipopt::TNLP {
         indexvec inner_idx, outer_ptr;
     } sparsity_J, sparsity_H;
 
-    static void set_sparsity(Index n, Index m, [[maybe_unused]] Index nele,
-                             Index *iRow, Index *jCol, const Sparsity &sp);
+    static void set_sparsity(Index n, Index m, Index nele, Index *iRow,
+                             Index *jCol, const Sparsity &sp);
 };
 
 } // namespace alpaqa
