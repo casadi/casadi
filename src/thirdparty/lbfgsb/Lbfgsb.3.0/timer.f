571018bf0cb48313bb60f5d4ac96ed638cac2857
Pieter Pas <pieter.pas@outlook.com>
2023-03-09 20:38:45 +0100

    Add Lbfgsb.3.0

---
+++ src/thirdparty/lbfgsb/Lbfgsb.3.0/timer.f
@@ -0,0 +1,32 @@
+c                                                                                      
+c  L-BFGS-B is released under the “New BSD License” (aka “Modified BSD License”        
+c  or “3-clause license”)                                                              
+c  Please read attached file License.txt                                               
+c                                        
+      subroutine timer(ttime)
+      double precision ttime
+c
+      real temp
+c
+c     This routine computes cpu time in double precision; it makes use of 
+c     the intrinsic f90 cpu_time therefore a conversion type is
+c     needed.
+c
+c           J.L Morales  Departamento de Matematicas, 
+c                        Instituto Tecnologico Autonomo de Mexico
+c                        Mexico D.F.
+c
+c           J.L Nocedal  Department of Electrical Engineering and
+c                        Computer Science.
+c                        Northwestern University. Evanston, IL. USA
+c                         
+c                        January 21, 2011
+c
+      temp = sngl(ttime)
+      call cpu_time(temp)
+      ttime = dble(temp) 
+
+      return
+
+      end
+      
---
+++ src/thirdparty/lbfgsb/README.md
@@ -0,0 +1,6 @@
+# L-BFGS-B
+
+This folder contains the Fortran implementation of L-BFGS-B by Ciyou Zhu,
+Richard Byrd, Jorge Nocedal and Jose Luis Morales.
+
+<http://users.iems.northwestern.edu/~nocedal/lbfgsb.html>
