251ea012cb0d41a504ddde7c49536beecbc01145
Joel Andersson <joel@jaeandersson.com>
2023-03-19 13:27:51 -0500

    Issue #2823 Cleanup indentation

--- casadi/interfaces/sundials/idas_interface.hpp
+++ casadi/interfaces/sundials/idas_interface.hpp
@@ -49,183 +49,179 @@
 /// \cond INTERNAL
 
 namespace casadi {
-  // Forward declaration
-  class IdasInterface;
 
-  // IdasMemory
-  struct CASADI_INTEGRATOR_IDAS_EXPORT IdasMemory : public SundialsMemory {
-    /// Function object
-    const IdasInterface& self;
+// Forward declaration
+class IdasInterface;
 
-    /// Idas memory block
-    void* mem;
+// IdasMemory
+struct CASADI_INTEGRATOR_IDAS_EXPORT IdasMemory : public SundialsMemory {
+  /// Function object
+  const IdasInterface& self;
 
-    /// Ids of backward problem
-    int whichB;
+  /// Idas memory block
+  void* mem;
 
-    /// Jacobian memory blocks, forward problem
-    double *jac_ode_x, *jac_alg_x, *jac_ode_z, *jac_alg_z;
+  /// Ids of backward problem
+  int whichB;
 
-    /// Jacobian memory blocks, backward problem
-    double *jac_rode_rx, *jac_ralg_rx, *jac_rode_rz, *jac_ralg_rz;
+  /// Jacobian memory blocks, forward problem
+  double *jac_ode_x, *jac_alg_x, *jac_ode_z, *jac_alg_z;
 
-    /// Constructor
-    IdasMemory(const IdasInterface& s);
+  /// Jacobian memory blocks, backward problem
+  double *jac_rode_rx, *jac_ralg_rx, *jac_rode_rz, *jac_ralg_rz;
 
-    /// Destructor
-    ~IdasMemory();
-  };
+  /// Constructor
+  IdasMemory(const IdasInterface& s);
 
-  /** \brief \pluginbrief{Integrator,idas}
+  /// Destructor
+  ~IdasMemory();
+};
 
-      @copydoc IdasIntegrator_doc
-      @copydoc plugin_Integrator_idas
+/** \brief \pluginbrief{Integrator,idas}
 
-      \author Joel Andersson
-      \date 2010
-  */
-  class CASADI_INTEGRATOR_IDAS_EXPORT IdasInterface : public SundialsInterface {
+    @copydoc IdasIntegrator_doc
+    @copydoc plugin_Integrator_idas
 
-   public:
+    \author Joel Andersson
+    \date 2010
+*/
+class CASADI_INTEGRATOR_IDAS_EXPORT IdasInterface : public SundialsInterface {
+ public:
 
-    /** \brief  Constructor */
-    IdasInterface(const std::string& name, const Function& dae,
-      double t0, const std::vector<double>& tout);
+  /** \brief  Constructor */
+  IdasInterface(const std::string& name, const Function& dae,
+    double t0, const std::vector<double>& tout);
 
-    /** \brief  Create a new integrator */
-    static Integrator* creator(const std::string& name, const Function& dae,
-        double t0, const std::vector<double>& tout) {
-      return new IdasInterface(name, dae, t0, tout);
-    }
+  /** \brief  Create a new integrator */
+  static Integrator* creator(const std::string& name, const Function& dae,
+      double t0, const std::vector<double>& tout) {
+    return new IdasInterface(name, dae, t0, tout);
+  }
 
-    /** \brief  Destructor */
-    ~IdasInterface() override;
+  /** \brief  Destructor */
+  ~IdasInterface() override;
 
-    // Get name of the plugin
-    const char* plugin_name() const override { return "idas";}
+  // Get name of the plugin
+  const char* plugin_name() const override { return "idas";}
 
-    // Get name of the class
-    std::string class_name() const override { return "IdasInterface";}
+  // Get name of the class
+  std::string class_name() const override { return "IdasInterface";}
 
-    ///@{
-    /** \brief Options */
-    static const Options options_;
-    const Options& get_options() const override { return options_;}
-    ///@}
+  ///@{
+  /** \brief Options */
+  static const Options options_;
+  const Options& get_options() const override { return options_;}
+  ///@}
 
-    /** \brief  Initialize */
-    void init(const Dict& opts) override;
+  /** \brief  Initialize */
+  void init(const Dict& opts) override;
 
-    /** \brief Set the (persistent) work vectors */
-    void set_work(void* mem, const double**& arg, double**& res,
-      casadi_int*& iw, double*& w) const override;
+  /** \brief Set the (persistent) work vectors */
+  void set_work(void* mem, const double**& arg, double**& res,
+    casadi_int*& iw, double*& w) const override;
 
-    /** \brief Create memory block */
-    void* alloc_mem() const override { return new IdasMemory(*this);}
+  /** \brief Create memory block */
+  void* alloc_mem() const override { return new IdasMemory(*this);}
 
-    /** \brief Initalize memory block */
-    int init_mem(void* mem) const override;
+  /** \brief Initalize memory block */
+  int init_mem(void* mem) const override;
 
-    /** \brief Free memory block */
-    void free_mem(void *mem) const override { delete static_cast<IdasMemory*>(mem);}
+  /** \brief Free memory block */
+  void free_mem(void *mem) const override { delete static_cast<IdasMemory*>(mem);}
 
-    /** \brief  Reset the forward problem and bring the time back to t0 */
-    void reset(IntegratorMemory* mem,
-      const double* x, const double* z, const double* p) const override;
+  /** \brief  Reset the forward problem and bring the time back to t0 */
+  void reset(IntegratorMemory* mem,
+    const double* x, const double* z, const double* p) const override;
 
-    /** \brief  Advance solution in time */
-    void advance(IntegratorMemory* mem,
-      const double* u, double* x, double* z, double* q) const override;
+  /** \brief  Advance solution in time */
+  void advance(IntegratorMemory* mem,
+    const double* u, double* x, double* z, double* q) const override;
 
-    /** \brief  Reset the backward problem and take time to tf */
-    void resetB(IntegratorMemory* mem,
-      const double* rx, const double* rz, const double* rp) const override;
+  /** \brief  Reset the backward problem and take time to tf */
+  void resetB(IntegratorMemory* mem,
+    const double* rx, const double* rz, const double* rp) const override;
 
-    /** \brief Introduce an impulse into the backwards integration at the current time */
-    void impulseB(IntegratorMemory* mem,
-      const double* rx, const double* rz, const double* rp) const override;
+  /** \brief Introduce an impulse into the backwards integration at the current time */
+  void impulseB(IntegratorMemory* mem,
+    const double* rx, const double* rz, const double* rp) const override;
 
-    /** \brief  Retreat solution in time */
-    void retreat(IntegratorMemory* mem, const double* u,
-      double* rx, double* rz, double* rq, double* uq) const override;
+  /** \brief  Retreat solution in time */
+  void retreat(IntegratorMemory* mem, const double* u,
+    double* rx, double* rz, double* rq, double* uq) const override;
 
-    /** \brief Cast to memory object */
-    static IdasMemory* to_mem(void *mem) {
-      IdasMemory* m = static_cast<IdasMemory*>(mem);
-      casadi_assert_dev(m);
-      return m;
-    }
+  /** \brief Cast to memory object */
+  static IdasMemory* to_mem(void *mem) {
+    IdasMemory* m = static_cast<IdasMemory*>(mem);
+    casadi_assert_dev(m);
+    return m;
+  }
 
-    /// A documentation string
-    static const std::string meta_doc;
+  /// A documentation string
+  static const std::string meta_doc;
 
-  protected:
+ protected:
 
-    // Sundials callback functions
-    static int res(double t, N_Vector xz, N_Vector xzdot, N_Vector rr, void *user_data);
-    static int resB(double t, N_Vector xz, N_Vector xzdot, N_Vector xzB, N_Vector xzdotB,
-                    N_Vector rrB, void *user_data);
-    static void ehfun(int error_code, const char *module, const char *function, char *msg,
-                      void *eh_data);
-    static int jtimesF(double t, N_Vector xz, N_Vector xzdot, N_Vector rr, N_Vector v,
-                      N_Vector Jv, double cj, void *user_data,
-                      N_Vector tmp1, N_Vector tmp2);
-    static int jtimesB(double t, N_Vector xz, N_Vector xzdot, N_Vector xzB, N_Vector xzdotB,
-                       N_Vector resvalB, N_Vector vB, N_Vector JvB, double cjB,
-                       void *user_data, N_Vector tmp1B, N_Vector tmp2B);
-    static int rhsQ(double t, N_Vector xz, N_Vector xzdot, N_Vector qdot, void *user_data);
-    static int rhsQB(double t, N_Vector xz, N_Vector xzdot, N_Vector rxz,
-      N_Vector rxzdot, N_Vector ruqdot, void *user_data);
-    static int psolve(double t, N_Vector xz, N_Vector xzdot, N_Vector rr, N_Vector rvec,
-                      N_Vector zvec, double cj, double delta, void *user_data,
-                      N_Vector tmp);
-    static int psetup(double t, N_Vector xz, N_Vector xzdot, N_Vector rr, double cj,
-                      void* user_data, N_Vector tmp1, N_Vector tmp2, N_Vector tmp3);
-    static int psolveB(double t,
-                       N_Vector xz, N_Vector xzdot, N_Vector xzB, N_Vector xzdotB,
-                       N_Vector resvalB, N_Vector rvecB, N_Vector zvecB, double cjB,
-                       double deltaB, void *user_dataB, N_Vector tmpB);
-    static int psetupB(double t,
-                       N_Vector xz, N_Vector xzdot, N_Vector xzB, N_Vector xzdotB,
-                       N_Vector resvalB, double cjB, void *user_dataB,
-                       N_Vector tmp1B, N_Vector tmp2B, N_Vector tmp3B);
-    static int lsetup(IDAMem IDA_mem, N_Vector xz, N_Vector xzdot, N_Vector resp,
-                      N_Vector vtemp1, N_Vector vtemp2, N_Vector vtemp3);
-    static int lsolve(IDAMem IDA_mem, N_Vector b, N_Vector weight, N_Vector ycur,
-                      N_Vector xzdotcur, N_Vector rescur);
-    static int lsetupB(IDAMem IDA_mem, N_Vector xz, N_Vector xzdot, N_Vector resp,
-                       N_Vector vtemp1, N_Vector vtemp2, N_Vector vtemp3);
-    static int lsolveB(IDAMem IDA_mem, N_Vector b, N_Vector weight, N_Vector ycur,
-                       N_Vector xzdotcur, N_Vector rescur);
-  public:
+  // Sundials callback functions
+  static int res(double t, N_Vector xz, N_Vector xzdot, N_Vector rr, void *user_data);
+  static int resB(double t, N_Vector xz, N_Vector xzdot, N_Vector xzB, N_Vector xzdotB,
+    N_Vector rrB, void *user_data);
+  static void ehfun(int error_code, const char *module, const char *function, char *msg,
+    void *eh_data);
+  static int jtimesF(double t, N_Vector xz, N_Vector xzdot, N_Vector rr, N_Vector v,
+    N_Vector Jv, double cj, void *user_data, N_Vector tmp1, N_Vector tmp2);
+  static int jtimesB(double t, N_Vector xz, N_Vector xzdot, N_Vector xzB, N_Vector xzdotB,
+    N_Vector resvalB, N_Vector vB, N_Vector JvB, double cjB,
+    void *user_data, N_Vector tmp1B, N_Vector tmp2B);
+  static int rhsQ(double t, N_Vector xz, N_Vector xzdot, N_Vector qdot, void *user_data);
+  static int rhsQB(double t, N_Vector xz, N_Vector xzdot, N_Vector rxz,
+    N_Vector rxzdot, N_Vector ruqdot, void *user_data);
+  static int psolve(double t, N_Vector xz, N_Vector xzdot, N_Vector rr, N_Vector rvec,
+    N_Vector zvec, double cj, double delta, void *user_data, N_Vector tmp);
+  static int psetup(double t, N_Vector xz, N_Vector xzdot, N_Vector rr, double cj,
+    void* user_data, N_Vector tmp1, N_Vector tmp2, N_Vector tmp3);
+  static int psolveB(double t, N_Vector xz, N_Vector xzdot, N_Vector xzB, N_Vector xzdotB,
+    N_Vector resvalB, N_Vector rvecB, N_Vector zvecB, double cjB,
+    double deltaB, void *user_dataB, N_Vector tmpB);
+  static int psetupB(double t, N_Vector xz, N_Vector xzdot, N_Vector xzB, N_Vector xzdotB,
+    N_Vector resvalB, double cjB, void *user_dataB,
+    N_Vector tmp1B, N_Vector tmp2B, N_Vector tmp3B);
+  static int lsetup(IDAMem IDA_mem, N_Vector xz, N_Vector xzdot, N_Vector resp,
+    N_Vector vtemp1, N_Vector vtemp2, N_Vector vtemp3);
+  static int lsolve(IDAMem IDA_mem, N_Vector b, N_Vector weight, N_Vector ycur,
+    N_Vector xzdotcur, N_Vector rescur);
+  static int lsetupB(IDAMem IDA_mem, N_Vector xz, N_Vector xzdot, N_Vector resp,
+    N_Vector vtemp1, N_Vector vtemp2, N_Vector vtemp3);
+  static int lsolveB(IDAMem IDA_mem, N_Vector b, N_Vector weight, N_Vector ycur,
+    N_Vector xzdotcur, N_Vector rescur);
+ public:
 
-    // Throw error
-    static void idas_error(const char* module, int flag);
+  // Throw error
+  static void idas_error(const char* module, int flag);
 
-    // Options
-    bool cj_scaling_;
-    bool calc_ic_;
-    bool calc_icB_;
-    bool suppress_algebraic_;
-    std::vector<double> abstolv_;
-    double first_time_;
+  // Options
+  bool cj_scaling_;
+  bool calc_ic_;
+  bool calc_icB_;
+  bool suppress_algebraic_;
+  std::vector<double> abstolv_;
+  double first_time_;
 
-    // Constraints on decision variables
-    std::vector<casadi_int> y_c_;
+  // Constraints on decision variables
+  std::vector<casadi_int> y_c_;
 
-    //  Initial values for \p xdot
-    std::vector<double> init_xdot_;
+  //  Initial values for \p xdot
+  std::vector<double> init_xdot_;
 
-    /** \brief Serialize an object without type information */
-    void serialize_body(SerializingStream &s) const override;
+  /** \brief Serialize an object without type information */
+  void serialize_body(SerializingStream &s) const override;
 
-    /** \brief Deserialize into MX */
-    static ProtoFunction* deserialize(DeserializingStream& s) { return new IdasInterface(s); }
+  /** \brief Deserialize into MX */
+  static ProtoFunction* deserialize(DeserializingStream& s) { return new IdasInterface(s); }
 
-  protected:
-    /** \brief Deserializing constructor */
-    explicit IdasInterface(DeserializingStream& s);
-  };
+ protected:
+  /** \brief Deserializing constructor */
+  explicit IdasInterface(DeserializingStream& s);
+};
 
 } // namespace casadi
 
