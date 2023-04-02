3d0255e2aa1d8d4d0e079fc603dfcf9dbdbb5920
Pieter Pas <pieter.pas@outlook.com>
2023-04-02 14:01:12 +0200

    Language-specific warnings

--- cmake/Warnings.cmake
+++ cmake/Warnings.cmake
@@ -1,43 +1,61 @@
 function(add_warnings_target tgt_name warnings_as_errors)
 
     # GCC, Clang, AppleClang
-    set(COMMON_WARNINGS
+    set(COMMON_WARNINGS_CXX
         -fdiagnostics-show-option
         -Wall
         -Wextra
         -pedantic
         -Wpedantic
         -pedantic-errors
-        $<$<COMPILE_LANGUAGE:CXX>:-Wdouble-promotion>
-        $<$<COMPILE_LANGUAGE:CXX>:-Wswitch-default>
-        $<$<COMPILE_LANGUAGE:CXX>:-Wswitch-enum>
-        $<$<COMPILE_LANGUAGE:CXX>:-Wno-missing-braces>
-        $<$<COMPILE_LANGUAGE:CXX>:-Wno-psabi>
+        -Wdouble-promotion
+        -Wswitch-default
+        -Wswitch-enum
+        -Wno-missing-braces
+        -Wno-psabi
+        -Wimplicit-fallthrough
+        -Wuninitialized
+        -Wconversion
+    )
+    set(COMMON_WARNINGS_Fortran
+        -fdiagnostics-show-option
+        -Wall
+        -Wextra
+        -Wpedantic
+    )
+    set(COMMON_WARNINGS_C
+        -fdiagnostics-show-option
+        -Wall
+        -Wextra
+        -pedantic
+        -Wpedantic
+        -pedantic-errors
         -Wimplicit-fallthrough
         -Wuninitialized
         -Wconversion
     )
     # GCC
-    set(GCC_WARNINGS
+    set(GCC_WARNINGS_CXX
         -Wno-error=unused-but-set-variable
-        $<$<COMPILE_LANGUAGE:CXX>:-Wsuggest-override>
+        -Wsuggest-override
         -Wno-error=attributes
     )
     # Clang, AppleClang
-    set(CLANG_WARNINGS
+    set(CLANG_WARNINGS_CXX
         -Wno-error=unknown-warning-option
         -Wno-newline-eof
         -Wno-error=unused-but-set-variable
-        $<$<COMPILE_LANGUAGE:CXX>:-Winconsistent-missing-override>
+        -Winconsistent-missing-override
         -Wno-gnu-zero-variadic-macro-arguments
         -Wunreachable-code
         -Wunreachable-code-break
         -Wunreachable-code-fallthrough
         -Wunreachable-code-return
         -Wunreachable-code-aggressive
+        -Wno-error=self-assign-overloaded
     )
     # MSVC (Microsoft)
-    set(MSVC_WARNINGS
+    set(MSVC_WARNINGS_CXX
         /W3
         /wd4127 # conditional expression is constant
         /wd4458 # declaration of 'x' hides class member
@@ -46,40 +64,53 @@ function(add_warnings_target tgt_name warnings_as_errors)
         /wd4661 # no suitable definition provided for explicit template instantiation request
         /wd5030 # attribute 'attribute-name' is not recognized
     )
+    set(MSVC_WARNINGS_C
+        /W4
+    )
     # Intel ICC
-    set(INTEL_WARNINGS
+    set(INTEL_WARNINGS_CXX
+        -Wall
+        -Wextra
+    )
+    set(INTEL_WARNINGS_Fortran
+        -Wall
+        -Wextra
+    )
+    set(INTEL_WARNINGS_C
         -Wall
         -Wextra
     )
 
-    # Enable warnings as errors
-    if (warnings_as_errors)
-        if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-            list(APPEND MSVC_WARNINGS /WX)
-        elseif (CMAKE_CXX_COMPILER_ID MATCHES "Intel")
-            list(APPEND INTEL_WARNINGS -Werror)
-        else()
-            list(APPEND COMMON_WARNINGS -Werror)
-        endif()
-    endif()
-
     # Add target that defines all the warning options in its interface.
     add_library(${tgt_name} INTERFACE)
     target_compile_options(${tgt_name} INTERFACE ${${PROJECT}_WARNNGS})
-    if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-        target_compile_options(${tgt_name} INTERFACE
-            $<BUILD_INTERFACE:${COMMON_WARNINGS} ${GCC_WARNINGS}>)
-    elseif (CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-        target_compile_options(${tgt_name} INTERFACE
-            $<BUILD_INTERFACE:${COMMON_WARNINGS} ${CLANG_WARNINGS}>)
-    elseif (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-        target_compile_options(${tgt_name} INTERFACE
-            $<BUILD_INTERFACE:${MSVC_WARNINGS}>)
-    elseif (CMAKE_CXX_COMPILER_ID MATCHES "Intel")
-        target_compile_options(${tgt_name} INTERFACE
-            $<BUILD_INTERFACE:${INTEL_WARNINGS}>)
-    else()
-        message(WARNING "No known warnings for this compiler")
-    endif()
+    
+    foreach (LANG C CXX Fortran)
+        # Enable warnings as errors
+        if (warnings_as_errors)
+            list(APPEND COMMON_WARNINGS_${LANG} -Werror)
+            list(APPEND MSVC_WARNINGS_${LANG} /WX)
+            list(APPEND INTEL_WARNINGS_${LANG} -Werror)
+            list(APPEND FLANG_WARNINGS_${LANG} -Werror)
+        endif()
+        if (CMAKE_${LANG}_COMPILER_ID MATCHES "GNU")
+            target_compile_options(${tgt_name} INTERFACE
+            $<$<COMPILE_LANGUAGE:${LANG}>:$<BUILD_INTERFACE:${COMMON_WARNINGS_${LANG}} ${GCC_WARNINGS_${LANG}}>>)
+        elseif (CMAKE_${LANG}_COMPILER_ID MATCHES ".*Clang")
+            target_compile_options(${tgt_name} INTERFACE
+            $<$<COMPILE_LANGUAGE:${LANG}>:$<BUILD_INTERFACE:${COMMON_WARNINGS_${LANG}} ${CLANG_WARNINGS_${LANG}}>>)
+        elseif (CMAKE_${LANG}_COMPILER_ID MATCHES "Flang")
+            target_compile_options(${tgt_name} INTERFACE
+            $<$<COMPILE_LANGUAGE:${LANG}>:$<BUILD_INTERFACE:${FLANG_WARNINGS_${LANG}}>>)
+        elseif (CMAKE_${LANG}_COMPILER_ID MATCHES "MSVC")
+            target_compile_options(${tgt_name} INTERFACE
+                $<$<COMPILE_LANGUAGE:${LANG}>:$<BUILD_INTERFACE:${MSVC_WARNINGS_${LANG}}>>)
+        elseif (CMAKE_${LANG}_COMPILER_ID MATCHES "Intel")
+            target_compile_options(${tgt_name} INTERFACE
+            $<$<COMPILE_LANGUAGE:${LANG}>:$<BUILD_INTERFACE:${INTEL_WARNINGS_${LANG}}>>)
+        else()
+            message(WARNING "No known warnings for this ${LANG} compiler")
+        endif()
+    endforeach()
 
 endfunction()
