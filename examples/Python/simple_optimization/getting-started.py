400a25292ea09cb642a58ed1cab459dd05b3c96d
Pieter Pas <pieter.pas@outlook.com>
2023-04-04 21:43:52 +0200

    [Py] update examples

--- examples/Python/simple_optimization/getting-started.py
+++ examples/Python/simple_optimization/getting-started.py
@@ -24,7 +24,7 @@ g = cs.Function("g", [x, p], [g_expr])
 C = [-0.25, -0.5], [1.5, 2.5]  # -0.25 <= x1 <= 1.5, -0.5 <= x2 <= 2.5
 D = [-np.inf, -np.inf], [0, 0]  #         g1 <= 0,           g2 <= 0
 # Set the problem parameter
-param = [10.0]
+param = [1.0]
 
 # %% Generate and compile C-code for the objective and constraints using alpaqa
 import alpaqa.casadi_loader as cl
@@ -39,43 +39,43 @@ prob = cl.generate_and_compile_casadi_problem(
 )
 
 # You can change the bounds and parameters after loading the problem
-prob.param = [100.0]
+prob.param = [10.0]
 prob.D.lowerbound[1] = -1e20
 
 # %% Build a solver with the default parameters
 import alpaqa as pa
 
-innersolver = pa.PANOCSolver()
-solver = pa.ALMSolver(innersolver)
+inner_solver = pa.PANOCSolver()
+solver = pa.ALMSolver(inner_solver)
 
 # %% Build a solver with custom parameters
 inner_solver = pa.PANOCSolver(
     panoc_params={
-        "max_iter": 1000,
-        "stop_crit": pa.PANOCStopCrit.ApproxKKT,
+        'max_iter': 1000,
+        'stop_crit': pa.PANOCStopCrit.ApproxKKT,
+        'print_interval': 1,
     },
     lbfgs_params={
-        "memory": 10,
+        'memory': 10,
     },
 )
 
 solver = pa.ALMSolver(
     alm_params={
-        "ε": 1e-10,
-        "δ": 1e-10,
-        "Σ_0": 0,
-        "σ_0": 2,
-        "Δ": 20,
-        "print_interval": 1,
+        'ε': 1e-10,
+        'δ': 1e-10,
+        'Σ_0': 50,
+        'Δ': 20,
+        'print_interval': 1,
     },
-    inner_solver=inner_solver,
+    inner_solver=inner_solver
 )
 
 # %% Compute a solution
 
 # Set initial guesses at arbitrary values
-x0 = np.array([0.1, 1.8]) # decision variables
-y0 = np.zeros((prob.m,))  # Lagrange multipliers for g(x)
+x0 = [0.1, 1.8]  # decision variables
+y0 = [0.0, 0.0]  # Lagrange multipliers for g(x)
 
 # Solve the problem
 x_sol, y_sol, stats = solver(prob, x0, y0)
@@ -85,5 +85,3 @@ print(stats["status"])
 print(f"Solution:      {x_sol}")
 print(f"Multipliers:   {y_sol}")
 print(f"Cost:          {prob.eval_f(x_sol):.5f}")
-print(f"ε:             {stats['ε']}")
-print(f"δ:             {stats['δ']}")
--- examples/Python/simple_optimization/rosenbrock.py
+++ examples/Python/simple_optimization/rosenbrock.py
@@ -1,8 +1,11 @@
-# %% Build the problem for PANOC+ALM (CasADi code, independent of alpaqa)
+# %% Build the problem (CasADi code, independent of alpaqa)
 import casadi as cs
+import numpy as np
 
 # Make symbolic decision variables
 x1, x2 = cs.SX.sym("x1"), cs.SX.sym("x2")
+# Collect decision variables into one vector
+x = cs.vertcat(x1, x2)
 # Make a parameter symbol
 p = cs.SX.sym("p")
 
@@ -13,38 +16,45 @@ g_expr = cs.vertcat(
     x1 + x2 - 1.5,
 )
 
-# Collect decision variables into one vector
-x = cs.vertcat(x1, x2)
 # Convert the symbolic expressions to CasADi functions
 f = cs.Function("f", [x, p], [f_expr])
 g = cs.Function("g", [x, p], [g_expr])
 
+# Set the bounds
+C = [-0.25, -0.5], [1.5, 2.5]  # -0.25 <= x1 <= 1.5, -0.5 <= x2 <= 2.5
+D = [-np.inf, -np.inf], [0, 0]  #         g1 <= 0,           g2 <= 0
+# Set the problem parameter
+param = [10.0]
+
 # %% Generate and compile C-code for the objective and constraints using alpaqa
-import alpaqa as pa
 import alpaqa.casadi_loader as cl
 
 # Compile and load the problem
-prob = cl.generate_and_compile_casadi_problem(f, g)
+prob = cl.generate_and_compile_casadi_problem(
+    f=f,  # minimize    f(x; param)
+    C=C,  # subject to  x ∊ C
+    g=g,  # subject to  g(x; param) ∊ D
+    D=D,
+    param=param,
+    second_order="psi_prod",
+)
 
-# Set the bounds
-import numpy as np
-prob.C.lowerbound = [-0.25, -0.5]       # -0.25 <= x1 <= 1.5
-prob.C.upperbound = [1.5, 2.5]          # -0.5  <= x2 <= 2.5
-prob.D.lowerbound = [-np.inf, -np.inf]  # g1 <= 0
-prob.D.upperbound = [0, 0]              # g2 <= 0
+# %% Build a solver with the default parameters
+import alpaqa as pa
 
-# Set parameter to some value
-prob.param = [10.]
+inner_solver = pa.PANOCSolver()
+solver = pa.ALMSolver(inner_solver)
 
-# %% Build an inner Structured PANOC solver with custom parameters
-innersolver = pa.PANOCSolver(
-    pa.PANOCParams(
-        max_iter=1000,
-        stop_crit=pa.PANOCStopCrit.ApproxKKT,
-    ),
-    pa.LBFGS.Params(
-        memory=10,
-    ),
+# %% Build a solver with custom parameters
+inner_solver = pa.PANOCSolver(
+    panoc_params={
+        'max_iter': 1000,
+        'stop_crit': pa.PANOCStopCrit.ApproxKKT,
+        'print_interval': 1,
+    },
+    lbfgs_params={
+        'memory': 10,
+    },
 )
 
 # You can attach a callback that is called on each iteration, and keeps track of
@@ -56,26 +66,24 @@ def callback(it: pa.PANOCProgressInfo):
     iterates[-1].append(np.copy(it.x))
 # Note: the iterate values like it.x are only valid within the callback, if you
 # want to save them for later, you have to make a copy.
-innersolver.set_progress_callback(callback)
+inner_solver.set_progress_callback(callback)
 
-# %% Make an ALM solver with default parameters, using the PANOC solver
 solver = pa.ALMSolver(
-    pa.ALMParams(
-        ε=1e-10,
-        δ=1e-10,
-        Σ_0=0,
-        σ_0=2,
-        Δ=20,
-        print_interval=1,
-    ),
-    innersolver
+    alm_params={
+        'ε': 1e-10,
+        'δ': 1e-10,
+        'Σ_0': 50,
+        'Δ': 20,
+        'print_interval': 1,
+    },
+    inner_solver=inner_solver
 )
 
-# %% Compute a solution 
+# %% Compute a solution
 
 # Set initial guesses at arbitrary values
-x0 = np.array([0.1, 1.8]) # decision variables
-y0 = np.zeros((prob.m,))  # Lagrange multipliers for g(x)
+x0 = [0.1, 1.8]  # decision variables
+y0 = [0.0, 0.0]  # Lagrange multipliers for g(x)
 
 # Solve the problem
 x_sol, y_sol, stats = solver(prob, x0, y0)
