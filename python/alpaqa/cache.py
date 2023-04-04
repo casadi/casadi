8bad595527dfbfde5fd32a11a2bb0bf7e4653524
Pieter Pas <pieter.pas@outlook.com>
2023-04-04 11:23:02 +0100

    [Py] venv-specific cache directory

--- python/alpaqa/cache.py
+++ python/alpaqa/cache.py
@@ -2,6 +2,14 @@ import os
 from os.path import join, expanduser
 import tempfile
 import shutil
+import sys
+import typing
+
+
+def _is_truthy(s: typing.Optional[str]):
+    if s is None:
+        return False
+    return not s.lower() in ("", "false", "no", "off", "0")
 
 
 def get_cache_dir():
@@ -10,21 +18,30 @@ def get_cache_dir():
 
     If the environment variable ``ALPAQA_CACHE_DIR`` exists, its value is
     returned.
+    Otherwise, if a virtual environment is active and the environment variable
+    ``ALPAQA_GLOBAL_CACHE`` is unset (or set to a false value),
+    ``{sys.prefix}/cache`` is returned.
     Otherwise, if ``~/.cache`` exists, then it is returned.
-    Otherwise, ``tempfile.gettempdir()`` is returned.
+    Otherwise, the result of ``tempfile.gettempdir()`` is returned.
     """
+    # 1. Custom directory
     cache_dir = os.getenv("ALPAQA_CACHE_DIR")
-    if cache_dir is None:
-        home_cache_dir = expanduser("~/.cache")
-        if os.path.isdir(home_cache_dir):
-            cache_dir = home_cache_dir
-        else:
-            cache_dir = tempfile.gettempdir()
-    return cache_dir
+    if cache_dir is not None:
+        return cache_dir
+    # 2. Virtual environment cache directory
+    in_venv = sys.prefix != sys.base_prefix
+    if in_venv and not _is_truthy(os.getenv("ALPAQA_GLOBAL_CACHE")):
+        return join(sys.prefix, "cache")
+    # 3. Home cache directory
+    home_cache_dir = expanduser("~/.cache")
+    if os.path.isdir(home_cache_dir):
+        return home_cache_dir
+    # 4. System temporary directory
+    return tempfile.gettempdir()
 
 
 def get_alpaqa_cache_dir():
-    return join(get_cache_dir(), 'alpaqa', 'cache')
+    return join(get_cache_dir(), "alpaqa", "cache")
 
 
 def clean(alpaqa_cache_dir=None):
@@ -43,7 +60,7 @@ def interactive_clean(args):
         confirmed = args.force
         msg = f"Are you sure you want to remove {alpaqa_cache_dir} [y/N]? "
         if not confirmed:
-            confirmed = input(msg).lower() == 'y'
+            confirmed = input(msg).lower() == "y"
         if not confirmed:
             print("Aborted.")
             exit(1)
