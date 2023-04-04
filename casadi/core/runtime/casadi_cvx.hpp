c23ad4857830d66e3a216749bdbed9a54ad03727
Joris Gillis <joris.gillis42@gmail.com>
2023-04-04 09:57:06 +0200

    Revert "Changed givens algorithm to symmetric version"
    This reverts commit 4e7647e3309b55034980dfbbc89e027b40e36f58.

--- casadi/core/runtime/casadi_cvx.hpp
+++ casadi/core/runtime/casadi_cvx.hpp
@@ -145,40 +145,23 @@ void casadi_cvx_tri(T1* A, casadi_int n, T1* beta, T1* p) {
 
 
 // SYMBOL "cvx_givens"
-// Continuous algorithm for generating real plane rotations
-// Ref: Anderson Alg. 4, Discontinuous Plane Rotations
-// and the Symmetric Eigenvalue Problem
+// Ref: Golub & Van Loan Alg. 5.1.3
 template<typename T1>
 void casadi_cvx_givens(T1 a, T1 b, T1* c, T1* s) {
   T1 r;
   if (b==0) {
     *c = 1;
-    if (a<0) {
-      *c = -1;
-    }
     *s = 0;
-  } else if (a==0) {
-    *c = 0;
-    *s = 1;
-    if (b<0) {
-      *s = -1;
-    }
-  } else if (fabs(a)>fabs(b)) {
-    r = b/a;
-    if (a>0) {
-      *c = 1/sqrt(1+r*r);
-    } else {
-      *c = -1/sqrt(1+r*r);
-    }
-    *s = (*c)*r;
   } else {
-    r = a/b;
-    if (b>0) {
+    if (fabs(b)>fabs(a)) {
+      r = -a/b;
       *s = 1/sqrt(1+r*r);
+      *c = (*s)*r;
     } else {
-      *s = -1/sqrt(1+r*r);
+      r = -b/a;
+      *c = 1/sqrt(1+r*r);
+      *s = (*c)*r;
     }
-    *c = (*s)*r;
   }
 }
 
