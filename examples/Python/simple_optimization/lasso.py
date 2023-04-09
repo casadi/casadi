249e7b1b6bfdd5890bc21a7dc00b7b7901ebe411
Pieter Pas <pieter.pas@outlook.com>
2023-04-09 23:15:19 +0200

    [Py] use high-level API in lasso.py example

--- examples/Python/simple_optimization/lasso.py
+++ examples/Python/simple_optimization/lasso.py
@@ -14,41 +14,50 @@ rng = np.random.default_rng(0)
 # %% Build the problem (CasADi code, independent of alpaqa)
 
 # Quadratic loss plus l1-regularization
-# minimize  ‖Ax - b‖² + λ‖x‖₁
+# minimize  ½‖Ax - b‖² + λ‖x‖₁
 
 A = rng.uniform(-1, 1, (m, n))
 x_exact = rng.uniform(0, 1, n)
 x_exact[rng.uniform(0, 1, n) > sparsity] = 0
 b = A @ x_exact + rng.normal(0, 0.1, m)
-λ = 5
+λ = 0.025 * m
 
 # Symbolic solution
 x = cs.MX.sym("x", n)
 # Objective function is squared norm of Ax - b
-f = cs.Function("f", [x], [cs.sumsqr(A @ x - b)])
+f = 0.5 * cs.sumsqr(A @ x - b)
 
 # %% Generate and compile C-code for the objective and constraints using alpaqa
 
-# Compile and load the problem (without general constraints)
-prob = cl.generate_and_compile_casadi_problem(f, None, sym=cs.MX.sym)
-# Set the regularization parameter
-prob.l1_reg = [λ]
+# Compile and load the problem
+problem = (
+    pa.minimize(f, x)
+    .with_l1_regularizer(λ)
+).compile(sym=cs.MX.sym)
 
 # %% Solve the problem using alpaqa's PANOC solver
 
 direction = pa.LBFGSDirection({"memory": scale})
 solver = pa.PANOCSolver({"print_interval": 10}, direction)
 # Add evaluation counters to the problem
-cnt = pa.problem_with_counters(prob)
+cnt = pa.problem_with_counters(problem)
+# Solve
 sol, stats = solver(cnt.problem, {"tolerance": 1e-10})
 
 # %% Print the results
 
-print(f"\nSolution:      {sol}")
-print(stats["status"])
+final_f = problem.eval_f(sol)
+print()
 pprint(stats)
-print("\nEvaluations:")
+print()
+print("Evaluations:")
 print(cnt.evaluations)
+print(f"Cost:          {final_f + stats['final_h']}")
+print(f"Loss:          {final_f}")
+print(f"Regularizer:   {stats['final_h']}")
+print(f"FP Residual:   {stats['ε']}")
+print(f"Run time:      {stats['elapsed_time']}")
+print(stats["status"])
 
 # %% Plot the results
 
