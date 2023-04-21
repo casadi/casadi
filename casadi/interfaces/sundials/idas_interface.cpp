casadi/casadi

commit af60b3cee1f9451d8f271fccd5dce6bd2c8f7ed1

Issue #3047 Smaller backward linear system in Sundials No longer grows with number of adjoint sensitivities

casadi/interfaces/sundials/idas_interface.cpp
@@ -716,20 +716,18 @@ int IdasInterface::psolveB(double t, N_Vector xz, N_Vector xzdot, N_Vector xzB,
       for (int a = 0; a < s.nadj_; ++a) {
         casadi_copy(vx + (d * s.nadj_ + a) * s.nrx1_, s.nrx1_, v_it);
         v_it += s.nrx1_;
-      }
-      for (int a = 0; a < s.nadj_; ++a) {
         casadi_copy(vz + (d * s.nadj_ + a) * s.nrz1_, s.nrz1_, v_it);
         v_it += s.nrz1_;
       }
     }
 
     // Solve for undifferentiated right-hand-side, save to output
-    if (s.linsolB_.solve(m->jacB, m->v1, 1, false, m->mem_linsolB)) return 1;
+    if (s.linsolB_.solve(m->jacB, m->v1, s.nadj_, false, m->mem_linsolB)) return 1;
     vx = NV_DATA_S(zvecB); // possibly different from rvecB
     vz = vx + s.nrx_;
     for (int a = 0; a < s.nadj_; ++a) {
-      casadi_copy(m->v1 + a * s.nrx1_, s.nrx1_, vx + a * s.nrx1_);
-      casadi_copy(m->v1 + s.nrx1_ * s.nadj_ + a * s.nrz1_, s.nrz1_, vz + a * s.nrz1_);
+      casadi_copy(m->v1 + a * (s.nrx1_ + s.nrz1_), s.nrx1_, vx + a * s.nrx1_);
+      casadi_copy(m->v1 + a * (s.nrx1_ + s.nrz1_) + s.nrx1_, s.nrz1_, vz + a * s.nrz1_);
     }
 
     // Sensitivity equations
@@ -751,8 +749,6 @@ int IdasInterface::psolveB(double t, N_Vector xz, N_Vector xzdot, N_Vector xzB,
           for (int a = 0; a < s.nadj_; ++a) {
             casadi_axpy(s.nrx1_, -1., m->v2 + s.nrx1_ * (d * s.nadj_ + a), v_it);
             v_it += s.nrx1_;
-          }
-          for (int a = 0; a < s.nadj_; ++a) {
             casadi_axpy(s.nrz1_, -1., m->v2 + s.nrx_ + s.nrz1_ * (d * s.nadj_ + a), v_it);
             v_it += s.nrz1_;
           }
@@ -761,17 +757,15 @@ int IdasInterface::psolveB(double t, N_Vector xz, N_Vector xzdot, N_Vector xzB,
 
       // Solve for sensitivity right-hand-sides
       if (s.linsolB_.solve(m->jacB, m->v1 + s.nrx1_ * s.nadj_ + s.nrz1_ * s.nadj_,
-          s.nfwd_, false, m->mem_linsolB)) return 1;
+        s.nadj_ * s.nfwd_, false, m->mem_linsolB)) return 1;
 
       // Save to output, reordered
-      v_it = m->v1 + s.nrx1_ * s.nadj_ + s.nrz1_ * s.nadj_;
+      v_it = m->v1 + (s.nrx1_ + s.nrz1_) * s.nadj_;
       for (int d = 1; d <= s.nfwd_; ++d) {
         for (int a = 0; a < s.nadj_; ++a) {
           casadi_copy(v_it, s.nrx1_, vx + s.nrx1_ * (d * s.nadj_ + a));
           v_it += s.nrx1_;
-        }
-        for (int a = 0; a < s.nadj_; ++a) {
-          casadi_copy(v_it, s.nrz1_, vz + s.nrz1_ * (d * s.nadj_ + a));
+          casadi_axpy(s.nrz1_, -1., m->v2 + s.nrx_ + s.nrz1_ * (d * s.nadj_ + a), v_it);
           v_it += s.nrz1_;
         }
       }

casadi/interfaces/sundials/cvodes_interface.cpp
@@ -528,7 +528,7 @@ int CvodesInterface::psolveB(double t, N_Vector x, N_Vector xB, N_Vector xdotB, 
     casadi_copy(v, s.nrx_, m->v1);
 
     // Solve for undifferentiated right-hand-side, save to output
-    if (s.linsolB_.solve(m->jacB, m->v1, 1, false, m->mem_linsolB)) return 1;
+    if (s.linsolB_.solve(m->jacB, m->v1, s.nadj_, false, m->mem_linsolB)) return 1;
     v = NV_DATA_S(zvecB); // possibly different from rvecB
     casadi_copy(m->v1, s.nrx1_ * s.nadj_, v);
 
@@ -547,7 +547,7 @@ int CvodesInterface::psolveB(double t, N_Vector x, N_Vector xB, N_Vector xdotB, 
       }
 
       // Solve for sensitivity right-hand-sides
-      if (s.linsolB_.solve(m->jacB, m->v1 + s.nx1_, s.nfwd_, false, m->mem_linsolB)) return 1;
+      if (s.linsolB_.solve(m->jacB, m->v1 + s.nx1_, s.nadj_ * s.nfwd_, false, m->mem_linsolB)) return 1;
 
       // Save to output, reordered
       casadi_copy(m->v1 + s.nx1_, s.nx_ - s.nx1_, v + s.nx1_);

casadi/interfaces/sundials/sundials_interface.cpp
@@ -238,7 +238,7 @@ void SundialsInterface::init(const Dict& opts) {
     // Get Jacobian function, backward problem
     Sparsity jacB_sp;
     if (d == 0) {
-      jacB_sp = diagcat(std::vector<Sparsity>(nadj_, jacF_sp.T()));
+      jacB_sp = jacF_sp.T();
     } else {
       // Reuse existing linear solver
       linsolB_ = d->linsolB_;
@@ -306,10 +306,10 @@ void SundialsInterface::init(const Dict& opts) {
     sp_jac_ode_zB1_ = jacF.sparsity_out(JACF_ODE_Z).T();
     sp_jac_alg_zB1_ = jacF.sparsity_out(JACF_ALG_Z).T();
 
-    sp_jac_ode_xB_ = diagcat(std::vector<Sparsity>(nadj_, jacF.sparsity_out(JACF_ODE_X).T()));
-    sp_jac_alg_xB_ = diagcat(std::vector<Sparsity>(nadj_, jacF.sparsity_out(JACF_ALG_X).T()));
-    sp_jac_ode_zB_ = diagcat(std::vector<Sparsity>(nadj_, jacF.sparsity_out(JACF_ODE_Z).T()));
-    sp_jac_alg_zB_ = diagcat(std::vector<Sparsity>(nadj_, jacF.sparsity_out(JACF_ALG_Z).T()));
+    sp_jac_ode_xB_ = sp_jac_ode_xB1_;
+    sp_jac_alg_xB_ = sp_jac_alg_xB1_;
+    sp_jac_ode_zB_ = sp_jac_ode_zB1_;
+    sp_jac_alg_zB_ = sp_jac_alg_zB1_;
   }
 }
 
@@ -791,28 +791,26 @@ int SundialsInterface::calc_jacB(SundialsMemory* m, double t, const double* x, c
   if (calc_jacF(m, t, x, z, m->jac_ode_x, m->jac_alg_x, m->jac_ode_z, m->jac_alg_z)) return 1;
   // Copy to jacB
   const Function& jacF = get_function("jacF");
-  for (casadi_int d = 0; d < nadj_; ++d) {
-    // Copy transposed blocks
-    if (jac_adj_x_rx) {
-      casadi_trans(m->jac_ode_x, jacF.sparsity_out(JACF_ODE_X),
-        jac_adj_x_rx, sp_jac_ode_xB1_, m->iw);
-      jac_adj_x_rx += jacF.nnz_out(JACF_ODE_X);
-    }
-    if (jac_adj_z_rx) {
-      casadi_trans(m->jac_alg_x, jacF.sparsity_out(JACF_ALG_X),
-        jac_adj_z_rx, sp_jac_alg_xB1_, m->iw);
-      jac_adj_z_rx += jacF.nnz_out(JACF_ALG_X);
-    }
-    if (jac_adj_x_rz) {
-      casadi_trans(m->jac_ode_z, jacF.sparsity_out(JACF_ODE_Z),
-        jac_adj_x_rz, sp_jac_ode_zB1_, m->iw);
-      jac_adj_x_rz += jacF.nnz_out(JACF_ODE_Z);
-    }
-    if (jac_adj_z_rz) {
-      casadi_trans(m->jac_alg_z, jacF.sparsity_out(JACF_ALG_Z),
-        jac_adj_z_rz, sp_jac_alg_zB1_, m->iw);
-      jac_adj_z_rz += jacF.nnz_out(JACF_ALG_Z);
-    }
+  // Copy transposed blocks
+  if (jac_adj_x_rx) {
+    casadi_trans(m->jac_ode_x, jacF.sparsity_out(JACF_ODE_X),
+      jac_adj_x_rx, sp_jac_ode_xB1_, m->iw);
+    jac_adj_x_rx += jacF.nnz_out(JACF_ODE_X);
+  }
+  if (jac_adj_z_rx) {
+    casadi_trans(m->jac_alg_x, jacF.sparsity_out(JACF_ALG_X),
+      jac_adj_z_rx, sp_jac_alg_xB1_, m->iw);
+    jac_adj_z_rx += jacF.nnz_out(JACF_ALG_X);
+  }
+  if (jac_adj_x_rz) {
+    casadi_trans(m->jac_ode_z, jacF.sparsity_out(JACF_ODE_Z),
+      jac_adj_x_rz, sp_jac_ode_zB1_, m->iw);
+    jac_adj_x_rz += jacF.nnz_out(JACF_ODE_Z);
+  }
+  if (jac_adj_z_rz) {
+    casadi_trans(m->jac_alg_z, jacF.sparsity_out(JACF_ALG_Z),
+      jac_adj_z_rz, sp_jac_alg_zB1_, m->iw);
+    jac_adj_z_rz += jacF.nnz_out(JACF_ALG_Z);
   }
   return 0;
 }

commit 8a3a7d5733189f0a034d023c9559aee736cf77a4

Issue #3121 Fixed IDAS bug causing performance decay Bug was introduced in af60b3c

casadi/interfaces/sundials/idas_interface.cpp
@@ -776,7 +776,7 @@ int IdasInterface::solve_transposed(IdasMemory* m, double t, const double* xz, c
       for (int a = 0; a < nadj_; ++a) {
         casadi_copy(v_it, nrx1_, sol + nrx1_ * (d * nadj_ + a));
         v_it += nrx1_;
-        casadi_axpy(nrz1_, -1., m->v2 + nrx_ + nrz1_ * (d * nadj_ + a), v_it);
+        casadi_copy(v_it, nrz1_, sol + nrx_ + nrz1_ * (d * nadj_ + a));
         v_it += nrz1_;
       }
     }