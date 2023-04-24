casadi/casadi

commit 068b6c6df7c9a52cc8d8e80424110fc717816c4e

Issue #3047 Simplifying SundialsInterface::calc_jtimesB

casadi/interfaces/sundials/sundials_interface.cpp
@@ -745,43 +745,22 @@ void SundialsInterface::calc_jtimesF(SundialsMemory* m, double t, const double* 
 void SundialsInterface::calc_jtimesB(SundialsMemory* m, double t, const double* x, const double* z,
     const double* rx, const double* rz, const double* fwd_rx, const double* fwd_rz,
     double* fwd_adj_x, double* fwd_adj_z) const {
-  // Evaluate nondifferentiated
-  m->arg[JTIMESB_T] = &t;  // t
-  m->arg[JTIMESB_X] = x;  // x
-  m->arg[JTIMESB_Z] = z;  // z
-  m->arg[JTIMESB_P] = m->p;  // p
-  m->arg[JTIMESB_U] = m->u;  // u
-  m->arg[JTIMESB_ADJ_ODE] = rx;  // adj_ode
-  m->arg[JTIMESB_ADJ_ALG] = rz;  // adj_alg
-  m->arg[JTIMESB_ADJ_QUAD] = nullptr;  // adj_quad
-  m->arg[JTIMESB_FWD_ADJ_ODE] = fwd_rx;  // fwd:adj_ode
-  m->arg[JTIMESB_FWD_ADJ_ALG] = fwd_rz;  // fwd:adj_alg
-  m->res[JTIMESB_FWD_ADJ_X] = fwd_adj_x;  // fwd:adj_x
-  m->res[JTIMESB_FWD_ADJ_Z] = fwd_adj_z;  // fwd:adj_z
-  calc_function(m, "jtimesB");
-  // Evaluate sensitivities
-  if (nfwd_ > 0) {
-    m->arg[JTIMESB_NUM_IN + JTIMESB_FWD_ADJ_X] = fwd_adj_x;  // out:fwd:adj_x
-    m->arg[JTIMESB_NUM_IN + JTIMESB_FWD_ADJ_Z] = fwd_adj_z;  // out:fwd:adj_z
-    m->arg[JTIMESB_NUM_IN + JTIMESB_NUM_OUT + JTIMESB_T] = 0;  // fwd:t
-    m->arg[JTIMESB_NUM_IN + JTIMESB_NUM_OUT + JTIMESB_X] = x + nx1_;  // fwd:x
-    m->arg[JTIMESB_NUM_IN + JTIMESB_NUM_OUT + JTIMESB_Z] = z + nz1_;  // fwd:z
-    m->arg[JTIMESB_NUM_IN + JTIMESB_NUM_OUT + JTIMESB_P] = m->p + np1_;  // fwd:p
-    m->arg[JTIMESB_NUM_IN + JTIMESB_NUM_OUT + JTIMESB_U] = m->u + nu1_;  // fwd:u
-    m->arg[JTIMESB_NUM_IN + JTIMESB_NUM_OUT + JTIMESB_ADJ_ODE] =
-      rx + nrx1_ * nadj_;  // fwd:adj_ode
-    m->arg[JTIMESB_NUM_IN + JTIMESB_NUM_OUT + JTIMESB_ADJ_ALG] =
-      rz + nrz1_ * nadj_;  // fwd:adj_alg
-    m->arg[JTIMESB_NUM_IN + JTIMESB_NUM_OUT + JTIMESB_ADJ_QUAD] =
-      nullptr;  // fwd:adj_quad
-    m->arg[JTIMESB_NUM_IN + JTIMESB_NUM_OUT + JTIMESB_FWD_ADJ_ODE] =
-      fwd_rx + nrx1_ * nadj_;  // fwd:fwd:adj_ode
-    m->arg[JTIMESB_NUM_IN + JTIMESB_NUM_OUT + JTIMESB_FWD_ADJ_ALG] =
-      fwd_rz + nrz1_ * nadj_;  // fwd:fwd:adj_alg
-    m->res[JTIMESB_FWD_ADJ_X] = fwd_adj_x + nrx1_ * nadj_;  // fwd:fwd:adj_x
-    m->res[JTIMESB_FWD_ADJ_Z] = fwd_adj_z + nrz1_ * nadj_;  // fwd:fwd:adj_z
-    calc_function(m, forward_name("jtimesB", nfwd_));
- }
+  // Evaluate nondifferentiated and sensitivities
+  for (casadi_int d = 0; d <= nfwd_; ++d) {
+    m->arg[JTIMESB_T] = &t;  // t
+    m->arg[JTIMESB_X] = x;  // x
+    m->arg[JTIMESB_Z] = z;  // z
+    m->arg[JTIMESB_P] = m->p;  // p
+    m->arg[JTIMESB_U] = m->u;  // u
+    m->arg[JTIMESB_ADJ_ODE] = rx + d * nx1_;  // adj_ode
+    m->arg[JTIMESB_ADJ_ALG] = rz + d * nz1_;  // adj_alg
+    m->arg[JTIMESB_ADJ_QUAD] = nullptr;  // adj_quad
+    m->arg[JTIMESB_FWD_ADJ_ODE] = fwd_rx + d * nrx1_ * nadj_;  // fwd:adj_ode
+    m->arg[JTIMESB_FWD_ADJ_ALG] = fwd_rz + d * nrz1_ * nadj_;  // fwd:adj_alg
+    m->res[JTIMESB_FWD_ADJ_X] = fwd_adj_x + d * nrx1_ * nadj_;  // fwd:adj_x
+    m->res[JTIMESB_FWD_ADJ_Z] = fwd_adj_z + d * nrz1_ * nadj_;  // fwd:adj_z
+    calc_function(m, "jtimesB");
+  }
 }
 
 void SundialsInterface::calc_jacF(SundialsMemory* m, double t, const double* x, const double* z,

commit 2abdad7550fb9e301bf0e81d52003eda7db92b63

Issue #3139 Removed jtimesB in Sundials interface The backward DAE is a linear function in backward states so the Jacobian-times-vector function of the backward DAE is the backward DAE itself. This issue should resolve the bug introduced in 068b6c6 since calc_jtimesB then no longer exists.

casadi/interfaces/sundials/cvodes_interface.cpp
@@ -466,8 +466,9 @@ int CvodesInterface::jtimesB(N_Vector v, N_Vector Jv, double t, N_Vector x,
   try {
     auto m = to_mem(user_data);
     auto& s = m->self;
-    if (s.calc_jtimesB(m, t, NV_DATA_S(x), nullptr, NV_DATA_S(rx), nullptr,
-      NV_DATA_S(v), nullptr, NV_DATA_S(Jv), nullptr)) return 1;
+    // The function is linear so the Jacobian-times-vector function is the function itself
+    if (s.calc_daeB(m, t, NV_DATA_S(x), nullptr, NV_DATA_S(v), nullptr, nullptr,
+      NV_DATA_S(Jv), nullptr)) return 1;
     return 0;
   } catch(std::exception& e) { // non-recoverable error
     uerr() << "jtimesB failed: " << e.what() << std::endl;
@@ -535,11 +536,9 @@ int CvodesInterface::psolveB(double t, N_Vector x, N_Vector xB, N_Vector xdotB, 
     if (s.nfwd_ > 0) {
       // Second order correction
       if (s.second_order_correction_) {
-        // The outputs will double as seeds for jtimesB
+        // The outputs will double as seeds for calc_daeB
         casadi_clear(v + s.nrx1_ * s.nadj_, s.nrx_ - s.nrx1_ * s.nadj_);
-        if (s.calc_jtimesB(m, t, NV_DATA_S(x), nullptr, NV_DATA_S(xB),
-          nullptr, v, nullptr, m->v2, nullptr)) return 1;
-
+        if (s.calc_daeB(m, t, NV_DATA_S(x), nullptr, v, nullptr, nullptr, m->v2, nullptr)) return 1;
         // Subtract m->v2 from m->v1, scaled with gammaB
         casadi_axpy(s.nrx_ - s.nrx1_ * s.nadj_, -m->gammaB, m->v2 + s.nrx1_ * s.nadj_,
           m->v1 + s.nrx1_ * s.nadj_);

casadi/interfaces/sundials/idas_interface.cpp
@@ -206,11 +206,10 @@ int IdasInterface::jtimesB(double t, N_Vector xz, N_Vector xzdot, N_Vector rxz,
   try {
     auto m = to_mem(user_data);
     auto& s = m->self;
-    if (s.calc_jtimesB(m, t, NV_DATA_S(xz), NV_DATA_S(xz) + s.nx_,
-      NV_DATA_S(rxz), NV_DATA_S(rxz) + s.nrx_,
-      NV_DATA_S(v), NV_DATA_S(v) + s.nrx_,
+    // The function is linear so the Jacobian-times-vector function is the function itself
+    if (s.calc_daeB(m, t, NV_DATA_S(xz), NV_DATA_S(xz) + s.nx_,
+      NV_DATA_S(v), NV_DATA_S(v) + s.nrx_, nullptr,
       NV_DATA_S(Jv), NV_DATA_S(Jv) + s.nrx_)) return 1;
-
     // Subtract state derivative to get residual
     casadi_axpy(s.nrx_, cjB, NV_DATA_S(v), NV_DATA_S(Jv));
 
@@ -746,13 +745,13 @@ int IdasInterface::solve_transposed(IdasMemory* m, double t, const double* xz, c
   if (nfwd_ > 0) {
     // Second order correction
     if (second_order_correction_ && rxz) {
-      // The outputs will double as seeds for jtimesB
+      // The outputs will double as seeds for calc_daeB
       casadi_clear(sol + nrx1_ * nadj_, nrx_ - nrx1_ * nadj_);
       casadi_clear(sol + nrx_ + nrz1_ * nadj_, nrz_ - nrz1_ * nadj_);
 
       // Get second-order-correction, save to m->v2
-      if (calc_jtimesB(m, t, xz, xz + nx_, rxz, rxz + nrx_,
-        sol, sol + nrx_, m->v2, m->v2 + nrx_)) return 1;
+      if (calc_daeB(m, t, xz, xz + nx_, sol, sol + nrx_, nullptr,
+        m->v2, m->v2 + nrx_)) return 1;
 
       // Subtract m->v2 (reordered) from m->v1
       v_it = m->v1 + (nrx1_ + nrz1_) * nadj_;

casadi/interfaces/sundials/sundials_interface.cpp
@@ -262,14 +262,6 @@ void SundialsInterface::init(const Dict& opts) {
     if (nfwd_ > 0) {
       create_forward("jtimesF", nfwd_);
     }
-    if (nadj_ > 0) {
-      create_function(rdae_, "jtimesB", {"t", "x", "z", "p", "u",
-        "adj_ode", "adj_alg", "adj_quad", "fwd:adj_ode", "fwd:adj_alg"},
-        {"fwd:adj_x", "fwd:adj_z"});
-      if (nfwd_ > 0) {
-        create_forward("jtimesB", nfwd_);
-      }
-    }
   }
 
 
@@ -716,28 +708,6 @@ int SundialsInterface::calc_jtimesF(SundialsMemory* m, double t, const double* x
     if (calc_function(m, forward_name("jtimesF", nfwd_))) return 1;
   }
   // Successful return
-  return 0;
-}
-
-int SundialsInterface::calc_jtimesB(SundialsMemory* m, double t, const double* x, const double* z,
-    const double* rx, const double* rz, const double* fwd_rx, const double* fwd_rz,
-    double* fwd_adj_x, double* fwd_adj_z) const {
-  // Evaluate nondifferentiated and sensitivities
-  for (casadi_int d = 0; d <= nfwd_; ++d) {
-    m->arg[JTIMESB_T] = &t;  // t
-    m->arg[JTIMESB_X] = x;  // x
-    m->arg[JTIMESB_Z] = z;  // z
-    m->arg[JTIMESB_P] = m->p;  // p
-    m->arg[JTIMESB_U] = m->u;  // u
-    m->arg[JTIMESB_ADJ_ODE] = rx + d * nx1_;  // adj_ode
-    m->arg[JTIMESB_ADJ_ALG] = rz + d * nz1_;  // adj_alg
-    m->arg[JTIMESB_ADJ_QUAD] = nullptr;  // adj_quad
-    m->arg[JTIMESB_FWD_ADJ_ODE] = fwd_rx + d * nrx1_ * nadj_;  // fwd:adj_ode
-    m->arg[JTIMESB_FWD_ADJ_ALG] = fwd_rz + d * nrz1_ * nadj_;  // fwd:adj_alg
-    m->res[JTIMESB_FWD_ADJ_X] = fwd_adj_x + d * nrx1_ * nadj_;  // fwd:adj_x
-    m->res[JTIMESB_FWD_ADJ_Z] = fwd_adj_z + d * nrz1_ * nadj_;  // fwd:adj_z
-    if (calc_function(m, "jtimesB")) return 1;
-  }
   return 0;
 }
 

casadi/interfaces/sundials/sundials_interface.hpp
@@ -143,11 +143,6 @@ namespace casadi {
     int calc_jtimesF(SundialsMemory* m, double t, const double* x, const double* z,
       const double* fwd_x, const double* fwd_z, double* fwd_ode, double* fwd_alg) const;
 
-    // Jacobian of DAE-times-vector function, backward problem
-    int calc_jtimesB(SundialsMemory* m, double t, const double* x, const double* z,
-      const double* rx, const double* rz, const double* fwd_rx, const double* fwd_rz,
-      double* fwd_adj_x, double* fwd_adj_z) const;
-
     // Jacobian of DAE right-hand-side function, forward problem
     int calc_jacF(SundialsMemory* m, double t, const double* x, const double* z,
       double* jac_ode_x, double* jac_alg_x, double* jac_ode_z, double* jac_alg_z) const;
@@ -181,10 +176,6 @@ namespace casadi {
     enum JtimesFIn { JTIMESF_T, JTIMESF_X, JTIMESF_Z, JTIMESF_P, JTIMESF_U, JTIMESF_FWD_X,
       JTIMESF_FWD_Z, JTIMESF_NUM_IN};
     enum JtimesFOut { JTIMESF_FWD_ODE, JTIMESF_FWD_ALG, JTIMESF_NUM_OUT};
-    enum JtimesBIn { JTIMESB_T, JTIMESB_X, JTIMESB_Z, JTIMESB_P, JTIMESB_U,
-      JTIMESB_ADJ_ODE, JTIMESB_ADJ_ALG, JTIMESB_ADJ_QUAD,
-      JTIMESB_FWD_ADJ_ODE, JTIMESB_FWD_ADJ_ALG, JTIMESB_NUM_IN};
-    enum JtimesBOut { JTIMESB_FWD_ADJ_X, JTIMESB_FWD_ADJ_Z, JTIMESB_NUM_OUT};
     enum JacFOut {JACF_ODE_X, JACF_ALG_X, JACF_ODE_Z, JACF_ALG_Z, JACF_NUM_OUT};
     ///@}
 