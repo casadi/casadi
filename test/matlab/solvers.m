50a9c74d2bd7ed9658b97c46e89acb4cf4ece4ee
Joris Gillis <joris.gillis42@gmail.com>
2023-04-26 12:51:18 +0200

    Merge loadallplugins and solvers test

--- test/matlab/solvers.m
+++ test/matlab/solvers.m
@@ -1,5 +1,18 @@
 import casadi.*
 
+for pl = strsplit(CasadiMeta.plugins(),';')
+  out  = strsplit(pl{:},'::');
+  cls  = out{1};
+  name = out{2};
+
+if strcmp(cls,'Importer') || strcmp(cls,'XmlFile') || strcmp(cls,'Linsol')
+  eval([cls '.load_plugin(''' name ''')'])
+else
+  eval(['load_' lower(cls) '(''' name ''')'])
+end
+
+end
+
 plugins = strsplit(CasadiMeta.plugins(),';');
 
 failure = false;
