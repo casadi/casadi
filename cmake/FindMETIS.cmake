cb7efeb1e4707dc82b116bb618326baede3b8c6a
Joris Gillis <joris.gillis42@gmail.com>
2023-05-16 23:16:21 +0200

    issue #3180: restore compatibibility with system metis

--- cmake/FindMETIS.cmake
+++ cmake/FindMETIS.cmake
@@ -1,14 +1,54 @@
-# TRY TO FIND THE METIS LIBRARY
+message("METIS: find via CONFIG")
+find_package(METIS CONFIG NO_CMAKE_FIND_ROOT_PATH)
+message("METIS: ${METIS_FOUND}")
 
-find_library(METIS_LIB
-    names coinmetis metis
-    HINTS $ENV{METIS})
+if(NOT WORHP_FOUND)
 
-if(METIS_LIB)
-  message(STATUS "Found METIS: ${METIS_LIB}")
-  set(METIS_LIBRARIES ${METIS_LIB})
-  set(METIS_FOUND TRUE)
-else(METIS_LIB)
-  set(METIS_FOUND FALSE)
-  message(STATUS "Could not find METIS; looking in environmental variable METIS ($ENV{METIS})")
-endif(METIS_LIB)
+    # TRY TO FIND THE METIS LIBRARY
+
+    find_library(METIS_LIB
+        names coinmetis metis
+        HINTS $ENV{METIS})
+        
+    find_path(METIS_INCLUDE_DIR
+        metis.h
+        HINTS $ENV{METIS})
+
+    if(METIS_LIB)
+      message(STATUS "Found METIS: ${METIS_LIB}")
+      set(METIS_LIBRARIES ${METIS_LIB})
+    else()
+      message(STATUS "Could not find METIS; looking in environmental variable METIS ($ENV{METIS})")
+    endif()
+
+    if(METIS_INCLUDE_DIR)
+      message(STATUS "Found METIS include dir: ${METIS_INCLUDE_DIR}")
+    else()
+      message(STATUS "Could not find METIS include dir; looking in environmental variable METIS ($ENV{METIS})")
+    endif()
+
+    if(METIS_INCLUDE_DIR AND METIS_LIB)
+      set(METIS_FOUND TRUE)
+    else()
+      set(METIS_FOUND FALSE)
+    endif()
+
+
+
+    if(METIS_FOUND)
+        add_library(metis::metis INTERFACE IMPORTED)
+        set_target_properties(metis::metis PROPERTIES
+          INTERFACE_INCLUDE_DIRECTORIES ${METIS_INCLUDE_DIR}
+        )
+        if("${METIS_LIB}" MATCHES "coinmetis")
+            set_target_properties(metis::metis PROPERTIES
+              IMPORTED_LIBNAME coinmetis
+            )
+        else()
+            set_target_properties(metis::metis PROPERTIES
+              IMPORTED_LIBNAME metis
+            )
+        endif()
+        target_link_libraries(metis::metis INTERFACE ${METIS_LIBRARIES})
+    endif()
+endif()
