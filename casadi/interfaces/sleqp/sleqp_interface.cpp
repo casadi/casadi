96a0c1530636e00f0b71f63f5e4623ac8f7e95c8
Christoph Hansknecht <christoph.hansknecht@tu-clausthal.de>
2023-07-05 23:25:59 +0200

    fixup spelling

--- casadi/interfaces/sleqp/sleqp_interface.cpp
+++ casadi/interfaces/sleqp/sleqp_interface.cpp
@@ -1,12 +1,37 @@
+/*
+ *    This file is part of CasADi.
+ *
+ *    CasADi -- A symbolic framework for dynamic optimization.
+ *    Copyright (C) 2010-2023 Joel Andersson, Joris Gillis, Moritz Diehl,
+ *                            KU Leuven. All rights reserved.
+ *    Copyright (C) 2011-2014 Greg Horn
+ *
+ *    CasADi is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    CasADi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with CasADi; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+
 #include "sleqp_interface.hpp"
 
 
 namespace casadi {
   extern "C"
   int CASADI_NLPSOL_SLEQP_EXPORT
-  casadi_register_nlpsol_SLEQP(Nlpsol::Plugin* plugin) {
+  casadi_register_nlpsol_sleqp(Nlpsol::Plugin* plugin) {
     plugin->creator = SLEQPInterface::creator;
-    plugin->name = "SLEQP";
+    plugin->name = "sleqp";
     plugin->doc = SLEQPInterface::meta_doc.c_str();
     plugin->version = CASADI_VERSION;
     plugin->options = &SLEQPInterface::options_;
@@ -15,8 +40,8 @@ namespace casadi {
   }
 
   extern "C"
-  void CASADI_NLPSOL_SLEQP_EXPORT casadi_load_nlpsol_SLEQP() {
-    Nlpsol::registerPlugin(casadi_register_nlpsol_SLEQP);
+  void CASADI_NLPSOL_SLEQP_EXPORT casadi_load_nlpsol_sleqp() {
+    Nlpsol::registerPlugin(casadi_register_nlpsol_sleqp);
   }
 
   SLEQPInterface::SLEQPInterface(const std::string& name, const Function& nlp)
@@ -56,4 +81,5 @@ namespace casadi {
     //return calc_function(NULL, "nlp_f")==0;
     return 0;
   }
-}
+
+} // namespace casadi
