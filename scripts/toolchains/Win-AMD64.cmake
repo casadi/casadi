a012a63ae4df9498b399909e50a042e09b42cdb1
Pieter Pas <pieter.pas@outlook.com>
2023-07-25 18:25:08 +0200

    [CI] build wheels for Windows x86 and ARM64

--- scripts/toolchains/Win-AMD64.cmake
+++ scripts/toolchains/Win-AMD64.cmake
@@ -1,3 +1,3 @@
-set(CMAKE_SYSTEM_NAME "Windows")
+# set(CMAKE_SYSTEM_NAME "Windows") # Not set to avoid cross-compilation mode
 set(CMAKE_SYSTEM_PROCESSOR "AMD64")
 set(CMAKE_GENERATOR_PLATFORM "x64" CACHE INTERNAL "")
--- scripts/toolchains/Win-x86.cmake
+++ scripts/toolchains/Win-x86.cmake
@@ -1,3 +1,3 @@
-set(CMAKE_SYSTEM_NAME "Windows")
+# set(CMAKE_SYSTEM_NAME "Windows") # Not set to avoid cross-compilation mode
 set(CMAKE_SYSTEM_PROCESSOR "x86")
 set(CMAKE_GENERATOR_PLATFORM "Win32" CACHE INTERNAL "")
