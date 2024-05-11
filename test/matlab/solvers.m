2c8046eb986c1c0c46e9f53a07ed1b58834b3659
Joris Gillis <joris.gillis42@gmail.com>
2024-05-11 23:18:32 +0200

    fatrop in solvers.m

--- test/matlab/solvers.m
+++ test/matlab/solvers.m
@@ -26,7 +26,7 @@ for i=1:length(plugins)
   end
   
   if strcmp(type,'Nlpsol')
-      if ismember(name, {'scpgen', 'ampl', 'qrsqp'})
+      if ismember(name, {'scpgen', 'ampl', 'qrsqp', 'fatrop'})
          continue 
       end
       disp(['Solver:' name])
@@ -49,7 +49,7 @@ for i=1:length(plugins)
   end
   
   if strcmp(type,'Conic')
-      if ismember(name, {'hpipm', 'nlpsol', 'qrqp', 'ipqp'})
+      if ismember(name, {'hpipm', 'nlpsol', 'qrqp', 'ipqp', 'fatrop'})
          continue 
       end
       disp(['Solver:' name])
@@ -131,6 +131,99 @@ catch E
     failure = true;
 end
 end
+
+if ismember('Nlpsol::fatrop',plugins)
+try
+
+% Time horizon and number of control intervals
+T = 10; 
+N = 10;
+
+% Declare model variables
+import casadi.*
+x1 = MX.sym('x1');
+x2 = MX.sym('x2');
+x = [x1; x2];
+u = MX.sym('u');
+p = MX.sym('p');
+
+% Model equations
+xdot = [(1-x2^2)*x1 - x2 + u + p, x1];
+
+% Define integrator
+opts = struct('simplify', true, 'number_of_finite_elements', 1);
+F = casadi.integrator('F', 'rk', struct('x', x, 'p', p, 'u', u, 'ode', xdot), opts);
+
+% Start with an empty NLP
+w = {};
+w0 = [];
+lbw = [];
+ubw = [];
+J = 0;
+g = {};
+lbg = [];
+ubg = [];
+
+% "Lift" initial conditions
+Xk = MX.sym('X0', 2);
+w = {w{:}, Xk};
+lbw = [lbw, 0, 1];
+ubw = [ubw, 0, 1];
+w0 = [w0, 0.1, 0.2];
+
+% Formulate the NLP
+for k = 1:N
+    % New NLP variable for the control
+    Uk = MX.sym(['U_', num2str(k)]);
+    w = {w{:}, Uk};
+    lbw = [lbw, -1];
+    ubw = [ubw, 1];
+    w0 = [w0, 0.3];
+
+    % Integrate till the end of the interval
+    Fk = F('x0', Xk, 'u', Uk, 'p', p);
+    Xk_end = Fk.xf;
+    J = J + sumsqr(Xk) + sumsqr(Uk);
+
+    % New NLP variable for state at end of interval
+    Xk_next = MX.sym(['X_', num2str(k+1)], 2);
+    w = {w{:}, Xk_next};
+    lbw = [lbw, -0.25, -inf];
+    ubw = [ubw, inf, inf];
+    w0 = [w0, 0.1, 0.2];
+        
+    % Add equality constraint
+    g = {g{:}, Xk_next-Xk_end};
+    lbg = [lbg, 0, 0];
+    ubg = [ubg, 0, 0];
+
+    Xk = Xk_next;
+end
+
+nlp = struct;
+nlp.f = J;
+nlp.x = vertcat(w{:});
+nlp.g = vertcat(g{:});
+nlp.p = p;
+
+args = struct;
+args.x0 = w0;
+args.lbx = lbw;
+args.ubx = ubw;
+args.lbg = lbg;
+args.ubg = ubg;
+args.p = 0;
+
+f = nlpsol('solver', 'fatrop', nlp);
+f.call(args);
+
+catch E
+    disp(['Failure for fatrop'])
+    disp(E.message)
+    failure = true;
+end
+end
+
 
 assert(~failure)
 disp('Search the log for Failure')
