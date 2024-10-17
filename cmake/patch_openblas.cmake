48ad82d802cb4774917fc5b33b3bb1d4a051a685
Joris Gillis <joris.gillis42@gmail.com>
2024-10-17 14:07:23 +0000

    issue #3890: openblas compat with Visual Studio

--- cmake/patch_openblas.cmake
+++ cmake/patch_openblas.cmake
@@ -3,6 +3,7 @@ file(READ CMakeLists.txt INPUT_TEXT)
 string(REPLACE "libopenblas" "lib${CASADI_TP_SHARED_PREFIX}openblas" INPUT_TEXT "${INPUT_TEXT}")
 string(REPLACE "-fpic -shared" "-fpic -L. -shared" INPUT_TEXT "${INPUT_TEXT}")
 string(REPLACE "-Wl,-all_load" "-Wl,-all_load -Wl,-L." INPUT_TEXT "${INPUT_TEXT}")
+string(REPLACE "add_subdirectory(utest)" "" INPUT_TEXT "${INPUT_TEXT}")
 
 file(WRITE CMakeLists.txt "${INPUT_TEXT}")
 file(APPEND CMakeLists.txt "set_target_properties( \${OpenBLAS_LIBS} PROPERTIES LIBRARY_OUTPUT_NAME_DEBUG \"\${OpenBLAS_LIBNAME}\")\n")
@@ -16,5 +17,3 @@ file(APPEND CMakeLists.txt "install (FILES \${PROJECT_BINARY_DIR}/lapack\${SUFFI
 file(READ cmake/openblas.pc.in INPUT_TEXT)
 string(REPLACE "lopenblas" "l${CASADI_TP_SHARED_PREFIX}openblas" INPUT_TEXT "${INPUT_TEXT}")
 file(WRITE cmake/openblas.pc.in "${INPUT_TEXT}")
-
-
