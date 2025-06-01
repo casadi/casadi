ad45eaff1783afb94b214f2cbe8d6d3fde990e71
Joris Gillis <joris@yacoda.com>
2025-06-01 08:59:22 +0200

    main! casadi misc adding all_equal

--- casadi/core/casadi_misc.hpp
+++ casadi/core/casadi_misc.hpp
@@ -134,6 +134,12 @@ private:
   template<typename T, typename S>
   std::vector<T> vector_static_cast(const std::vector<S>& rhs);
 
+  template<typename T, typename S>
+  bool all_equal(const std::vector<T>& v, const S& e);
+
+  template<typename T>
+  bool all_equal(const std::vector<T>& v);
+
   CASADI_EXPORT std::string str_bvec(bvec_t v);
 
   /**  \brief Slicing vector
@@ -553,6 +559,20 @@ namespace casadi {
     ret.reserve(rhs.size());
     for (auto e : rhs) ret.push_back(static_cast<T>(e));
     return ret;
+  }
+
+  template<typename T>
+  bool all_equal(const std::vector<T>& v) {
+    if (v.empty()) return true;
+    return all_equal(v, v.front());
+  }
+
+  template<typename T, typename S>
+  bool all_equal(const std::vector<T>& v, const S& e) {
+    for (const auto& k : v) {
+      if (k!=e) return false;
+    }
+    return true;
   }
 
   template<typename T>
