0b169ed788d6861587fd1f90ef871622dbfdad00
Joris Gillis <joris@yacoda.com>
2025-06-02 11:45:14 +0200

    Adding Function::with_options

--- casadi/core/function_internal.cpp
+++ casadi/core/function_internal.cpp
@@ -1105,6 +1105,10 @@ namespace casadi {
     return Function(fname, arg, res, name_in_, name_out_, my_opts);
   }
 
+  Function FunctionInternal::with_options(const Dict& opts) const {
+    return wrap_as_needed(opts);
+  }
+
   Function FunctionInternal::wrap() const {
     Function f;
     std::string fname = "wrap_" + name_;
--- casadi/core/function_internal.hpp
+++ casadi/core/function_internal.hpp
@@ -798,6 +798,8 @@ namespace casadi {
         \identifier{26g} */
     Dict cache() const;
 
+    virtual Function with_options(const Dict& opts) const;
+
     /** \brief Get function in cache
 
         \identifier{lk} */
