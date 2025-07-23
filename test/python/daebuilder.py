add8afb845a3f3af375ebc51b5def432b61cc0ce
Joris Gillis <joris.gillis42@gmail.com>
2025-07-23 15:03:54 +0200

    Make DaeBuilder test reflect new get() functionality for symbolic repr

--- test/python/daebuilder.py
+++ test/python/daebuilder.py
@@ -628,9 +628,9 @@ class Daebuildertests(casadiTestCase):
     c2_val = 0.19
     dae.set('c2',c2_val)
     c_val = vertcat(c0_val,c1_val,c2_val)
-    with self.assertInException("Functionality not implemented for symbolic representatio"):
-        dae.get('c2')
-        #self.checkarray(dae.get('c2'),c2_val)
+    
+    dae.get('c2')
+    self.checkarray(dae.get('c2'),c2_val)
 
     # Parameters
     p = dae.add('p','parameter','tunable')
