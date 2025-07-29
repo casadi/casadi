0244dc188d091710c01f6a885e30f38e02f5b0f9
Joris Gillis <joris@yacoda.com>
2025-07-29 13:42:08 +0200

    cleanup

--- casadi/core/mx.cpp
+++ casadi/core/mx.cpp
@@ -1732,7 +1732,6 @@ namespace casadi {
           casadi_error("No such option: " + std::string(op.first));
         }
       }
-      uout() << "lift_shared:" << lift_shared << std::endl;
       // Sort the expression
       Function f("tmp_extract", std::vector<MX>{}, ex, Dict{{"max_io", 0}, {"allow_free", true}});
       auto *ff = f.get<MXFunction>();
@@ -1802,7 +1801,6 @@ namespace casadi {
           break;
         }
       }
-      uout() << "replace" << replace << std::endl;
       // New variables and definitions
       v.clear();
       v.reserve(replace.size());
