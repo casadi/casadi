42eeb6bf6a233a02a308d0d41f3b8296410f50f4
Pieter Pas <pieter.p.dev@outlook.com>
2025-08-28 14:52:16 +0200

    Support shared Matlab installations

--- scripts/ci/build-matlab-linux.sh
+++ scripts/ci/build-matlab-linux.sh
@@ -12,12 +12,15 @@ triple="${2:-x86_64-bionic-linux-gnu}"
 # Package and output directories
 pkg_dir="${3:-.}"
 out_dir="${4:-staging/matlab}"
+shared="${5:-False}"
 
 # Create Conan profiles
 matlab_profile="$PWD/profile-matlab.local.conan"
 cat <<- EOF > "$matlab_profile"
 include($PWD/scripts/ci/profiles/$triple.profile)
 include($PWD/scripts/ci/profiles/alpaqa-matlab.profile)
+[options]
+alpaqa/*:shared=$shared
 [conf]
 tools.build:exelinkflags+=["-static-libgcc"]
 tools.build:sharedlinkflags+=["-static-libgcc"]
@@ -36,9 +39,13 @@ pushd "$pkg_dir"
 cmake --preset conan-matlab-release \
     -D CMAKE_FIND_ROOT_PATH="$matlab_dir" \
     -D CMAKE_C_COMPILER_LAUNCHER=sccache \
-    -D CMAKE_CXX_COMPILER_LAUNCHER=sccache
-cmake --build --preset conan-matlab-release \
-    -t alpaqa_mex -v
-cmake --install build/matlab-release \
-    --component mex_interface --prefix "$out_dir"
+    -D CMAKE_CXX_COMPILER_LAUNCHER=sccache \
+    -D CMAKE_INSTALL_PREFIX="/" \
+    -D ALPAQA_INSTALL_MEXDIR="/" \
+    -D ALPAQA_INSTALL_LIBDIR="/alpaqa.lib" --fresh
+cmake --build --preset conan-matlab-release -v
+for component in lib casadi extra mex_interface; do
+    DESTDIR="$out_dir" \
+    cmake --install build/matlab-release --component $component
+done
 popd
