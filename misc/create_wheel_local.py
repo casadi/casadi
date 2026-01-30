4adef54603a2b3db1bd73a566057b20d7484ff0c
Joris Gillis <joris.gillis42@gmail.com>
2026-01-30 14:59:18 +0100

    Bump macos intel minimal version to 11.0

--- misc/create_wheel_local.py
+++ misc/create_wheel_local.py
@@ -49,8 +49,8 @@ if os_name=="linux":
   tag = "cp%s-none-%s" % (pyversion,arch.replace("-","_"))
 elif os_name=="osx":
   if arch=="x86_64":
-    tag = ["cp%s-none-macosx_10_13_x86_64" % (pyversion),
-         "cp%s-none-macosx_10_13_intel" % (pyversion)]
+    tag = ["cp%s-none-macosx_11_0_x86_64" % (pyversion),
+         "cp%s-none-macosx_11_0_intel" % (pyversion)]
   elif arch=="arm64":
     tag = "cp%s-none-macosx_11_0_arm64" % (pyversion)
 elif os_name=="windows":
