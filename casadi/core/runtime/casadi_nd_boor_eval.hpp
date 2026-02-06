3f4031247189611241b0e08bb6662ec7c127e61e
Joris Gillis <joris.gillis42@gmail.com>
2026-02-06 15:12:03 +0100

    Refactor bspline with a tensor product (recursive impl)

--- casadi/core/runtime/casadi_nd_boor_eval.hpp
+++ casadi/core/runtime/casadi_nd_boor_eval.hpp
@@ -20,23 +20,17 @@
 // SYMBOL "nd_boor_eval"
 template<typename T1>
 void casadi_nd_boor_eval(T1* ret, casadi_int n_dims, const T1* all_knots, const casadi_int* offset, const casadi_int* all_degree, const casadi_int* strides, const T1* c, casadi_int m, const T1* all_x, const casadi_int* lookup_mode, casadi_int* iw, T1* w) { // NOLINT(whitespace/line_length)
-  casadi_int n_iter, k, i, pivot;
-  casadi_int *boor_offset, *starts, *index, *coeff_offset;
-  T1 *cumprod, *all_boor;
+  casadi_int k;
+  casadi_int *boor_offset, *starts;
+  T1 *all_boor;
 
   boor_offset = iw; iw+=n_dims+1;
   starts = iw; iw+=n_dims;
-  index = iw; iw+=n_dims;
-  coeff_offset = iw;
 
-  cumprod = w; w+= n_dims+1;
   all_boor = w;
 
   boor_offset[0] = 0;
-  cumprod[n_dims] = 1;
-  coeff_offset[n_dims] = 0;
 
-  n_iter = 1;
   for (k=0;k<n_dims;++k) {
     T1 *boor;
     const T1* knots;
@@ -71,50 +65,9 @@ void casadi_nd_boor_eval(T1* ret, casadi_int n_dims, const T1* all_knots, const 
     }
     casadi_de_boor(x, knots+start, 2*degree+2, degree, boor);
     boor+= degree+1;
-    n_iter*= degree+1;
     boor_offset[k+1] = boor_offset[k] + degree+1;
   }
 
-  casadi_clear_casadi_int(index, n_dims);
-
-  // Prepare cumulative product
-  for (pivot=n_dims-1;pivot>=0;--pivot) {
-    cumprod[pivot] = (*(all_boor+boor_offset[pivot]))*cumprod[pivot+1];
-    coeff_offset[pivot] = starts[pivot]*strides[pivot]+coeff_offset[pivot+1];
-  }
-
-  for (k=0;k<n_iter;++k) {
-    casadi_int pivot = 0;
-    // accumulate result
-    for (i=0;i<m;++i) ret[i] += c[coeff_offset[0]+i]*cumprod[0];
-
-    // Increment index
-    index[0]++;
-
-    // Handle index overflow
-    {
-      // increment next index (forward)
-      while (index[pivot]==boor_offset[pivot+1]-boor_offset[pivot]) {
-        index[pivot] = 0;
-        if (pivot==n_dims-1) break;
-        index[++pivot]++;
-      }
-
-      // update cumulative structures (reverse)
-      while (pivot>0) {
-        // Compute product
-        cumprod[pivot] = (*(all_boor+boor_offset[pivot]+index[pivot]))*cumprod[pivot+1];
-        // Compute offset
-        coeff_offset[pivot] = (starts[pivot]+index[pivot])*strides[pivot]+coeff_offset[pivot+1];
-        pivot--;
-      }
-    }
-
-    // Compute product
-    cumprod[0] = (*(all_boor+index[0]))*cumprod[1];
-
-    // Compute offset
-    coeff_offset[0] = (starts[0]+index[0])*m+coeff_offset[1];
-
-  }
+  casadi_tensor_ttv(ret, n_dims-1, n_dims, all_boor, boor_offset,
+      starts, strides, c, m, 1.0, 0);
 }
