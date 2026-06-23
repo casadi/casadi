9fcce8ec15dfea8a91a7ae741185ec6c0ce32f7f
Joel Andersson <joel@jaeandersson.com>
2026-06-23 07:50:44 -0400

    Issue #4365 Added Sparsity::star_coloring_new Should replace Sparsity::star_coloring and Sparsity::star_coloring2

--- casadi/core/sparsity.hpp
+++ casadi/core/sparsity.hpp
@@ -1091,6 +1091,20 @@ namespace casadi {
                           casadi_int cutoff = std::numeric_limits<casadi_int>::max()) const;
 
     /** \brief Perform a star coloring of a symmetric matrix:
+        A greedy distance-2 coloring algorithm
+        Algorithm 4.1 in
+          What Color Is Your Jacobian? Graph Coloring for Computing Derivatives
+          A. H. GEBREMEDHIN, F. MANNE, A. POTHEN
+          SIAM Rev., 47(4), 629–705 (2006)
+        or, alternatively (new_algo = true),
+          NEW ACYCLIC AND STAR COLORING ALGORITHMS WITH APPLICATION TO COMPUTING HESSIANS
+          A. H. GEBREMEDHIN, A. TARAFDAR, F. MANNE, A. POTHEN
+          SIAM J. SCI. COMPUT. Vol. 29, No. 3, pp. 1042–1072 (2007)
+    */
+    Sparsity star_coloring_new(std::vector<casadi_int>& SWIG_OUTPUT(which_color),
+        const Dict& opts=Dict()) const;
+
+    /** \brief Perform a star coloring of a symmetric matrix:
 
         A greedy distance-2 coloring algorithm
         Algorithm 4.1 in
